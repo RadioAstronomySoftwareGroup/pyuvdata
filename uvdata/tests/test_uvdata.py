a0adac64411b6208bc5b0df9fffc53c193f1256a
Danny Jacobs <daniel.c.jacobs@asu.edu>
2016-05-18 13:19:48 -0700

    commenting the ReadMiriad test

--- uvdata/tests/test_uvdata.py
+++ uvdata/tests/test_uvdata.py
@@ -206,7 +206,7 @@ class TestReadFHD(unittest.TestCase):
 
             del(fhd_uv)
             del(uvfits_uv)
-
+'''
 class TestReadMiriad(unittest.TestCase):
     def setUp(self):
         self.datafile = '../../data/zen.2456865.60537.uvcRRE'
@@ -216,6 +216,6 @@ class TestReadMiriad(unittest.TestCase):
         miriad_uv = UVData()
         status = miriad_uv.read_miriad(self.datafile)
         self.assertTrue(status)
-        
+'''        
 if __name__ == '__main__':
     unittest.main()
