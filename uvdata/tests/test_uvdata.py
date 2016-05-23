2be5a0c4958631c0c26714c6f6fcf5efcb7269ac
Danny Jacobs <daniel.c.jacobs@asu.edu>
2016-05-23 07:17:54 -0700

    made miriad test run not by default

--- uvdata/tests/test_uvdata.py
+++ uvdata/tests/test_uvdata.py
@@ -206,16 +206,17 @@ class TestReadFHD(unittest.TestCase):
 
             del(fhd_uv)
             del(uvfits_uv)
-'''
 class TestReadMiriad(unittest.TestCase):
     def setUp(self):
+        self.supress_miriad_test = True
+        if self.supress_miriad_test: return True
         self.datafile = '../../data/zen.2456865.60537.uvcRRE'
         if not os.path.exists(self.datafile):
             raise(IOError,'miriad file not found')
     def test_ReadMiriad(self):
+        if self.supress_miriad_test: return True
         miriad_uv = UVData()
         status = miriad_uv.read_miriad(self.datafile)
         self.assertTrue(status)
-'''        
 if __name__ == '__main__':
     unittest.main()
