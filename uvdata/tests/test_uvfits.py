a58f6d3e0b2684069eca80f0ae5f177f887ab9d9
Adam Beardsley <adam.p.beardsley@gmail.com>
2016-07-29 15:14:50 -0700

    Cleaned up uvfits tests

--- uvdata/tests/test_uvfits.py
+++ uvdata/tests/test_uvfits.py
@@ -1,4 +1,4 @@
-import unittest
+import nose.tools as nt
 import os
 import os.path as op
 import shutil
@@ -8,92 +8,78 @@ from astropy.io import fits
 from test_functions import *
 
 
-class TestReadUVFits(unittest.TestCase):
-    def test_ReadNRAO(self):
-        testfile = '../data/day2_TDEM0003_10s_norx_1src_1spw.uvfits'
-        testfile_no_spw = '../data/zen.2456865.60537.xy.uvcRREAAM.uvfits'
-        UV = UVData()
-        self.assertRaises(ValueError, UV.read, testfile, 'vufits')  # Wrong filetype
-        test = checkWarnings(UV.read, [testfile, 'uvfits'],
-                             message='Telescope EVLA is not')
-        self.assertEqual(test, (True, True))
-        expected_extra_keywords = ['OBSERVER', 'SORTORD', 'SPECSYS',
-                                   'RESTFREQ', 'ORIGIN']
-        self.assertEqual(expected_extra_keywords.sort(),
-                         UV.extra_keywords.keys().sort())
-
-        del(UV)
-        UV = UVData()
-        test = checkWarnings(UV.read, [testfile_no_spw, 'uvfits'],
-                             known_warning='paper_uvfits')
-        self.assertEqual(test, (True, True))
-
-        del(UV)
-
-    def test_breaks(self):
-        # Here I'll group together tests that should raise exceptions
-        multi_subarray_file = '../data/test/multi_subarray.uvfits'
-        if not op.exists(multi_subarray_file):
-            # Make uvfits file with multiple subarrays
-            # First read/write using pyuvdata to force use of antenna arrays.
-            UV = UVData()
-            UV.read('../data/day2_TDEM0003_10s_norx_1src_1spw.uvfits', 'uvfits')
-            UV.write('../data/test/temp.uvfits', 'uvfits')
-            # Then use astropy.io.fits to change the subarray array
-            F = fits.open('../data/test/temp.uvfits')
-            F[0].data['SUBARRAY'][2] = 2.0
-            F.writeto(multi_subarray_file)
-            os.remove('../data/test/temp.uvfits')
-            del(UV)
-        UV = UVData()
-        self.assertRaises(ValueError, UV.read, multi_subarray_file, 'uvfits')
-
-    # def test_readRTS(self):
-    #     testfile = '../data/pumav2_SelfCal300_Peel300_01.uvfits'
-    #     UV = UVData()
-    #     test = UV.read_uvfits(testfile)
-    #     self.assertTrue(test)
+def test_ReadNRAO():
+    UV = UVData()
+    testfile = '../data/day2_TDEM0003_10s_norx_1src_1spw.uvfits'
+    expected_extra_keywords = ['OBSERVER', 'SORTORD', 'SPECSYS',
+                               'RESTFREQ', 'ORIGIN']
+    read_out, status = checkWarnings(UV.read, [testfile, 'uvfits'],
+                                     message='Telescope EVLA is not')
+    nt.assert_true(read_out)
+    nt.assert_true(status)
+    nt.assert_equal(expected_extra_keywords.sort(),
+                    UV.extra_keywords.keys().sort())
+    del(UV)
 
 
-class TestWriteUVFits(unittest.TestCase):
-    def setUp(self):
-        self.test_file_directory = '../data/test/'
+def test_noSPW():
+    UV = UVData()
+    testfile_no_spw = '../data/zen.2456865.60537.xy.uvcRREAAM.uvfits'
+    read_output, status = checkWarnings(UV.read, [testfile_no_spw, 'uvfits'],
+                                        known_warning='paper_uvfits')
+    nt.assert_true(read_output)
+    nt.assert_true(status)
+    del(UV)
 
-    # def tearDown(self):
-    #      shutil.rmtree(self.test_file_directory)
 
-    def test_writeNRAO(self):
-        testfile = '../data/day2_TDEM0003_10s_norx_1src_1spw.uvfits'
-        # testfile = '../data/PRISim_output_manual_conversion.uvfits'
-        UV = UVData()
-        checkWarnings(UV.read, [testfile, 'uvfits'], message='Telescope EVLA is not')
+# def test_readRTS():
+#     UV = UVData()
+#     testfile = '../data/pumav2_SelfCal300_Peel300_01.uvfits'
+#     test = UV.read_uvfits(testfile)
+#     nt.assert_true(test)
 
-        write_file = op.join(self.test_file_directory,
-                             'outtest_casa_1src_1spw.uvfits')
+def test_BreakReadUVFits():
+    UV = UVData()
+    testfile = '../data/day2_TDEM0003_10s_norx_1src_1spw.uvfits'
+    multi_subarray_file = '../data/multi_subarray.uvfits'
+    nt.assert_raises(ValueError, UV.read, testfile, 'vufits')  # Wrong filetype
+    del(UV)
+    UV = UVData()
+    nt.assert_raises(ValueError, UV.read, multi_subarray_file, 'uvfits')
+    del(UV)
 
-        test = UV.write(write_file, file_type='uvfits')
-        self.assertTrue(test)
-        del(UV)
 
-    def test_spwnotsupported(self):
-        # testfile = '../data/day2_TDEM0003_10s_norx_1src_1spw.uvfits'
-        # testfile = '../data/PRISim_output_manual_conversion.uvfits'
-        testfile = '../data/day2_TDEM0003_10s_norx_1scan.uvfits'
-        self.assertTrue(op.exists(testfile))
-        UV = UVData()
-        self.assertRaises(ValueError, UV.read, testfile, 'uvfits')
-        del(UV)
+def test_writeNRAO():
+    UV = UVData()
+    testfile = '../data/day2_TDEM0003_10s_norx_1src_1spw.uvfits'
+    write_file = '../data/test/outtest_casa_1src_1spw.uvfits'
+    read_out, status = checkWarnings(UV.read, [testfile, 'uvfits'],
+                                     message='Telescope EVLA is not')
+    test = UV.write(write_file, file_type='uvfits')
+    nt.assert_true(status)
+    nt.assert_true(test)
+    del(UV)
 
-    def test_readwriteread(self):
-        testfile = '../data/day2_TDEM0003_10s_norx_1src_1spw.uvfits'
-        uv_in = UVData()
-        uv_out = UVData()
-        checkWarnings(uv_in.read, [testfile, 'uvfits'], message='Telescope EVLA is not')
-        write_file = op.join(self.test_file_directory,
-                             'outtest_casa.uvfits')
-        uv_in.write(write_file, file_type='uvfits')
-        checkWarnings(uv_out.read, [write_file, 'uvfits'],
-                      message='Telescope EVLA is not')
-        self.assertEqual(uv_in, uv_out)
-        del(uv_in)
-        del(uv_out)
+
+def test_spwnotsupported():
+    UV = UVData()
+    testfile = '../data/day2_TDEM0003_10s_norx_1scan.uvfits'
+    nt.assert_raises(ValueError, UV.read, testfile, 'uvfits')
+    del(UV)
+
+
+def test_readwriteread():
+    uv_in = UVData()
+    uv_out = UVData()
+    testfile = '../data/day2_TDEM0003_10s_norx_1src_1spw.uvfits'
+    write_file = '../data/test/outtest_casa.uvfits'
+    read_out, read_status = checkWarnings(uv_in.read, [testfile, 'uvfits'],
+                                          message='Telescope EVLA is not')
+    uv_in.write(write_file, file_type='uvfits')
+    write_out, write_status = checkWarnings(uv_out.read, [write_file, 'uvfits'],
+                                            message='Telescope EVLA is not')
+    nt.assert_true(read_status)
+    nt.assert_true(write_status)
+    nt.assert_equal(uv_in, uv_out)
+    del(uv_in)
+    del(uv_out)
