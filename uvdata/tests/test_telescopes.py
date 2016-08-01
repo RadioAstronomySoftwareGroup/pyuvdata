f2ae4339f39bfef7f364107c781f7b4a7535f4c9
Bryna Hazelton <bryna.hazelton@gmail.com>
2016-08-01 13:33:28 -0700

    bug fix in test_telescopes

--- uvdata/tests/test_telescopes.py
+++ uvdata/tests/test_telescopes.py
@@ -1,18 +1,17 @@
 import unittest
 import numpy as np
 import nose.tools as nt
-from uvdata.telescopes import Telescope
-
+import uvdata.telescopes as uvtel
 
 required_parameters = ['_telescope_name', '_telescope_location']
 required_properties = ['telescope_name', 'telescope_location']
 other_attributes = ['citation', 'telescope_location_lat_lon_alt',
                     'telescope_location_lat_lon_alt_degrees']
-known_telescopes = ['PAPER', 'HERA', 'MWA']
+expected_known_telescopes = ['PAPER', 'HERA', 'MWA']
 
 
 def test_parameter_iter():
-    telescope_obj = Telescope()
+    telescope_obj = uvtel.Telescope()
     all = []
     for prop in telescope_obj.parameter_iter():
         all.append(prop)
@@ -22,7 +21,7 @@ def test_parameter_iter():
 
 
 def test_required_parameter_iter():
-    telescope_obj = Telescope()
+    telescope_obj = uvtel.Telescope()
     required = []
     for prop in telescope_obj.required_parameter_iter():
         required.append(prop)
@@ -32,7 +31,7 @@ def test_required_parameter_iter():
 
 
 def test_parameters_exist():
-    telescope_obj = Telescope()
+    telescope_obj = uvtel.Telescope()
     expected_parameters = required_parameters
     for a in expected_parameters:
         nt.assert_true(hasattr(telescope_obj, a),
@@ -40,7 +39,7 @@ def test_parameters_exist():
 
 
 def test_unexpected_attributes():
-    telescope_obj = Telescope()
+    telescope_obj = uvtel.Telescope()
     expected_attributes = required_properties + other_attributes
     attributes = [i for i in telescope_obj.__dict__.keys() if i[0] != '_']
     for a in attributes:
@@ -49,7 +48,7 @@ def test_unexpected_attributes():
 
 
 def test_properties():
-    telescope_obj = Telescope()
+    telescope_obj = uvtel.Telescope()
     prop_dict = dict(zip(required_properties, required_parameters))
     for k, v in prop_dict.iteritems():
         rand_num = np.random.rand()
@@ -63,5 +62,4 @@ def test_properties():
 
 
 def test_known_telescopes():
-    nt.assert_equal(uvdata.telescopes.known_telescopes.sort(),
-                    known_telescopes.sort())
+    nt.assert_equal(uvtel.known_telescopes().sort(), expected_known_telescopes.sort())
