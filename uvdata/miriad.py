a9e467b889bfdf5379b66787010a110604c2b18d
Jonathan Pober <jonathan.pober@gmail.com>
2016-08-10 11:18:52 -0400

    Don't write antenna positions to miriad files when they don't exist

--- uvdata/miriad.py
+++ uvdata/miriad.py
@@ -93,7 +93,7 @@ class Miriad(uvdata.uv.UVData):
         try:
             self.antenna_positions = \
                 self.antenna_positions.reshape(3, self.Nants_telescope).T
-        except(ValueError, AttributeError): #workaround for known errors with bad antenna positions
+        except(ValueError): #workaround for known errors with bad antenna positions
             self.antenna_positions = None
         self.channel_width *= 1e9  # change from GHz to Hz
 
@@ -285,8 +285,11 @@ class Miriad(uvdata.uv.UVData):
         uv['longitu'] = self.telescope_location_lat_lon_alt[1]
         uv.add_var('nants', 'i')
         uv['nants'] = self.Nants_telescope
-        uv.add_var('antpos', 'd')
-        uv['antpos'] = self.antenna_positions.T.flatten()
+        try:
+            uv.add_var('antpos', 'd')
+            uv['antpos'] = self.antenna_positions.T.flatten()
+        except(AttributeError):
+            pass #don't write bad antenna positions
         uv.add_var('sfreq', 'd')
         uv['sfreq'] = self.freq_array[0, 0] / 1e9  # first spw; in GHz
         uv.add_var('epoch', 'r')
