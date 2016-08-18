b1259e35358389fc7e2efbd730ebddb5d27bf084
Bryna Hazelton <bryna.hazelton@gmail.com>
2016-08-17 18:06:54 -0700

    PEP8 fixes

--- scripts/miriad_to_uvfits.py
+++ scripts/miriad_to_uvfits.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python
-import aipy as a, numpy as np
-import optparse, sys, os
+import aipy as a
+import numpy as np
+import optparse
+import sys
+import os
 import uvdata
 from matplotlib.pyplot import *
 o = optparse.OptionParser()
 o.set_usage('miriad_to_uvfits.py  *.uv')
 o.set_description(__doc__)
-#a.scripting.add_standard_options(o,cal=True)
-opts,args = o.parse_args(sys.argv[1:])
+# a.scripting.add_standard_options(o,cal=True)
+opts, args = o.parse_args(sys.argv[1:])
 
 for filename in args:
     UV = uvdata.uv.UVData()
-    UV.read_miriad(filename,'miriad')
-    outfilename = filename+'.uvfits'
+    UV.read_miriad(filename, 'miriad')
+    outfilename = filename + '.uvfits'
     UV.phase(time=UV.time_array[0])
-    UV.write_uvfits(outfilename,'uvfits')
+    UV.write_uvfits(outfilename, 'uvfits')
 show()
-
