47b8963eee35456f1293063edffbf705fddc0e93
Danny Jacobs <daniel.c.jacobs@asu.edu>
2016-08-29 11:54:36 -0700

    Adding a script to build the parameter list

---
+++ docs/make_parameters.py
@@ -0,0 +1,42 @@
+"""
+Format the UVData object parameters into a sphinx rst file.
+"""
+from uvdata.uv import UVData
+import numpy as np
+from astropy.time import Time
+
+UV = UVData()
+out = 'Parameters\n==============\n'
+out += 'Required\n----------------\n'
+out += """These parameters are required to make most kinds of uv data files.  In many cases there are default values available which let you sidestep the requirement (see spoof values).
+
+            Parameters are implimented using the UVParameter object. See the uvbase documentation for more
+
+            """
+out += "\n\n"
+for thing in UV.required_parameter_iter():
+    obj = getattr(UV,thing)
+    out += '*UVData.*\\ **{name}**\n'.format(name=obj.name)
+    out += '     {desc}\n'.format(desc=obj.description)
+    out += "\n"
+
+
+
+
+
+
+
+out += 'Not required\n----------------\n'
+out += 'These parameters are defined by one or more file standard but are not officially required.\n'
+out += "\n\n"
+for thing in UV.extra_parameter_iter():
+    obj = getattr(UV,thing)
+    out += '*UVData.*\\ **{name}**\n'.format(name=obj.name)
+    out += '     {desc}\n'.format(desc=obj.description)
+    out += "\n"
+t = Time.now()
+t.out_subfmt = 'date'
+out += "last updated: {date}".format(date=t.iso)
+F = open('parameters.rst','w')
+F.write(out)
+print "wrote parameters.rst"
