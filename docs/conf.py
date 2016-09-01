865ad5c6eb85ec0ef744f9a48b39567cdc5f77d1
Bryna Hazelton <bryna.hazelton@gmail.com>
2016-08-31 17:56:04 -0700

    trying to fix napoleon sphinx plugin

--- docs/conf.py
+++ docs/conf.py
@@ -34,7 +34,7 @@ extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
     'sphinx.ext.viewcode'
-    'sphinx.ext.napoleon',
+    'sphinxcontrib-napoleon',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
