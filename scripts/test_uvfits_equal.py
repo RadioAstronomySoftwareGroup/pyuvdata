b588d426ec500db82995fa830298757f7a1afd52
Bryna Hazelton <brynah@phys.washington.edu>
2016-09-01 18:42:56 -0700

    Add little script to test 2 uvfits files for equality

---
+++ scripts/test_uvfits_equal.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python
+
+import argparse
+import os.path as op
+from uvdata.uv import UVData
+
+parser = argparse.ArgumentParser()
+parser.add_argument('uvfits1',
+                    help='name of first uvfits file.')
+parser.add_argument('uvfits2',
+                    help='name of second uvfits file to compare to first.')
+
+args = parser.parse_args()
+
+uvfits_file1 = args.uvfits1
+if not op.isfile(uvfits_file1):
+    raise IOError('There is no file named {}'.format(args.uvfits_file1))
+
+uvfits_file2 = args.uvfits2
+if not op.isfile(uvfits_file2):
+    raise IOError('There is no file named {}'.format(args.uvfits_file2))
+
+uv1 = UVData()
+uv1.read_uvfits(uvfits_file1)
+
+uv2 = UVData()
+uv2.read_uvfits(uvfits_file2)
+
+if uv1 == uv2:
+    print('uvdata objects from files are equal')
+else:
+    print('uvdata objects from files are not equal')
+
+del(this_uv)
