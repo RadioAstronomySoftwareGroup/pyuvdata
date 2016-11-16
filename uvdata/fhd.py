fix calculation of Nants_data

uvdata/fhd.py
@@ -154,8 +154,7 @@ class FHD(UVData):
         self.ant_1_array = bl_info['TILE_A'][0] - 1
         self.ant_2_array = bl_info['TILE_B'][0] - 1
 
-        self.Nants_data = np.max([len(np.unique(self.ant_1_array)),
-                                  len(np.unique(self.ant_2_array))])
+        self.Nants_data = int(len(np.unique(self.ant_1_array.tolist() + self.ant_2_array.tolist())))
 
         self.antenna_names = bl_info['TILE_NAMES'][0].tolist()
         self.Nants_telescope = len(self.antenna_names)

uvdata/miriad.py
@@ -162,6 +162,7 @@ class Miriad(UVData):
             np.concatenate([[k[2] for k in d] for d in data_accumulator.values()])))
         ant_j_unique = list(set(
             np.concatenate([[k[3] for k in d] for d in data_accumulator.values()])))
+        sorted_unique_ants = sorted(list(set(ant_i_unique + ant_j_unique)))
 
         unique_blts = []
         for d in data_accumulator.values():
@@ -169,8 +170,8 @@ class Miriad(UVData):
                 blt = [k[1], k[2], k[3]]
                 if blt not in unique_blts:
                     unique_blts.append(blt)
+        self.Nants_data = len(sorted_unique_ants)
 
-        self.Nants_data = max(len(ant_i_unique), len(ant_j_unique))
         self.antenna_numbers = np.arange(self.Nants_telescope)
         self.antenna_names = self.antenna_numbers.astype(str).tolist()
         # form up a grid which indexes time and baselines along the 'long'

uvdata/uvfits.py
@@ -113,8 +113,7 @@ class UVFITS(UVData):
         self.Nbls = len(np.unique(self.baseline_array))
 
         # initialize internal variables based on the antenna table
-        self.Nants_data = int(np.max([len(np.unique(self.ant_1_array)),
-                                      len(np.unique(self.ant_2_array))]))
+        self.Nants_data = int(len(np.unique(self.ant_1_array.tolist() + self.ant_2_array.tolist())))
 
         self.set_phased()
         # check if we have an spw dimension