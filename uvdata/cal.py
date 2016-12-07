423149a2dad74164c9b46953cb3c1f360aaa3b94
Zaki Ali <zakiali88@gmail.com>
2016-12-06 22:09:46 -0800

    conform to pep8 in cal.py

--- uvdata/cal.py
+++ uvdata/cal.py
@@ -1,7 +1,7 @@
-#comment notes
-#1. Currently supporting direction independent gains. Future versions may
+# comment notes
+# 1. Currently supporting direction independent gains. Future versions may
 #   support direction dependent gains with list of such objects.
-#2.
+# 2.
 import numpy as np
 from uvbase import UVBase
 import parameter as uvp
@@ -10,13 +10,17 @@ import parameter as uvp
 class UVCal(UVBase):
     """ A class defining a calibration """
     def __init__(self):
-        self._Nfreqs = uvp.UVParameter('Nfreqs', description='Number of frequency channels',
+        self._Nfreqs = uvp.UVParameter('Nfreqs',
+                                       description='Number of frequency channels',
                                        expected_type=int)
-        self._Npols = uvp.UVParameter('Npols', description='Number of polarizations',
+        self._Npols = uvp.UVParameter('Npols',
+                                      description='Number of polarizations',
                                       expected_type=int)
-        self._Ntimes = uvp.UVParameter('Ntimes', description='Number of times',
+        self._Ntimes = uvp.UVParameter('Ntimes',
+                                       description='Number of times',
                                        expected_type=int)
-        self._history = uvp.UVParameter('history', description='String of history, units English',
+        self._history = uvp.UVParameter('history',
+                                        description='String of history, units English',
                                         form='str', expected_type=str)
 
         desc = ('Number of antennas with data present (i.e. number of unique '
@@ -30,31 +34,32 @@ class UVCal(UVBase):
                 'to ant_1_array and ant_2_array). There must be one entry '
                 'here for each unique entry in ant_1_array and '
                 'ant_2_array, but there may be extras as well.')
-        self._antenna_names = uvp.UVParameter('antenna_names', description=desc,
+        self._antenna_names = uvp.UVParameter('antenna_names',
+                                              description=desc,
                                               form=('Nants_telescope',),
                                               expected_type=str)
 
-        desc = ('List of integer antenna numbers corresponding to antenna_names, '
+        desc = ('List of integer antenna numbers corresponding to'
+                'antenna_names,'
                 'shape (Nants_telescope). There must be one '
                 'entry here for each unique entry in ant_1_array and '
                 'ant_2_array, but there may be extras as well.')
-        self._antenna_numbers = uvp.UVParameter('antenna_numbers', description=desc,
+        self._antenna_numbers = uvp.UVParameter('antenna_numbers',
+                                                description=desc,
                                                 form=('Nants_telescope',),
                                                 expected_type=int)
 
         desc = ('Number of antennas in the array. May be larger ' +
                 'than the number of antennas with data')
         self._Nants_telescope = uvp.UVParameter('Nants_telescope',
-                                                description=desc, expected_type=int)
-
-
+                                                description=desc,
+                                                expected_type=int)
 
         desc = 'Array of frequencies, shape (Nspws, Nfreqs), units Hz'
         self._freq_array = uvp.UVParameter('freq_array', description=desc,
                                            form=('Nspws', 'Nfreqs'),
                                            expected_type=np.float,
                                            tols=1e-3)  # mHz
-
 
         desc = ('Array of polarization integers, shape (Npols). '
                 'AIPS Memo 117 says: stokes 1:4 (I,Q,U,V);  '
@@ -75,12 +80,12 @@ class UVCal(UVBase):
         desc = ('The convention for applying he calibration solutions to data.'
                 'Divide/multiply uncalibrated data by gains.')
         self._gain_convention = uvp.UVParameter('gain_convention', form='str',
-                                           expected_type=str,
-                                           description=desc,
-                                           sane_vals=['divide', 'multiply'])
+                                                expected_type=str,
+                                                description=desc,
+                                                sane_vals=['divide', 'multiply'])
 
-        desc = ('Array of flags, True is flagged. shape: (Nants_data, Nfreqs, Ntimes, '
-                'Npols), type = bool.')
+        desc = ('Array of flags, True is flagged.'
+                'shape: (Nants_data, Nfreqs, Ntimes, Npols), type = bool.')
         self._flag_array = uvp.UVParameter('flag_array', description=desc,
                                            form=('Nants_data', 'Nfreqs',
                                                  'Ntimes', 'Npols'),
@@ -89,20 +94,20 @@ class UVCal(UVBase):
         desc = ('Array of qualities, shape: (Nants_data, Nfreqs, Ntimes, '
                 'Npols), type = float.')
         self._quality_array = uvp.UVParameter('quality_array', description=desc,
-                                           form=('Nants_data', 'Nfreqs',
-                                                 'Ntimes', 'Npols'),
-                                           expected_type=np.float)
+                                              form=('Nants_data', 'Nfreqs',
+                                                    'Ntimes', 'Npols'),
+                                              expected_type=np.float)
         desc = ('Delay or gain switch parameter. Values are delay/gain.')
         self._delay_gain_switch = uvp.UVParameter('delay_gain_switch', form='str',
-                                           expected_type=str,
-                                           description=desc,
-                                           sane_vals=['delay', 'gain'])
+                                                  expected_type=str,
+                                                  description=desc,
+                                                  sane_vals=['delay', 'gain'])
 
         desc = ('Polarization orientation. Values are E/N/Unknown.')
         self._x_orientation = uvp.UVParameter('x_orientation', description=desc,
-                                            expected_type=str,
-                                            sane_vals=['E', 'N', 'U'])
-#--- optional parameters ---
+                                              expected_type=str,
+                                              sane_vals=['E', 'N', 'U'])
+# --- optional parameters ---
         desc = ('Array of gains, shape: (Nants_data, Nfreqs, Ntimes, '
                 'Npols), type = complex float.')
         self._gain_array = uvp.UVParameter('gain_array', description=desc,
@@ -115,17 +120,17 @@ class UVCal(UVBase):
         self._delay_array = uvp.UVParameter('delay_array', description=desc,
                                             required=False,
                                             form=('Nants_data', 'Ntimes',
-                                                   'Npols'),
+                                                  'Npols'),
                                             expected_type=np.float)
 
-#--- truly optional parameters ---
+# --- truly optional parameters ---
         desc = ('Array of input flags, True is flagged. shape: (Nants_data, '
-        'Nfreqs, Ntimes, Npols), type = bool.')
+                'Nfreqs, Ntimes, Npols), type = bool.')
         self._input_flag_array = uvp.UVParameter('input_flag_array',
-                                           description=desc,
-                                           required=False,
-                                           form=('Nants_data', 'Nfreqs',
-                                                 'Ntimes', 'Npols'),
-                                           expected_type=np.bool)
+                                                 description=desc,
+                                                 required=False,
+                                                 form=('Nants_data', 'Nfreqs',
+                                                       'Ntimes', 'Npols'),
+                                                 expected_type=np.bool)
         super(UVCal, self).__init__()
-        #need to have either gain_array or delay_array
+        # need to have either gain_array or delay_array
