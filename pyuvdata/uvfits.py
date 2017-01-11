0044013abf3f84a42d90afae037dd220c2f4d4c4
Bryna Hazelton <bryna.hazelton@gmail.com>
2017-01-11 09:50:50 -0800

    bug fix: make integration_time in read_uvfits be in seconds

--- pyuvdata/uvfits.py
+++ pyuvdata/uvfits.py
@@ -159,6 +159,7 @@ class UVFITS(UVData):
                 self.integration_time = \
                     float(np.diff(np.sort(list(set(self.time_array))))[0]) * 86400
 
+
             else:
                 raise ValueError('integration time not specified and only '
                                  'one time present')
