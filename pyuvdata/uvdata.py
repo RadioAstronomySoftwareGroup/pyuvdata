ffacaaed492a95fe40e2c7cca0bf12d14dd09f80
aelanman <adam_lanman@brown.edu>
2017-03-05 01:38:09 -0500

    Faster set_lsts_from_time_array and improvements to read_miriad

--- pyuvdata/uvdata.py
+++ pyuvdata/uvdata.py
@@ -460,13 +460,11 @@ class UVData(UVBase):
         """Set the lst_array based from the time_array."""
         lsts = []
         curtime = self.time_array[0]
-        for ind, jd in enumerate(self.time_array):
-            if ind == 0 or not np.isclose(jd, curtime, atol=1e-6, rtol=1e-12):
-                curtime = jd
-                latitude, longitude, altitude = self.telescope_location_lat_lon_alt_degrees
-                t = Time(jd, format='jd', location=(longitude, latitude))
-            lsts.append(t.sidereal_time('apparent').radian)
-        self.lst_array = np.array(lsts)
+
+        latitude, longitude, altitude = self.telescope_location_lat_lon_alt_degrees
+        for ind, jd in enumerate(np.unique(self.time_array)):
+            t = Time(jd, format='jd', location=(longitude,latitude))
+            self.lst_array[np.where(np.isclose(jd, self.time_array,atol=1e-6, rtol=1e-12))] = t.sidereal_time('apparent').radian
 
     def juldate2ephem(self, num):
         """
