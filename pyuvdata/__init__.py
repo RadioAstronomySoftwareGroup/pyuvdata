fffdcb0a790a4dcc3492642ca3a3d26d99356aec
Adam Beardsley <adam.p.beardsley@gmail.com>
2017-03-20 11:46:41 -0700

    Adding version and git info in setup

--- pyuvdata/__init__.py
+++ pyuvdata/__init__.py
@@ -9,3 +9,13 @@ from .fhd import *
 from .miriad import *
 from .uvcal import *
 from .calfits import *
+
+try:
+    from .version import __version__
+    from .version import git_origin
+    from .version import git_hash
+except ImportError:
+    # TODO: Issue a warning using the logging framework
+    __version__ = ''
+    git_origin = ''
+    git_hash = ''
