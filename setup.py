5f6e7795172819d4bd4263d5cad0b7bc7e6e3804
Bryna Hazelton <brynah@phys.washington.edu>
2017-03-31 11:53:38 -0700

    An alternative approach to getting the version programmatically

--- setup.py
+++ setup.py
@@ -4,34 +4,6 @@ import os.path as op
 from os import listdir
 import subprocess
 
-print "Generating pyuvdata/__version__.py: ",
-try:
-    __version__ = open('VERSION').read().strip()
-except:
-    __version__ = ''
-try:
-    git_origin = subprocess.check_output(['git', 'config', '--get', 'remote.origin.url'],
-                                         stderr=subprocess.STDOUT).strip()
-    git_hash = subprocess.check_output(['git', 'rev-parse', 'HEAD'],
-                                       stderr=subprocess.STDOUT).strip()
-    git_description = subprocess.check_output(['git', 'describe', '--dirty']).strip()
-    git_branch = subprocess.check_output(['git', 'rev-parse', '--abbrev-ref', 'HEAD'],
-                                         stderr=subprocess.STDOUT).strip()
-    git_version = subprocess.check_output(['git', 'describe', '--abbrev=0']).strip()
-except:
-    git_origin = ''
-    git_hash = ''
-    git_description = ''
-    git_branch = ''
-
-print('Version = {0}'.format(__version__))
-print('git origin = {0}'.format(git_origin))
-print('git description = {0}'.format(git_description))
-version_text = ('version = "{0}"\ngit_origin = "{1}"\ngit_hash = "{2}"\n' +
-                'git_description = "{3}"\ngit_branch = "{4}"'
-                ).format(__version__, git_origin, git_hash, git_description, git_branch)
-open('pyuvdata/version.py', 'w').write(version_text)
-
 setup_args = {
     'name': 'pyuvdata',
     'author': 'HERA Team',
@@ -41,7 +13,7 @@ setup_args = {
     'package_dir': {'pyuvdata': 'pyuvdata', 'uvdata': 'uvdata'},
     'packages': ['pyuvdata', 'uvdata'],
     'scripts': glob.glob('scripts/*'),
-    'version': __version__,
+    'version': open('VERSION').read().strip(),
     'package_data': {'pyuvdata': [f for f in listdir('./pyuvdata/data') if op.isfile(op.join('./pyuvdata/data', f))]},
     'install_requires': ['numpy>=1.10', 'scipy', 'astropy>=1.2', 'pyephem', 'aipy'],
     'classifiers': ['Development Status :: 5 - Production/Stable',
