ce7dbf8196b2c469b24b7d1002257be324783c20
Adam Beardsley <adam.p.beardsley@gmail.com>
2017-06-04 11:31:01 -0700

    Fixed multifile ms read test. Reinstated importuvfits test

--- pyuvdata/tests/test_ms.py
+++ pyuvdata/tests/test_ms.py
@@ -22,7 +22,7 @@ def test_readNRAO():
                     UV.extra_keywords.keys().sort())
     del(UV)
 
-    
+
 def test_noSPW():
     """Test reading in a PAPER ms convertes by CASA from a uvfits with no spw axis."""
     UV=UVData()
@@ -41,7 +41,7 @@ def test_noSPW():
 #    del(UV)
 
 #Need a method to test casacore import error!
-    
+
 
 def test_spwnotsupported():
     """Test errors on reading in an ms file with multiple spws."""
@@ -49,7 +49,7 @@ def test_spwnotsupported():
     testfile=os.path.join(DATA_PATH,'day2_TDEM0003_10s_norx_1scan.ms')
     nt.assert_raises(ValueError,UV.read_ms,testfile)
     del(UV)
-    
+
 
 def test_readMSWriteUVFITS():
     """
@@ -66,11 +66,11 @@ def test_readMSWriteUVFITS():
                          nwarnings=0)
     ms_uv.write_uvfits(testfile,spoof_nonessential=True)
     uvtest.checkWarnings(uvfits_uv.read_uvfits,[testfile],
-                         message='Telescope EVLA is not')                                             
+                         message='Telescope EVLA is not')
     nt.assert_equal(uvfits_uv,ms_uv)
     del(ms_uv)
     del(uvfits_uv)
-    
+
 def test_readMSWriteMiriad():
     """
     read ms, write miriad test.
@@ -92,47 +92,39 @@ def test_readMSWriteMiriad():
     del(miriad_uv)
 
 
-#this test fails because history strings are different. It will be ignored for now.
-#def test_readUVFITS_readMS():
-#    """Test reading in an ms produced by casa importuvfits and compare to original uvfits"""
-#    ms_uv=UVData()
-#    uvfits_uv=UVData()
-#    ms_file=os.path.join(DATA_PATH,'day2_TDEM0003_10s_norx_1src_1spw.ms')
-#    uvfits_file=os.path.join(DATA_PATH,'day2_TDEM0003_10s_norx_1src_1spw.uvfits')
-#    uvfits_status=uvtest.checkWarnings(uvfits_uv.read_uvfits,[uvfits_file],
-#                                       message='Telescope EVLA is not')
-#    ms_status=uvtest.checkWarnings(ms_uv.read_ms,[ms_file,True,True,'DATA','AIPS',True],
-#                                    message='Telescope EVLA is not',
-#                                    nwarnings=0)
-#    nt.assert_true(uvfits_status)
-#    nt.assert_true(ms_status)
-#    nt.assert_equal(ms_uv,uvfits_uv)
-#    del(ms_uv)
-#    del(uvfits_uv)
+def test_readUVFITS_readMS():
+    """
+    Test reading in an ms produced by casa importuvfits and compare to original uvfits
+    """
+    ms_uv = UVData()
+    uvfits_uv = UVData()
+    ms_file = os.path.join(DATA_PATH, 'day2_TDEM0003_10s_norx_1src_1spw.ms')
+    uvfits_file = os.path.join(DATA_PATH, 'day2_TDEM0003_10s_norx_1src_1spw.uvfits')
+    uvtest.checkWarnings(uvfits_uv.read_uvfits, [uvfits_file],
+                         message='Telescope EVLA is not')
+    uvtest.checkWarnings(ms_uv.read_ms, [ms_file, True, True, 'DATA', 'AIPS', True],
+                         message='Telescope EVLA is not',
+                         nwarnings=0)
+    # Casa scrambles the history parameter. Replace for now.
+    ms_uv.history = uvfits_uv.history
+    nt.assert_equal(ms_uv, uvfits_uv)
+    del(ms_uv)
+    del(uvfits_uv)
 
-# to fix this test we need split measurement sets to test with.
-# def test_multi_files():
-#     """
-#     Reading multiple files at once.
-#     """
-#     uv_full = UVData()
-#     uvfits_file = os.path.join(DATA_PATH, 'day2_TDEM0003_10s_norx_1src_1spw.uvfits')
-#     testfile1 = os.path.join(DATA_PATH, 'test/uv1')
-#     testfile2 = os.path.join(DATA_PATH, 'test/uv2')
-#     uvtest.checkWarnings(uv_full.read_uvfits, [uvfits_file], message='Telescope EVLA is not')
-#     uv_full.unphase_to_drift()
-#     uv1 = copy.deepcopy(uv_full)
-#     uv2 = copy.deepcopy(uv_full)
-#     uv1.select(freq_chans=np.arange(0, 32))
-#     uv2.select(freq_chans=np.arange(32, 64))
-#     uv1.write_ms(testfile1, clobber=True)
-#     uv2.write_ms(testfile2, clobber=True)
-#     uvtest.checkWarnings(uv1.read_ms, [[testfile1, testfile2]], nwarnings=2,
-#                          category=[UserWarning, UserWarning],
-#                          message=['Telescope EVLA is not', 'Telescope EVLA is not'])
-#     # Check history is correct, before replacing and doing a full object check
-#     nt.assert_equal(uv_full.history + '  Downselected to specific frequencies'
-#                     ' using pyuvdata. Combined data along frequency axis using'
-#                     ' pyuvdata.', uv1.history.replace('\n', ''))
-#     uv1.history = uv_full.history
-#     nt.assert_equal(uv1, uv_full)
+
+def test_multi_files():
+    """
+    Reading multiple files at once.
+    """
+    uv_full = UVData()
+    uv_multi = UVData()
+    uvfits_file = os.path.join(DATA_PATH, 'day2_TDEM0003_10s_norx_1src_1spw.uvfits')
+    uvtest.checkWarnings(uv_full.read_uvfits, [uvfits_file], message='Telescope EVLA is not')
+    testfile1 = os.path.join(DATA_PATH, 'multi_1.ms')
+    testfile2 = os.path.join(DATA_PATH, 'multi_2.ms')
+    uv_multi.read_ms([testfile1, testfile2])
+    # Casa scrambles the history parameter. Replace for now.
+    uv_multi.history = uv_full.history
+    nt.assert_equal(uv_multi, uv_full)
+    del(uv_full)
+    del(uv_multi)
