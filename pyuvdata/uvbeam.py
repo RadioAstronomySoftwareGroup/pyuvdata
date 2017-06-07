86cd53a48b8254c040ae41b4ad72a5c6eddc7e1f
Bryna Hazelton <brynah@phys.washington.edu>
2017-06-06 19:01:13 -0700

    Add basic uvbeam testing

--- pyuvdata/uvbeam.py
+++ pyuvdata/uvbeam.py
@@ -2,6 +2,7 @@
 import numpy as np
 from uvbase import UVBase
 import parameter as uvp
+import version as uvversion
 
 
 class UVBeam(UVBase):
@@ -197,6 +198,15 @@ class UVBeam(UVBase):
                                              form=(4, 'Nspws', 'Nfreqs'),
                                              expected_type=np.float,
                                              tols=1e-3)
+
+        # String to add to history of any files written with this version of pyuvdata
+        self.pyuvdata_version_str = ('  Read/written with pyuvdata version: ' +
+                                     uvversion.version + '.')
+        if uvversion.git_hash is not '':
+            self.pyuvdata_version_str += ('  Git origin: ' + uvversion.git_origin +
+                                          '.  Git hash: ' + uvversion.git_hash +
+                                          '.  Git branch: ' + uvversion.git_branch +
+                                          '.  Git description: ' + uvversion.git_description + '.')
 
         super(UVBeam, self).__init__()
 
