e2e256b048f2e9b82834cd6de13d0795c6db107f
Bryna Hazelton <bryna.hazelton@gmail.com>
2017-06-15 13:47:02 -0700

    Fix to string comparison (newly discovered with this branch name)

--- pyuvdata/parameter.py
+++ pyuvdata/parameter.py
@@ -122,7 +122,8 @@ class UVParameter(object):
                             return False
                     else:
                         if self.value.strip() != other.value.strip():
-                            if self.value.replace('\n', '') != other.value.replace('\n', ''):
+                            if (self.value.replace('\n', '').replace(' ', '') !=
+                                    other.value.replace('\n', '').replace(' ', '')):
                                 print('{name} parameter value is a string (not '
                                       'a list), values are different'.format(name=self.name))
                                 return False
