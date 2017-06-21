c293302c290aef9915796a40f5cc43b2eec5f634
Bryna Hazelton <bryna.hazelton@gmail.com>
2017-06-20 17:38:15 -0700

    major rework of beams to better support regular pixel grids

--- pyuvdata/uvbeam.py
+++ pyuvdata/uvbeam.py
@@ -16,65 +16,117 @@ class UVBeam(UVBase):
             and others are always optional.
     """
 
-    coordinate_system_dict = {'az_za': {'naxes': 2, 'axis_list': ['az', 'za']},
-                              'ecef': {'naxes': 3, 'axis_list': ['x', 'y', 'z']}}
+    coordinate_system_dict = {
+        # uniformly gridded az/za coordinate system, az runs from East to North
+        'az_za': ['az', 'za'],
+        # sine projection at zenith. y points North, x point East
+        'sin_zenith': ['sin_x', 'sin_y'],
+        # HEALPix map with zenith at north pole.
+        'healpix': ['hpx_inds']}
 
     def __init__(self):
         """Create a new UVBeam object."""
         # add the UVParameters to the class
-
         self._Nfreqs = uvp.UVParameter('Nfreqs', description='Number of frequency channels',
                                        expected_type=int)
-
-        self._Npixels = uvp.UVParameter('Npixels', description='Number of pixels '
-                                        'with beam model values',
-                                        expected_type=int)
-
-        self._Nfeeds = uvp.UVParameter('Nfeeds', description='Number of feeds',
-                                       expected_type=int, acceptable_vals=[1, 2])
-
-        self._Naxes_pix = uvp.UVParameter('Naxes_pix', description='Number of directions '
-                                          'in pixel_coordinate_system, options are 2 or 3',
-                                          expected_type=int, acceptable_vals=[2, 3])
-
-        self._Naxes_vec = uvp.UVParameter('Naxes_vec', description='Number of directions '
-                                          'in vector_coordinate_system, options are 2 or 3',
-                                          expected_type=int, acceptable_vals=[2, 3])
 
         self._Nspws = uvp.UVParameter('Nspws', description='Number of spectral windows '
                                       '(ie non-contiguous spectral chunks). '
                                       'More than one spectral window is not '
                                       'currently supported.', expected_type=int)
 
-        desc = 'Pixel coordinate system, options are: ' + ', '.join(self.coordinate_system_dict.keys())
+        desc = ('Number of directions in vector_coordinate_system, options '
+                'are 2 or 3 (or 1 if beam_type is "power")')
+        self._Naxes_vec = uvp.UVParameter('Naxes_vec', description=desc,
+                                          expected_type=int, acceptable_vals=[2, 3])
+
+        desc = ('Pixel coordinate system, options are: '
+                ', '.join(self.coordinate_system_dict.keys()))
         self._pixel_coordinate_system = uvp.UVParameter('pixel_coordinate_system',
                                                         description=desc, form='str',
                                                         expected_type=str,
                                                         acceptable_vals=self.coordinate_system_dict.keys())
 
-        desc = 'Array of pixel locations, shape: (Naxes_pix, Npixels)'
-        self._pixel_location_array = uvp.UVParameter('pixel_location_array',
-                                                     description=desc,
-                                                     form=('Naxes_pix', 'Npixels'),
-                                                     expected_type=np.float)
+        desc = ('Number of elements along the first pixel axis. '
+                'Not required if pixel_coordinate_system is "healpix".')
+        self._Naxes1 = uvp.UVParameter('Naxes1', description=desc, expected_type=int,
+                                       required=False)
+
+        desc = ('Coordinates along first pixel axis. '
+                'Not required if pixel_coordinate_system is "healpix".')
+        self._axis1_array = uvp.UVParameter('axis1_array', description=desc,
+                                            expected_type=np.float,
+                                            required=False, form=('Naxes1',))
+
+        desc = ('Number of elements along the second pixel axis. '
+                'Not required if pixel_coordinate_system is "healpix".')
+        self._Naxes2 = uvp.UVParameter('Naxes2', description=desc, expected_type=int,
+                                       required=False)
+
+        desc = ('Coordinates along second pixel axis. '
+                'Not required if pixel_coordinate_system is "healpix".')
+        self._axis2_array = uvp.UVParameter('axis2_array', description=desc,
+                                            expected_type=np.float,
+                                            required=False, form=('Naxes2',))
+
+        desc = ('Healpix nside parameter. Only required if pixel_coordinate_system is "healpix".')
+        self._nside = uvp.UVParameter('nside', description=desc, expected_type=int,
+                                      required=False)
+
+        desc = ('Healpix ordering parameter, allowed values are "ring" and "nested". '
+                'Only required if pixel_coordinate_system is "healpix".')
+        self._ordering = uvp.UVParameter('ordering', description=desc, expected_type=int,
+                                         required=False)
+
+        desc = ('Number of healpix pixels. Only required if pixel_coordinate_system is "healpix".')
+        self._Npixels = uvp.UVParameter('Npixels', description=desc, expected_type=int,
+                                        required=False)
+
+        desc = ('Healpix pixel numbers. Only required if pixel_coordinate_system is "healpix".')
+        self._pixel_array = uvp.UVParameter('pixel_array', description=desc, expected_type=int,
+                                            required=False, form=('Npixels',))
+
+        desc = 'String indicating beam type. Allowed values are "efield", and "power".'
+        self._beam_type = uvp.UVParameter('beam_type', description=desc, form='str',
+                                          expected_type=str,
+                                          acceptable_vals=['efield', 'power'])
 
         desc = ('Beam basis vector components -- directions for which the '
                 'electric field values are recorded in the pixel coordinate system. '
-                'If the components are aligned with the pixel coordinate system '
-                'all the values will be 1 or 0. shape: (Naxes_vec, Naxes_pix, Npixels).')
+                'Not required if beam_type is "power". The shape depends on the '
+                'pixel_coordinate_system, if it is "healpix", the shape is: '
+                '(Naxes_vec, 2, Npixels), otherwise it is (Naxes_vec, 2, Naxes1, Naxes2)')
         self._basis_vector_array = uvp.UVParameter('basis_vector_array',
-                                                   description=desc,
+                                                   description=desc, required=False,
                                                    expected_type=np.float,
-                                                   form=('Naxes_vec', 'Naxes_pix', 'Npixels'),
+                                                   form=('Naxes_vec', 2, 'Naxes2', 'Naxes1'),
                                                    acceptable_range=(0, 1),
                                                    tols=1e-3)
 
+        self._Nfeeds = uvp.UVParameter('Nfeeds', description='Number of feeds. '
+                                       'Not required if beam_type is "power".',
+                                       expected_type=int, acceptable_vals=[1, 2],
+                                       required=False)
+
         desc = ('Array of feed orientations. shape (Nfeeds). '
-                'options are: N/E or x/y or R/L')
-        self._feed_array = uvp.UVParameter('feed_array', description=desc,
+                'options are: N/E or x/y or R/L. Not required if beam_type is "power".')
+        self._feed_array = uvp.UVParameter('feed_array', description=desc, required=False,
                                            expected_type=str, form=('Nfeeds',),
                                            acceptable_vals=['N', 'E', 'x', 'y',
                                                             'R', 'L'])
+
+        self._Npols = uvp.UVParameter('Npols', description='Number of polarizations. '
+                                      'Only required if beam_type is "power".',
+                                      expected_type=int, required=False)
+
+        desc = ('Array of polarization integers, shape (Npols). '
+                'AIPS Memo 117 says: stokes 1:4 (I,Q,U,V);  '
+                'circular -1:-4 (RR,LL,RL,LR); linear -5:-8 (XX,YY,XY,YX)'
+                'Only required if beam_type is "power".')
+        self._polarization_array = uvp.UVParameter('polarization_array',
+                                                   description=desc, required=False,
+                                                   expected_type=int, form=('Npols',),
+                                                   acceptable_vals=list(np.arange(-8, 0)) + list(np.arange(1, 5)))
 
         desc = 'Array of frequencies, shape (Nspws, Nfreqs), units Hz'
         self._freq_array = uvp.UVParameter('freq_array', description=desc,
@@ -87,20 +139,23 @@ class UVBeam(UVBase):
                                           'Numbers, shape (Nspws)', form=('Nspws',),
                                           expected_type=int)
 
-        desc = 'Efield units, options are: "peak", "solid_angle"'
-        self._efield_units = uvp.UVParameter('efield_units', description=desc,
-                                             form='str', expected_type=str,
-                                             acceptable_vals=["peak", "solid_angle"])
+        desc = 'Normalization standard of data_array, options are: "peak", "solid_angle"'
+        self._data_normalization = uvp.UVParameter('data_normalization', description=desc,
+                                                   form='str', expected_type=str,
+                                                   acceptable_vals=["peak", "solid_angle"])
 
-        desc = ('Complex E-field values for beam model. units are linear '
-                'normalized to either peak or solid angle as given by efield_units. '
-                'shape = (Nfeeds, Naxes_vec, Npixels, Nspws, Nfreqs)')
-        self._efield_array = uvp.UVParameter('efield_array',
-                                             description=desc,
-                                             expected_type=np.complex,
-                                             form=('Nfeeds', 'Naxes_vec', 'Npixels', 'Nspws', 'Nfreqs'),
-                                             acceptable_range=(0, 1),
-                                             tols=1e-3)
+        desc = ('Depending on beam type, either complex E-field values ("efield" beam type) '
+                'or power values ("power" beam type) for beam model. units are linear '
+                'normalized to either peak or solid angle as given by data_normalization. '
+                'The shape depends on the beam_type and pixel_coordinate_system, if it is '
+                '"healpix", the shape is: (Naxes_vec, Nspws, Nfeeds or Npols, Nfreqs, Npixels), '
+                'otherwise it is (Naxes_vec, Nspws, Nfeeds or Npols, Nfreqs, Naxes2, Naxes1)')
+        self._data_array = uvp.UVParameter('data_array', description=desc,
+                                           expected_type=np.complex,
+                                           form=('Naxes_vec', 'Nspws', 'Nfeeds',
+                                                 'Nfreqs', 'Naxes2', 'Naxes1'),
+                                           acceptable_range=(0, 1),
+                                           tols=1e-3)
 
         # --------- metadata -------------
         self._telescope_name = uvp.UVParameter('telescope_name',
@@ -237,14 +292,77 @@ class UVBeam(UVBase):
             run_check_acceptability: Option to check if values in required parameters
                 are acceptable. Default is True.
         """
+        # first make sure the required parameters and forms are set properly
+        # for the pixel_coordinate_system
+        self.set_cs_params()
+
         # first run the basic check from UVBase
         super(UVBeam, self).check(run_check_acceptability=run_check_acceptability)
 
-        # Check consistency of coordinate_system and Naxes_pix
-        if self.Naxes_pix != self.coordinate_system_dict[self.pixel_coordinate_system]['naxes']:
-            raise ValueError('Number of coordinate axes is not consistent with coordinate_system.')
-
         return True
+
+    def set_cs_params(self):
+        """
+        Set various forms and required parameters depending on pixel_coordinate_system.
+        """
+        if self.pixel_coordinate_system == 'healpix':
+            self._Naxes1.required = False
+            self._axis1_array.required = False
+            self._Naxes2.required = False
+            self._axis2_array.required = False
+            self._nside.required = True
+            self._ordering.required = True
+            self._Npixels.required = True
+            self._pixel_array.required = True
+            self._basis_vector_array.form = ('Naxes_vec', 2, 'Npixels')
+            if self.beam_type == "power":
+                self._data_array.form = ('Naxes_vec', 'Nspws', 'Npols', 'Nfreqs',
+                                         'Npixels')
+            else:
+                self._data_array.form = ('Naxes_vec', 'Nspws', 'Nfeeds', 'Nfreqs',
+                                         'Npixels')
+        else:
+            self._Naxes1.required = True
+            self._axis1_array.required = True
+            self._Naxes2.required = True
+            self._axis2_array.required = True
+            self._nside.required = False
+            self._ordering.required = False
+            self._Npixels.required = False
+            self._pixel_array.required = False
+            self._basis_vector_array.form = ('Naxes_vec', 2, 'Naxes2', 'Naxes1')
+            if self.beam_type == "power":
+                self._data_array.form = ('Naxes_vec', 'Nspws', 'Npols', 'Nfreqs',
+                                         'Naxes2', 'Naxes1')
+            else:
+                self._data_array.form = ('Naxes_vec', 'Nspws', 'Nfeeds', 'Nfreqs',
+                                         'Naxes2', 'Naxes1')
+
+    def set_efield(self):
+        """Set beam_type to 'efield' and adjust required parameters."""
+        self.beam_type = 'efield'
+        self._Naxes_vec.acceptable_vals = [2, 3]
+        self._basis_vector_array.required = True
+        self._Nfeeds.required = True
+        self._feed_array.required = True
+        self._Npols.required = False
+        self._polarization_array.required = False
+        self._data_array.expected_type = np.complex
+        # call set_cs_params to fix data_array form
+        self.set_cs_params()
+
+    def set_power(self):
+        """Set beam_type to 'power' and adjust required parameters."""
+        self.beam_type = 'power'
+        self._Naxes_vec.acceptable_vals = [1, 2, 3]
+        self._basis_vector_array.required = False
+        self._Nfeeds.required = False
+        self._feed_array.required = False
+        self._Npols.required = True
+        self._polarization_array.required = True
+        self._data_array.expected_type = np.float
+        # call set_cs_params to fix data_array form
+        self.set_cs_params()
 
     def set_simple(self):
         """Set antenna_type to 'simple' and adjust required parameters."""
