63cc4f7702709dc266609f2650a855f6bba4a0d3
Bryna Hazelton <bryna.hazelton@gmail.com>
2017-06-21 17:23:25 -0700

    improve testing coverage

--- pyuvdata/tests/test_beamfits.py
+++ pyuvdata/tests/test_beamfits.py
@@ -2,9 +2,12 @@
 import nose.tools as nt
 import os
 import numpy as np
+import astropy
+from astropy.io import fits
 from pyuvdata import UVBeam
 from pyuvdata.data import DATA_PATH
 import pyuvdata.version as uvversion
+import pyuvdata.utils as uvutils
 
 
 def fill_dummy_beam(beam_obj, beam_type):
@@ -90,11 +93,58 @@ def test_writeread():
 
 def test_errors():
     beam_in = UVBeam()
+    beam_out = UVBeam()
     beam_in = fill_dummy_beam(beam_in, 'efield')
     beam_in.beam_type = 'foo'
 
     write_file = os.path.join(DATA_PATH, 'test/outtest_beam.fits')
     nt.assert_raises(ValueError, beam_in.write_beamfits, write_file, clobber=True)
+
+    # now change values for various items in basisvec hdu to not match primary hdu
+    beam_in.beam_type = 'efield'
+
+    header_vals_to_change = [{'COORDSYS': 'foo'}, {'CTYPE1': 'foo'},
+                             {'CTYPE2': 'foo'},
+                             {'CDELT1': np.diff(beam_in.axis1_array)[0] * 2},
+                             {'CDELT2': np.diff(beam_in.axis2_array)[0] * 2},
+                             {'NAXIS4': ''}]
+
+    for i, hdr_dict in enumerate(header_vals_to_change):
+        beam_in.write_beamfits(write_file, clobber=True)
+
+        keyword = hdr_dict.keys()[0]
+        new_val = hdr_dict[keyword]
+        F = fits.open(write_file)
+        data = F[0].data
+        primary_hdr = F[0].header
+        hdunames = uvutils.fits_indexhdus(F)
+        basisvec_hdu = F[hdunames['BASISVEC']]
+        basisvec_hdr = basisvec_hdu.header
+        basisvec_data = basisvec_hdu.data
+
+        if 'NAXIS' in keyword:
+            ax_num = keyword.split('NAXIS')[1]
+            if ax_num != '':
+                ax_num = int(ax_num)
+                ax_use = len(basisvec_data.shape) - ax_num
+                new_arrays = np.split(basisvec_data, basisvec_hdr[keyword], axis=ax_use)
+                basisvec_data = new_arrays[0]
+            else:
+                basisvec_data = np.split(basisvec_data, basisvec_hdr['NAXIS1'],
+                                         axis=len(basisvec_data.shape) - 1)[0]
+        else:
+            basisvec_hdr[keyword] = new_val
+
+        prihdu = fits.PrimaryHDU(data=data, header=primary_hdr)
+        basisvec_hdu = fits.ImageHDU(data=basisvec_data, header=basisvec_hdr)
+        hdulist = fits.HDUList([prihdu, basisvec_hdu])
+
+        if float(astropy.__version__[0:3]) < 1.3:
+            hdulist.writeto(write_file, clobber=True)
+        else:
+            hdulist.writeto(write_file, overwrite=True)
+
+        nt.assert_raises(ValueError, beam_out.read_beamfits, write_file)
 
 
 def test_casa_beam():
@@ -113,7 +163,7 @@ def test_casa_beam():
     beam_in.model_version = 'v0'
 
     # this file is actually in sine projection RA/DEC at zenith at a particular time.
-    # For now pretend it's in sin_zenith
+    # For now pretend it's in sine projection of az/za
     beam_in.pixel_coordinate_system = 'sin_zenith'
 
     beam_in.write_beamfits(write_file, clobber=True)
