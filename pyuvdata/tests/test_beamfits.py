698bbc34820d16e3b3214e2a99295abbf3152d63
Bryna Hazelton <bryna.hazelton@gmail.com>
2017-06-22 11:08:48 -0700

    Add select to uvbeam, with testing

--- pyuvdata/tests/test_beamfits.py
+++ pyuvdata/tests/test_beamfits.py
@@ -6,67 +6,8 @@ import astropy
 from astropy.io import fits
 from pyuvdata import UVBeam
 from pyuvdata.data import DATA_PATH
-import pyuvdata.version as uvversion
 import pyuvdata.utils as uvutils
-
-
-def fill_dummy_beam(beam_obj, beam_type):
-    beam_obj.set_simple()
-    beam_obj.telescope_name = 'testscope'
-    beam_obj.feed_name = 'testfeed'
-    beam_obj.feed_version = '0.1'
-    beam_obj.model_name = 'testmodel'
-    beam_obj.model_version = '1.0'
-    beam_obj.history = 'random data for test'
-
-    pyuvdata_version_str = ('  Read/written with pyuvdata version: ' +
-                            uvversion.version + '.')
-    if uvversion.git_hash is not '':
-        pyuvdata_version_str += ('  Git origin: ' + uvversion.git_origin +
-                                 '.  Git hash: ' + uvversion.git_hash +
-                                 '.  Git branch: ' + uvversion.git_branch +
-                                 '.  Git description: ' + uvversion.git_description + '.')
-    beam_obj.history += pyuvdata_version_str
-
-    beam_obj.pixel_coordinate_system = 'az_za'
-    beam_obj.axis1_array = np.arange(-180.0, 180.0, 5.0)
-    beam_obj.Naxes1 = len(beam_obj.axis1_array)
-    beam_obj.axis2_array = np.arange(-90.0, 90.0, 5.0)
-    beam_obj.Naxes2 = len(beam_obj.axis2_array)
-
-    beam_obj.freq_array = np.arange(150e6, 160e6, 1e6)
-    beam_obj.freq_array = beam_obj.freq_array[np.newaxis, :]
-    beam_obj.Nfreqs = beam_obj.freq_array.shape[1]
-    beam_obj.spw_array = np.array([0])
-    beam_obj.Nspws = len(beam_obj.spw_array)
-    beam_obj.data_normalization = 'peak'
-
-    if beam_type == 'power':
-        beam_obj.set_power()
-        beam_obj.polarization_array = np.array([-5, -6, -7, -8])
-        beam_obj.Npols = len(beam_obj.polarization_array)
-        beam_obj.Naxes_vec = 1
-
-        data_size_tuple = (beam_obj.Naxes_vec, beam_obj.Nspws, beam_obj.Npols,
-                           beam_obj.Nfreqs, beam_obj.Naxes2, beam_obj.Naxes1)
-        beam_obj.data_array = np.square(np.random.normal(0.0, 0.2, size=data_size_tuple))
-    else:
-        beam_obj.set_efield()
-        beam_obj.feed_array = ['x', 'y']
-        beam_obj.Nfeeds = len(beam_obj.feed_array)
-        beam_obj.Naxes_vec = 2
-        beam_obj.basis_vector_array = np.zeros((beam_obj.Naxes_vec, 2, beam_obj.Naxes2, beam_obj.Naxes1))
-        beam_obj.basis_vector_array[0, 0, :, :] = 1.0
-        beam_obj.basis_vector_array[1, 1, :, :] = 1.0
-
-        data_size_tuple = (beam_obj.Naxes_vec, beam_obj.Nspws, beam_obj.Nfeeds,
-                           beam_obj.Nfreqs, beam_obj.Naxes2, beam_obj.Naxes1)
-        beam_obj.data_array = (np.random.normal(0.0, 0.2, size=data_size_tuple) +
-                               1j * np.random.normal(0.0, 0.2, size=data_size_tuple))
-
-    beam_obj.extra_keywords = {'KEY1': 'test_keyword'}
-
-    return beam_obj
+from .test_uvbeam import fill_dummy_beam
 
 
 def test_writeread():
--- pyuvdata/tests/test_uvbeam.py
+++ pyuvdata/tests/test_uvbeam.py
@@ -1,7 +1,71 @@
 """Tests for uvbeam object."""
 import nose.tools as nt
+import os
 import numpy as np
+import copy
 from pyuvdata import UVBeam
+import pyuvdata.tests as uvtest
+import pyuvdata.version as uvversion
+from pyuvdata.data import DATA_PATH
+
+
+def fill_dummy_beam(beam_obj, beam_type):
+    beam_obj.set_simple()
+    beam_obj.telescope_name = 'testscope'
+    beam_obj.feed_name = 'testfeed'
+    beam_obj.feed_version = '0.1'
+    beam_obj.model_name = 'testmodel'
+    beam_obj.model_version = '1.0'
+    beam_obj.history = 'random data for test'
+
+    pyuvdata_version_str = ('  Read/written with pyuvdata version: ' +
+                            uvversion.version + '.')
+    if uvversion.git_hash is not '':
+        pyuvdata_version_str += ('  Git origin: ' + uvversion.git_origin +
+                                 '.  Git hash: ' + uvversion.git_hash +
+                                 '.  Git branch: ' + uvversion.git_branch +
+                                 '.  Git description: ' + uvversion.git_description + '.')
+    beam_obj.history += pyuvdata_version_str
+
+    beam_obj.pixel_coordinate_system = 'az_za'
+    beam_obj.axis1_array = np.arange(-180.0, 180.0, 5.0)
+    beam_obj.Naxes1 = len(beam_obj.axis1_array)
+    beam_obj.axis2_array = np.arange(-90.0, 90.0, 5.0)
+    beam_obj.Naxes2 = len(beam_obj.axis2_array)
+
+    beam_obj.freq_array = np.arange(150e6, 160e6, 1e5)
+    beam_obj.freq_array = beam_obj.freq_array[np.newaxis, :]
+    beam_obj.Nfreqs = beam_obj.freq_array.shape[1]
+    beam_obj.spw_array = np.array([0])
+    beam_obj.Nspws = len(beam_obj.spw_array)
+    beam_obj.data_normalization = 'peak'
+
+    if beam_type == 'power':
+        beam_obj.set_power()
+        beam_obj.polarization_array = np.array([-5, -6, -7, -8])
+        beam_obj.Npols = len(beam_obj.polarization_array)
+        beam_obj.Naxes_vec = 1
+
+        data_size_tuple = (beam_obj.Naxes_vec, beam_obj.Nspws, beam_obj.Npols,
+                           beam_obj.Nfreqs, beam_obj.Naxes2, beam_obj.Naxes1)
+        beam_obj.data_array = np.square(np.random.normal(0.0, 0.2, size=data_size_tuple))
+    else:
+        beam_obj.set_efield()
+        beam_obj.feed_array = np.array(['x', 'y'])
+        beam_obj.Nfeeds = len(beam_obj.feed_array)
+        beam_obj.Naxes_vec = 2
+        beam_obj.basis_vector_array = np.zeros((beam_obj.Naxes_vec, 2, beam_obj.Naxes2, beam_obj.Naxes1))
+        beam_obj.basis_vector_array[0, 0, :, :] = 1.0
+        beam_obj.basis_vector_array[1, 1, :, :] = 1.0
+
+        data_size_tuple = (beam_obj.Naxes_vec, beam_obj.Nspws, beam_obj.Nfeeds,
+                           beam_obj.Nfreqs, beam_obj.Naxes2, beam_obj.Naxes1)
+        beam_obj.data_array = (np.random.normal(0.0, 0.2, size=data_size_tuple) +
+                               1j * np.random.normal(0.0, 0.2, size=data_size_tuple))
+
+    beam_obj.extra_keywords = {'KEY1': 'test_keyword'}
+
+    return beam_obj
 
 
 class TestUVBeamInit(object):
@@ -120,3 +184,230 @@ class TestUVBeamInit(object):
 def test_errors():
     beam_obj = UVBeam()
     nt.assert_raises(ValueError, beam_obj._convert_to_filetype, 'foo')
+
+
+class TestUVBeamSelect(object):
+    def setUp(self):
+        """Set up test"""
+        self.power_beam = UVBeam()
+        self.power_beam = fill_dummy_beam(self.power_beam, 'power')
+
+        self.efield_beam = UVBeam()
+        self.efield_beam = fill_dummy_beam(self.efield_beam, 'efield')
+
+    def teardown(self):
+        """Tear down test"""
+        del(self.power_beam)
+        del(self.efield_beam)
+
+    def test_select_axis1(self):
+        old_history = self.power_beam.history
+        inds1_to_keep = np.arange(14, 63)
+
+        self.power_beam2 = self.power_beam.select(axis1_inds=inds1_to_keep, inplace=False)
+
+        nt.assert_equal(len(inds1_to_keep), self.power_beam2.Naxes1)
+        for i in inds1_to_keep:
+            nt.assert_true(self.power_beam.axis1_array[i] in self.power_beam2.axis1_array)
+        for i in np.unique(self.power_beam2.axis1_array):
+            nt.assert_true(i in self.power_beam.axis1_array)
+
+        nt.assert_equal(old_history + '  Downselected to specific parts of '
+                        'first image axis using pyuvdata.', self.power_beam2.history)
+
+        write_file_beamfits = os.path.join(DATA_PATH, 'test/select_beam.fits')
+
+        # test writing beamfits with only one element in axis1
+        inds_to_keep = [len(inds1_to_keep) + 1]
+        self.power_beam2 = self.power_beam.select(axis1_inds=inds_to_keep, inplace=False)
+        self.power_beam2.write_beamfits(write_file_beamfits, clobber=True)
+
+        # check for errors associated with indices not included in data
+        nt.assert_raises(ValueError, self.power_beam2.select, axis1_inds=[self.power_beam.Naxes1 - 1])
+
+        # check for warnings and errors associated with unevenly spaced image pixels
+        self.power_beam2 = copy.deepcopy(self.power_beam)
+        uvtest.checkWarnings(self.power_beam2.select, [], {'axis1_inds': [0, 5, 6]},
+                             message='Selected values along first image axis are not evenly spaced')
+        nt.assert_raises(ValueError, self.power_beam2.write_beamfits, write_file_beamfits)
+
+    def test_select_axis2(self):
+        old_history = self.power_beam.history
+        inds2_to_keep = np.arange(5, 22)
+
+        self.power_beam2 = self.power_beam.select(axis2_inds=inds2_to_keep, inplace=False)
+
+        nt.assert_equal(len(inds2_to_keep), self.power_beam2.Naxes2)
+        for i in inds2_to_keep:
+            nt.assert_true(self.power_beam.axis2_array[i] in self.power_beam2.axis2_array)
+        for i in np.unique(self.power_beam2.axis2_array):
+            nt.assert_true(i in self.power_beam.axis2_array)
+
+        nt.assert_equal(old_history + '  Downselected to specific parts of '
+                        'second image axis using pyuvdata.', self.power_beam2.history)
+
+        write_file_beamfits = os.path.join(DATA_PATH, 'test/select_beam.fits')
+
+        # test writing beamfits with only one element in axis2
+        inds_to_keep = [len(inds2_to_keep) + 1]
+        self.power_beam2 = self.power_beam.select(axis2_inds=inds_to_keep, inplace=False)
+        self.power_beam2.write_beamfits(write_file_beamfits, clobber=True)
+
+        # check for errors associated with indices not included in data
+        nt.assert_raises(ValueError, self.power_beam2.select, axis2_inds=[self.power_beam.Naxes2 - 1])
+
+        # check for warnings and errors associated with unevenly spaced image pixels
+        self.power_beam2 = copy.deepcopy(self.power_beam)
+        uvtest.checkWarnings(self.power_beam2.select, [], {'axis2_inds': [0, 5, 6]},
+                             message='Selected values along second image axis are not evenly spaced')
+        nt.assert_raises(ValueError, self.power_beam2.write_beamfits, write_file_beamfits)
+
+    def test_select_frequencies(self):
+        old_history = self.power_beam.history
+        freqs_to_keep = self.power_beam.freq_array[0, np.arange(7, 94)]
+
+        self.power_beam2 = self.power_beam.select(frequencies=freqs_to_keep, inplace=False)
+
+        nt.assert_equal(len(freqs_to_keep), self.power_beam2.Nfreqs)
+        for f in freqs_to_keep:
+            nt.assert_true(f in self.power_beam2.freq_array)
+        for f in np.unique(self.power_beam2.freq_array):
+            nt.assert_true(f in freqs_to_keep)
+
+        nt.assert_equal(old_history + '  Downselected to specific frequencies '
+                        'using pyuvdata.', self.power_beam2.history)
+
+        write_file_beamfits = os.path.join(DATA_PATH, 'test/select_beam.fits')
+        # test writing beamfits with only one frequency
+
+        freqs_to_keep = self.power_beam.freq_array[0, 51]
+        self.power_beam2 = self.power_beam.select(frequencies=freqs_to_keep, inplace=False)
+        self.power_beam2.write_beamfits(write_file_beamfits, clobber=True)
+
+        # check for errors associated with frequencies not included in data
+        nt.assert_raises(ValueError, self.power_beam.select, frequencies=[np.max(self.power_beam.freq_array) + 10])
+
+        # check for warnings and errors associated with unevenly spaced frequencies
+        self.power_beam2 = copy.deepcopy(self.power_beam)
+        uvtest.checkWarnings(self.power_beam2.select, [], {'frequencies': self.power_beam2.freq_array[0, [0, 5, 6]]},
+                             message='Selected frequencies are not evenly spaced')
+        nt.assert_raises(ValueError, self.power_beam2.write_beamfits, write_file_beamfits)
+
+    def test_select_freq_chans(self):
+        old_history = self.power_beam.history
+        chans_to_keep = np.arange(7, 94)
+
+        self.power_beam2 = self.power_beam.select(freq_chans=chans_to_keep, inplace=False)
+
+        nt.assert_equal(len(chans_to_keep), self.power_beam2.Nfreqs)
+        for chan in chans_to_keep:
+            nt.assert_true(self.power_beam.freq_array[0, chan] in self.power_beam2.freq_array)
+        for f in np.unique(self.power_beam2.freq_array):
+            nt.assert_true(f in self.power_beam.freq_array[0, chans_to_keep])
+
+        nt.assert_equal(old_history + '  Downselected to specific frequencies '
+                        'using pyuvdata.', self.power_beam2.history)
+
+        # Test selecting both channels and frequencies
+        freqs_to_keep = self.power_beam.freq_array[0, np.arange(93, 100)]  # Overlaps with chans
+        all_chans_to_keep = np.arange(7, 100)
+
+        self.power_beam2 = self.power_beam.select(frequencies=freqs_to_keep,
+                                                  freq_chans=chans_to_keep,
+                                                  inplace=False)
+
+        nt.assert_equal(len(all_chans_to_keep), self.power_beam2.Nfreqs)
+        for chan in all_chans_to_keep:
+            nt.assert_true(self.power_beam.freq_array[0, chan] in self.power_beam2.freq_array)
+        for f in np.unique(self.power_beam2.freq_array):
+            nt.assert_true(f in self.power_beam.freq_array[0, all_chans_to_keep])
+
+    def test_select_feeds(self):
+
+        old_history = self.efield_beam.history
+        feeds_to_keep = ['x']
+
+        self.efield_beam2 = self.efield_beam.select(feeds=feeds_to_keep,
+                                                    inplace=False)
+
+        nt.assert_equal(len(feeds_to_keep), self.efield_beam2.Nfeeds)
+        for f in feeds_to_keep:
+            nt.assert_true(f in self.efield_beam2.feed_array)
+        for f in np.unique(self.efield_beam2.feed_array):
+            nt.assert_true(f in feeds_to_keep)
+
+        nt.assert_equal(old_history + '  Downselected to specific feeds '
+                        'using pyuvdata.', self.efield_beam2.history)
+
+        # check for errors associated with feeds not included in data
+        nt.assert_raises(ValueError, self.efield_beam2.select, feeds=['N'])
+
+    def test_select_polarizations(self):
+
+        old_history = self.power_beam.history
+        pols_to_keep = [-5, -6]
+
+        self.power_beam2 = self.power_beam.select(polarizations=pols_to_keep,
+                                                  inplace=False)
+
+        nt.assert_equal(len(pols_to_keep), self.power_beam2.Npols)
+        for p in pols_to_keep:
+            nt.assert_true(p in self.power_beam2.polarization_array)
+        for p in np.unique(self.power_beam2.polarization_array):
+            nt.assert_true(p in pols_to_keep)
+
+        nt.assert_equal(old_history + '  Downselected to specific polarizations '
+                        'using pyuvdata.', self.power_beam2.history)
+
+        # check for errors associated with polarizations not included in data
+        nt.assert_raises(ValueError, self.power_beam2.select, polarizations=[-3, -4])
+
+        # check for warnings and errors associated with unevenly spaced polarizations
+        uvtest.checkWarnings(self.power_beam.select, [], {'polarizations': self.power_beam.polarization_array[[0, 1, 3]]},
+                             message='Selected polarizations are not evenly spaced')
+        write_file_beamfits = os.path.join(DATA_PATH, 'test/select_beam.fits')
+        nt.assert_raises(ValueError, self.power_beam.write_beamfits, write_file_beamfits)
+
+    def test_select(self):
+        # now test selecting along all axes at once
+        old_history = self.power_beam.history
+
+        inds1_to_keep = np.arange(14, 63)
+        inds2_to_keep = np.arange(5, 22)
+        freqs_to_keep = self.power_beam.freq_array[0, np.arange(31, 56)]
+        pols_to_keep = [-5]
+
+        self.power_beam2 = self.power_beam.select(axis1_inds=inds1_to_keep,
+                                                  axis2_inds=inds2_to_keep,
+                                                  frequencies=freqs_to_keep,
+                                                  polarizations=pols_to_keep,
+                                                  inplace=False)
+
+        nt.assert_equal(len(inds1_to_keep), self.power_beam2.Naxes1)
+        for i in inds1_to_keep:
+            nt.assert_true(self.power_beam.axis1_array[i] in self.power_beam2.axis1_array)
+        for i in np.unique(self.power_beam2.axis1_array):
+            nt.assert_true(i in self.power_beam.axis1_array)
+
+        nt.assert_equal(len(inds2_to_keep), self.power_beam2.Naxes2)
+        for i in inds2_to_keep:
+            nt.assert_true(self.power_beam.axis2_array[i] in self.power_beam2.axis2_array)
+        for i in np.unique(self.power_beam2.axis2_array):
+            nt.assert_true(i in self.power_beam.axis2_array)
+
+        nt.assert_equal(len(freqs_to_keep), self.power_beam2.Nfreqs)
+        for f in freqs_to_keep:
+            nt.assert_true(f in self.power_beam2.freq_array)
+        for f in np.unique(self.power_beam2.freq_array):
+            nt.assert_true(f in freqs_to_keep)
+
+        nt.assert_equal(len(pols_to_keep), self.power_beam2.Npols)
+        for p in pols_to_keep:
+            nt.assert_true(p in self.power_beam2.polarization_array)
+        for p in np.unique(self.power_beam2.polarization_array):
+            nt.assert_true(p in pols_to_keep)
+
+        nt.assert_equal(old_history + '  Downselected to specific parts of '
+                        'first image axis, parts of second image axis, '
+                        'frequencies, polarizations using pyuvdata.',
+                        self.power_beam2.history)
