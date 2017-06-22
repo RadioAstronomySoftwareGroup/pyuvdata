ecc0887520b2509a76340dc72247a8e14b819020
Bryna Hazelton <bryna.hazelton@gmail.com>
2017-06-22 13:17:38 -0700

    improve test coverage for beamfits

--- pyuvdata/tests/test_beamfits.py
+++ pyuvdata/tests/test_beamfits.py
@@ -31,6 +31,44 @@ def test_writeread():
 
     nt.assert_equal(beam_in, beam_out)
 
+    # now replace 'power' with 'intensity' for btype
+    F = fits.open(write_file)
+    data = F[0].data
+    primary_hdr = F[0].header
+    primary_hdr['BTYPE'] = 'Intensity'
+    hdunames = uvutils.fits_indexhdus(F)
+    basisvec_hdu = F[hdunames['BASISVEC']]
+
+    prihdu = fits.PrimaryHDU(data=data, header=primary_hdr)
+    hdulist = fits.HDUList([prihdu, basisvec_hdu])
+
+    if float(astropy.__version__[0:3]) < 1.3:
+        hdulist.writeto(write_file, clobber=True)
+    else:
+        hdulist.writeto(write_file, overwrite=True)
+
+    beam_out.read_beamfits(write_file)
+    nt.assert_equal(beam_in, beam_out)
+
+    # now remove coordsys but leave ctypes 1 & 2
+    F = fits.open(write_file)
+    data = F[0].data
+    primary_hdr = F[0].header
+    primary_hdr.pop('COORDSYS')
+    hdunames = uvutils.fits_indexhdus(F)
+    basisvec_hdu = F[hdunames['BASISVEC']]
+
+    prihdu = fits.PrimaryHDU(data=data, header=primary_hdr)
+    hdulist = fits.HDUList([prihdu, basisvec_hdu])
+
+    if float(astropy.__version__[0:3]) < 1.3:
+        hdulist.writeto(write_file, clobber=True)
+    else:
+        hdulist.writeto(write_file, overwrite=True)
+
+    beam_out.read_beamfits(write_file)
+    nt.assert_equal(beam_in, beam_out)
+
 
 def test_errors():
     beam_in = UVBeam()
@@ -40,9 +78,51 @@ def test_errors():
 
     write_file = os.path.join(DATA_PATH, 'test/outtest_beam.fits')
     nt.assert_raises(ValueError, beam_in.write_beamfits, write_file, clobber=True)
+    nt.assert_raises(ValueError, beam_in.write_beamfits, write_file,
+                     clobber=True, run_check=False)
+
+    # now change values for various items in primary hdu to test errors
+    beam_in = fill_dummy_beam(beam_in, 'efield')
+
+    header_vals_to_change = [{'BTYPE': 'foo'}, {'COORDSYS': 'sin_zenith'},
+                             {'NAXIS': ''}]
+
+    for i, hdr_dict in enumerate(header_vals_to_change):
+        beam_in.write_beamfits(write_file, clobber=True)
+
+        keyword = hdr_dict.keys()[0]
+        new_val = hdr_dict[keyword]
+        F = fits.open(write_file)
+        data = F[0].data
+        primary_hdr = F[0].header
+        hdunames = uvutils.fits_indexhdus(F)
+        basisvec_hdu = F[hdunames['BASISVEC']]
+
+        if 'NAXIS' in keyword:
+            ax_num = keyword.split('NAXIS')[1]
+            if ax_num != '':
+                ax_num = int(ax_num)
+                ax_use = len(basisvec_data.shape) - ax_num
+                new_arrays = np.split(data, primary_hdr[keyword], axis=ax_use)
+                data = new_arrays[0]
+            else:
+                data = np.split(data, primary_hdr['NAXIS1'],
+                                axis=len(data.shape) - 1)[0]
+        else:
+            primary_hdr[keyword] = new_val
+
+        prihdu = fits.PrimaryHDU(data=data, header=primary_hdr)
+        hdulist = fits.HDUList([prihdu, basisvec_hdu])
+
+        if float(astropy.__version__[0:3]) < 1.3:
+            hdulist.writeto(write_file, clobber=True)
+        else:
+            hdulist.writeto(write_file, overwrite=True)
+
+        nt.assert_raises(ValueError, beam_out.read_beamfits, write_file)
 
     # now change values for various items in basisvec hdu to not match primary hdu
-    beam_in.beam_type = 'efield'
+    beam_in = fill_dummy_beam(beam_in, 'efield')
 
     header_vals_to_change = [{'COORDSYS': 'foo'}, {'CTYPE1': 'foo'},
                              {'CTYPE2': 'foo'},
