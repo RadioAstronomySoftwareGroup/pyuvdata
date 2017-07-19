2e32062ac4c24c8d21eb37433b15fee03c8ddf4f
Zachary Martinot <zmartinot@gmail.com>
2017-07-19 16:52:12 -0400

    now the test does something and passes

--- pyuvdata/tests/test_cst_reader.py
+++ pyuvdata/tests/test_cst_reader.py
@@ -1,13 +1,14 @@
 import nose.tools as nt
-import os, sys, glob
+import os, sys
 import numpy as np, healpy as hp
 from pyuvdata.data import DATA_PATH
 from pyuvdata import UVBeam
 
 def test_read():
     beams = UVBeam()
-    files_use = glob.glob(os.path.join(DATA_PATH) + 'HERA_NicCST*.txt')
+    files_use = ['HERA_NicCST_150MHz.txt', 'HERA_NicCST_123MHz.txt']
+    file_paths = [os.path.join(DATA_PATH, f) for f in files_use]
 
-    beams.read_cst_power(files_use, 'peak')
+    beams.read_cst_power(file_paths, 'peak')
 
     nt.assert_true(beams.check())
