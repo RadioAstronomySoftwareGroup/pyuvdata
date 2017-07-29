Add round-trip rotation test, bug fix

pyuvdata/tests/test_utils.py
@@ -135,13 +135,17 @@ def test_mwa_ecef_conversion():
 
     # The STABXYZ coordinates are defined with X through the local meridian,
     # so rotate back to the prime meridian
-    ecef_xyz = uvutils.ECEF_from_rotECEF(xyz, lon)
+    new_xyz = uvutils.ECEF_from_rotECEF(xyz, lon)
     # add in array center to get real ECEF
-    ecef_xyz = (ecef_xyz.T + arrcent).T
+    ecef_xyz = (new_xyz.T + arrcent).T
 
     enu = uvutils.ENU_from_ECEF(ecef_xyz, lat, lon, alt)
 
     nt.assert_true(np.allclose(enu, enh))
+
+    # test other direction of ECEF rotation
+    rot_xyz = uvutils.rotECEF_from_ECEF(new_xyz, lon)
+    nt.assert_true(np.allclose(rot_xyz, xyz))
 
 
 def test_pol_funcs():

pyuvdata/utils.py
@@ -108,7 +108,7 @@ def rotECEF_from_ECEF(xyz, longitude):
     rot_matrix = np.array([[np.cos(angle), -1 * np.sin(angle), 0],
                            [np.sin(angle), np.cos(angle), 0],
                            [0, 0, 1]])
-    return rot_matrix.dot(xyz.T).T
+    return rot_matrix.dot(xyz)
 
 
 def ECEF_from_rotECEF(xyz, longitude):