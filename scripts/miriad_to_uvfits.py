c6441bfd08b42734b3d912c243fd23b088298d86
Adam Beardsley <adam.p.beardsley@gmail.com>
2017-10-17 09:50:29 -0600

    Removed unnecessary import and corrected argument

--- scripts/miriad_to_uvfits.py
+++ scripts/miriad_to_uvfits.py
@@ -5,7 +5,6 @@ import optparse
 import sys
 import os
 import pyuvdata
-from matplotlib.pyplot import *
 o = optparse.OptionParser()
 o.set_usage('miriad_to_uvfits.py  *.uv')
 o.set_description(__doc__)
@@ -17,5 +16,4 @@ for filename in args:
     UV.read_miriad(filename, 'miriad')
     outfilename = filename + '.uvfits'
     UV.phase_to_time(UV.time_array[0])
-    UV.write_uvfits(outfilename, 'uvfits')
-show()
+    UV.write_uvfits(outfilename, spoof_nonessential=True)
