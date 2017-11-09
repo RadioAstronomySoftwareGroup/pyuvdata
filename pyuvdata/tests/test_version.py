acf248fffff686b26707baa33945164df9d00881
Peter Williams <peter@newton.cx>
2017-11-08 22:07:31 -0500

    pyuvdata/tests/test_version.py: fix this for non-Git directories

--- pyuvdata/tests/test_version.py
+++ pyuvdata/tests/test_version.py
@@ -4,6 +4,7 @@ import sys
 import os
 from StringIO import StringIO
 import subprocess
+import json
 import pyuvdata
 
 
@@ -12,14 +13,34 @@ def test_construct_version_info():
     # but it will detect accidental changes that could cause problems.
     # It does test that the __version__ attribute is set on pyuvdata.
     # I can't figure out how to test the except clause in construct_version_info.
-    git_origin = subprocess.check_output(['git', 'config', '--get', 'remote.origin.url'],
-                                         stderr=subprocess.STDOUT).strip()
-    git_hash = subprocess.check_output(['git', 'rev-parse', 'HEAD'],
-                                       stderr=subprocess.STDOUT).strip()
-    git_description = subprocess.check_output(['git', 'describe', '--dirty', '--tags', '--always']).strip()
-    git_branch = subprocess.check_output(['git', 'rev-parse', '--abbrev-ref', 'HEAD'],
-                                         stderr=subprocess.STDOUT).strip()
-    git_version = subprocess.check_output(['git', 'describe', '--tags', '--abbrev=0']).strip()
+
+    # this line is modified from the main implementation since we're in pyuvdata/tests/
+    pyuvdata_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+
+    try:
+        git_origin = subprocess.check_output(['git', 'config', '--get', 'remote.origin.url'],
+                                             stderr=subprocess.STDOUT).strip()
+        git_hash = subprocess.check_output(['git', 'rev-parse', 'HEAD'],
+                                           stderr=subprocess.STDOUT).strip()
+        git_description = subprocess.check_output(['git', 'describe', '--dirty', '--tags', '--always']).strip()
+        git_branch = subprocess.check_output(['git', 'rev-parse', '--abbrev-ref', 'HEAD'],
+                                             stderr=subprocess.STDOUT).strip()
+        git_version = subprocess.check_output(['git', 'describe', '--tags', '--abbrev=0']).strip()
+    except subprocess.CalledProcessError:
+        try:
+            # Check if a GIT_INFO file was created when installing package
+            git_file = os.path.join(pyuvdata_dir, 'GIT_INFO')
+            with open(git_file) as data_file:
+                data = [x.encode('UTF8') for x in json.loads(data_file.read().strip())]
+                git_origin = data[0]
+                git_hash = data[1]
+                git_description = data[2]
+                git_branch = data[3]
+        except (IOError, OSError):
+            git_origin = ''
+            git_hash = ''
+            git_description = ''
+            git_branch = ''
 
     test_version_info = {'version': pyuvdata.__version__, 'git_origin': git_origin,
                          'git_hash': git_hash, 'git_description': git_description,
