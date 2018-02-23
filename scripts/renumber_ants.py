c5e4ff8a093a2cd83e8b894d8b5c2ddf2883742f
Bryna Hazelton <bryna.hazelton@gmail.com>
2018-02-23 09:14:53 -0800

    decrease max ant number to 254, pep8 fixes
    CASA can't handle antenna numbers above 254

--- scripts/renumber_ants.py
+++ scripts/renumber_ants.py
@@ -8,11 +8,14 @@ import argparse
 from pyuvdata import UVData
 
 # setup argparse
-a = argparse.ArgumentParser(description="A command-line script for renumbering antenna numbers > 255 if possible in uvfits files.")
+a = argparse.ArgumentParser(description="A command-line script for renumbering "
+                            "antenna numbers > 255 if possible in uvfits files.")
 a.add_argument("file_in", type=str, help="input uvfits file.")
 a.add_argument("file_out", type=str, help="output uvfits file.")
-a.add_argument("--overwrite", default=False, action='store_true', help="overwrite output file if it already exists.")
-a.add_argument("--verbose", default=False, action='store_true', help="report feedback to stdout.")
+a.add_argument("--overwrite", default=False, action='store_true',
+               help="overwrite output file if it already exists.")
+a.add_argument("--verbose", default=False, action='store_true',
+               help="report feedback to stdout.")
 
 # get args
 args = a.parse_args()
@@ -24,9 +27,9 @@ if os.path.exists(args.file_out) and args.overwrite is False:
 uv_obj = UVData()
 uv_obj.read_uvfits(args.file_in)
 
-large_ant_nums = uv_obj.antenna_numbers[np.where(uv_obj.antenna_numbers > 255)[0]]
+large_ant_nums = sorted(list(uv_obj.antenna_numbers[np.where(uv_obj.antenna_numbers > 254)[0]]))
 
-new_nums = sorted(list(set(range(256)) - set(uv_obj.antenna_numbers)))
+new_nums = sorted(list(set(range(255)) - set(uv_obj.antenna_numbers)))
 if len(new_nums) < len(large_ant_nums):
     raise ValueError('too many antennas in dataset, cannot renumber all below 255')
 new_nums = new_nums[-1 * len(large_ant_nums):]
@@ -44,7 +47,8 @@ for ant_in, ant_out in renumber_dict.iteritems():
     uv_obj.ant_1_array[wh_ant1_arr] = ant_out
     uv_obj.ant_2_array[wh_ant2_arr] = ant_out
     blt_inds = np.array(sorted(list(set(wh_ant1_arr.tolist() + wh_ant2_arr.tolist()))))
-    uv_obj.baseline_array[blt_inds] = uv_obj.antnums_to_baseline(uv_obj.ant_1_array[blt_inds], uv_obj.ant_2_array[blt_inds])
+    uv_obj.baseline_array[blt_inds] = \
+        uv_obj.antnums_to_baseline(uv_obj.ant_1_array[blt_inds], uv_obj.ant_2_array[blt_inds])
 
 uv_obj.check()
 
