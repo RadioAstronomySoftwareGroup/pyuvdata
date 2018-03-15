2808adfd815806eeea2af75716c26b3647f3064d
Nicholas Kern <nkern@berkeley.edu>
2018-03-14 19:38:53 -0700

    modified:   scripts/renumber_ants.py

--- scripts/renumber_ants.py
+++ scripts/renumber_ants.py
@@ -69,4 +69,4 @@ uv_obj.check()
 if args.filetype == 'uvfits':
     uv_obj.write_uvfits(args.file_out)
 elif args.filetype == 'miriad':
-    uv_obj.write_miriad(args.file_out)
+    uv_obj.write_miriad(args.file_out, clobber=True)
