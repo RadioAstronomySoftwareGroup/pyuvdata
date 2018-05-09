b41ac8c5f7fdea1b982ebe21b573a22dbb3a8466
EXTERNAL-Ewall-Wice <aaron.m.ewall-wice@jpl.nasa.gov>
2018-05-08 22:36:06 -0700

    working through snap imports.

--- scripts/import_snap.py
+++ scripts/import_snap.py
@@ -21,8 +21,12 @@ import numpy
 import yaml
 import argparse
 from pyuvdata import UVData
+<<<<<<< HEAD
 
 >>>>>>> starting snap conversion script.
+=======
+from pyuvdata import polstr2num
+>>>>>>> working through snap imports.
 desc=('Convert SNAP outputs into uvdata sets.'
       'Usage: import_snap.py -c config.yaml -z observation.npz'
       'config.yaml is a .yaml file containing instrument meta-data'
@@ -196,5 +200,44 @@ with open(parser.config) as configfile:
 
 data=np.load(parser.data)
 
+<<<<<<< HEAD
 #instantiate a uvdata object.
 >>>>>>> starting snap conversion script.
+=======
+#instatiate uvdata object
+data_uv=UVData()
+data_uv.Ntimes=len(data['times'])
+data_uv.Nbls=len(data['antenna_pairs'])
+data_uv.Nblts=data_uv.Ntimes*data_uv.Nbls
+data_uv.Nfreqs=len(data['frequencies'])
+data_uv.Npols=len(data['polarizations'])
+data_uv.vis_units='uncalib'
+data_uv.Nspws=1 #!!!Assume a single spectral window.!!!
+#instantiate flag array with no flags
+data_uv.flag_array=np.empty((data_uv.Nblts,data_uv.Nspws,
+                             data_uv.Nfreqs,data_uv.Npols,dtype=bool)
+data_uv.flag_array[:]=False
+#instantiate nsamples array with equal unity weights.
+data_uv.nsample_array=np.ones(data_uv.Nblts,data_uv.Nspws,
+                              data_uv.Nfreqs,data_uv.Npols,dtype=float)
+
+#create frequency array
+data_uv.freq_array=np.zeros((self.Nspws,self.Nfreqs))
+data_uv.freq_array[0,:]=data['frequencies']
+#channel width
+data_uv.channel_width=data_uv.freq_array[0,1]-data_uv.freq_array[0,0]
+#convert to Nblt ordering
+data_uv.data_array
+#create ant_1_array
+#create ant_2_array
+#create baseline_array
+#create polarization array
+data_uv.polarization_array=\
+np.array([polstr2num(config['POLARIZATIONS'][p])\
+ for p in len(config['POLARIZATIONS'])]).astype(int)
+#set telescope location
+data_uv.telescope_location=np.array(config['TELESCOPE_LOCATION_ITRF'])
+
+#create time array
+#convert times to julian days
+>>>>>>> working through snap imports.
