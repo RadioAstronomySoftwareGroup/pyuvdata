01f7225f0143ccb2e9d361398d0dd0ee11446e42
Bryna Hazelton <bryna.hazelton@gmail.com>
2018-06-22 16:55:09 -0700

    improve utils test coverage

--- pyuvdata/tests/test_utils.py
+++ pyuvdata/tests/test_utils.py
@@ -221,6 +221,18 @@ def test_phasing_funcs():
     nt.assert_true(np.allclose(gcrs_uvw[0, 1], mwa_tools_calcuvw_v, atol=1e-3))
     nt.assert_true(np.allclose(gcrs_uvw[0, 2], mwa_tools_calcuvw_w, atol=1e-3))
 
+    # also test with simple phasing:
+    gcrs_uvw_simple = uvutils.phase_uvw(gcrs_coord.ra.rad, gcrs_coord.dec.rad,
+                                        gcrs_rel.value)
+    nt.assert_true(np.allclose(gcrs_uvw, gcrs_uvw_simple))
+
+    # also test unphasing
+    temp = uvutils.mwatools_calcuvw_unphase(ha_gcrs.rad, gcrs_coord.dec.rad, gcrs_uvw)
+    nt.assert_true(np.allclose(gcrs_rel_rot, temp))
+
+    temp2 = uvutils.unphase_uvw(gcrs_coord.ra.rad, gcrs_coord.dec.rad, gcrs_uvw_simple)
+    nt.assert_true(np.allclose(gcrs_rel.value, temp2))
+
 
 def test_pol_funcs():
     """ Test utility functions to convert between polarization strings and numbers """
