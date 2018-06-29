c7fd2f890e0ee3e3d4ef6b4b2c697d1aaeffba23
Ruby Byrne <rlbyrne@uw.edu>
2018-06-29 13:26:24 -0700

    more pep8 corrections

--- pyuvdata/uvdata.py
+++ pyuvdata/uvdata.py
@@ -861,14 +861,14 @@ class UVData(UVBase):
         antenna_locs_ENU = uvutils.ENU_from_ECEF(
             (self.antenna_positions + self.telescope_location).T,
             *self.telescope_location_lat_lon_alt
-            ).T
+        ).T
         uvw_array = np.zeros((self.baseline_array.size, 3))
         for ant1 in list(set(self.ant_1_array)):
             for ant2 in list(set(self.ant_2_array)):
                 baseline_inds = np.intersect1d(
                     np.where(self.ant_1_array == ant1)[0],
                     np.where(self.ant_2_array == ant2)[0]
-                    )
+                )
                 uvw_array[baseline_inds, :] = (antenna_locs_ENU[ant2, :]
                                                - antenna_locs_ENU[ant1, :])
         self.uvw_array = uvw_array
