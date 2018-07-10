c387e7276a4cf88ab83cfdce00690ce653bf8960
Ruby Byrne <rlbyrne@uw.edu>
2018-07-10 15:29:47 -0700

    bug fix and testing for set_uvws_from_antenna_positions

--- pyuvdata/uvdata.py
+++ pyuvdata/uvdata.py
@@ -863,14 +863,16 @@ class UVData(UVBase):
             *self.telescope_location_lat_lon_alt
         ).T
         uvw_array = np.zeros((self.baseline_array.size, 3))
-        for ant1 in list(set(self.ant_1_array)):
-            for ant2 in list(set(self.ant_2_array)):
-                baseline_inds = np.intersect1d(
-                    np.where(self.ant_1_array == ant1)[0],
-                    np.where(self.ant_2_array == ant2)[0]
-                )
-                uvw_array[baseline_inds, :] = (antenna_locs_ENU[ant2, :]
-                                               - antenna_locs_ENU[ant1, :])
+        for baseline in list(set(self.baseline_array)):
+            baseline_inds = np.where(self.baseline_array == baseline)[0]
+            ant1_index = np.where(
+                self.antenna_numbers == self.ant_1_array[baseline_inds[0]]
+            )[0][0]
+            ant2_index = np.where(
+                self.antenna_numbers == self.ant_2_array[baseline_inds[0]]
+            )[0][0]
+            uvw_array[baseline_inds, :] = (antenna_locs_ENU[ant2_index, :]
+                                           - antenna_locs_ENU[ant1_index, :])
         self.uvw_array = uvw_array
         if phase_type == 'phased':
             self.phase(phase_center_ra, phase_center_dec, phase_center_epoch)
