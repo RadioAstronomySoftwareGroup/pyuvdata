52a7df250eb72515bfad8b24d85121d75c2df5f7
Bryna Hazelton <bryna.hazelton@gmail.com>
2018-10-05 12:24:07 -0700

    remove top-level imports of file-specific and base classes

--- pyuvdata/__init__.py
+++ pyuvdata/__init__.py
@@ -7,18 +7,11 @@
 """
 from __future__ import absolute_import, division, print_function
 
-from .uvbase import *
-from .parameter import *
 from .uvdata import *
-from .utils import *
 from .telescopes import *
-from .uvfits import *
-from .fhd import *
-from .miriad import *
 from .uvcal import *
-from .calfits import *
 from .uvbeam import *
-from .uvh5 import *
+from .utils import *
 from . import version
 
 __version__ = version.version
