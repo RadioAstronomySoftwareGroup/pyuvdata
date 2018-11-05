b8cf6f096e14ee7311c18117d57f98b1745b8105
Adam Beardsley <adam.p.beardsley@gmail.com>
2018-11-05 11:13:46 -0700

    Move warning filter above other imports in init

--- pyuvdata/__init__.py
+++ pyuvdata/__init__.py
@@ -7,15 +7,15 @@
 """
 from __future__ import absolute_import, division, print_function
 
+# Filter annoying Cython warnings that serve no good purpose. see numpy#432
+import warnings
+warnings.filterwarnings("ignore", message="numpy.dtype size changed")
+warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
+
 from .uvdata import *
 from .telescopes import *
 from .uvcal import *
 from .uvbeam import *
 from . import version
-
-# Filter annoying Cython warnings that serve no good purpose. see numpy#432
-import warnings
-warnings.filterwarnings("ignore", message="numpy.dtype size changed")
-warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
 
 __version__ = version.version
