f5187b02b738a6c25952adc76dda9fd54083ca13
Bryna Hazelton <bryna.hazelton@gmail.com>
2019-03-06 14:39:05 -0800

    fix python version test

--- ci/install-circle.sh
+++ ci/install-circle.sh
@@ -10,6 +10,6 @@ source activate ${ENV_NAME}
 conda list -n ${ENV_NAME}
 # check that the python version matches the desired one; exit immediately if not
 PYVER=`python -c "from __future__ import print_function; import sys; print('{:d}.{:d}'.format(sys.version_info.major, sys.version_info.minor))"`
-if [[ $PYVER != $PYTHON_VERSION ]]; then
+if [[ $PYVER != $PYTHON ]]; then
   exit 1;
 fi
