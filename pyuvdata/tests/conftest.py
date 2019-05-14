bd53d6624d455ab11e5c9d8b0d1a29e798f03c3c
Matthew Kolopanis <Matthew.Kolopanis@gmail.com>
2019-05-14 10:08:28 -0700

    created conftest file used for testing environment in pytest
    moved and combined setup and teardown information for testing environment to conftest file. pytest cannot use nose-style setup_package and teardown_package functions in __init__

---
+++ pyuvdata/tests/conftest.py
@@ -0,0 +1,41 @@
+# -*- mode: python; coding: utf-8 -*
+# Copyright (c) 2018 Radio Astronomy Software Group
+# Licensed under the 2-clause BSD License
+
+"""Testing environment setup and teardown for pytest."""
+from __future__ import absolute_import, division, print_function
+
+import os
+import warnings
+import sys
+import pytest
+
+import functools
+import types
+import six
+import six.moves.urllib as urllib
+
+from astropy.utils import iers
+
+from pyuvdata.data import DATA_PATH
+import pyuvdata.utils as uvutils
+
+
+@pytest.fixtures(autouse=True, scope="session")
+def setup_and_teardown_package():
+    """Make data/test directory to put test output files in."""
+    testdir = os.path.join(DATA_PATH, 'test/')
+    if not os.path.exists(testdir):
+        print('making test directory')
+        os.mkdir(testdir)
+
+    # try to download the iers table. If it fails, turn off auto downloading for the tests
+    # and turn it back on in teardown_package (done by extending auto_max_age)
+    try:
+        iers_a = iers.IERS_A.open(iers.IERS_A_URL)
+    except(urllib.error.URLError):
+        iers.conf.auto_max_age = None
+
+    yield
+
+    iers.conf.auto_max_age = 30
