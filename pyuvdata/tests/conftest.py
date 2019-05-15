55b984d1965323638d9b7e3dae171b198c16d32a
Matthew Kolopanis <Matthew.Kolopanis@gmail.com>
2019-05-15 07:36:57 -0700

    conftest copyright 2019

--- pyuvdata/tests/conftest.py
+++ pyuvdata/tests/conftest.py
@@ -1,5 +1,5 @@
 # -*- mode: python; coding: utf-8 -*
-# Copyright (c) 2018 Radio Astronomy Software Group
+# Copyright (c) 2019 Radio Astronomy Software Group
 # Licensed under the 2-clause BSD License
 
 """Testing environment setup and teardown for pytest."""
