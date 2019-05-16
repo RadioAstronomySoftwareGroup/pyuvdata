aab463d96132d43973643c69bde17701bf29c776
Matthew Kolopanis <Matthew.Kolopanis@gmail.com>
2019-05-16 10:41:20 -0700

    removed unittest from uvbeam in favor of pytest fixtures

--- pyuvdata/tests/test_uvbeam.py
+++ pyuvdata/tests/test_uvbeam.py
@@ -11,7 +11,6 @@ import pytest
 import os
 import numpy as np
 import copy
-import unittest
 
 from pyuvdata import UVBeam
 import pyuvdata.tests as uvtest
@@ -29,120 +28,139 @@ cst_folder = 'NicCSTbeams'
 cst_files = [os.path.join(DATA_PATH, cst_folder, f) for f in filenames]
 
 
-class TestUVBeamInit(unittest.TestCase):
-    def setUp(self):
-        """Setup for basic parameter, property and iterator tests."""
-        self.required_parameters = ['_beam_type', '_Nfreqs', '_Naxes_vec', '_Nspws',
-                                    '_pixel_coordinate_system',
-                                    '_freq_array', '_spw_array',
-                                    '_data_normalization',
-                                    '_data_array', '_bandpass_array',
-                                    '_telescope_name', '_feed_name',
-                                    '_feed_version', '_model_name',
-                                    '_model_version', '_history',
-                                    '_antenna_type']
+@pytest.fixture(scope='function')
+def uvbeam_data(request):
+    """Setup and teardown for basic parameter, property and iterator tests."""
+    required_parameters = ['_beam_type', '_Nfreqs', '_Naxes_vec', '_Nspws',
+                           '_pixel_coordinate_system',
+                           '_freq_array', '_spw_array',
+                           '_data_normalization',
+                           '_data_array', '_bandpass_array',
+                           '_telescope_name', '_feed_name',
+                           '_feed_version', '_model_name',
+                           '_model_version', '_history',
+                           '_antenna_type']
 
-        self.required_properties = ['beam_type', 'Nfreqs', 'Naxes_vec', 'Nspws',
-                                    'pixel_coordinate_system',
-                                    'freq_array', 'spw_array',
-                                    'data_normalization',
-                                    'data_array', 'bandpass_array',
-                                    'telescope_name', 'feed_name',
-                                    'feed_version', 'model_name',
-                                    'model_version', 'history',
-                                    'antenna_type']
+    required_properties = ['beam_type', 'Nfreqs', 'Naxes_vec', 'Nspws',
+                           'pixel_coordinate_system',
+                           'freq_array', 'spw_array',
+                           'data_normalization',
+                           'data_array', 'bandpass_array',
+                           'telescope_name', 'feed_name',
+                           'feed_version', 'model_name',
+                           'model_version', 'history',
+                           'antenna_type']
 
-        self.extra_parameters = ['_Naxes1', '_Naxes2', '_Npixels', '_Nfeeds', '_Npols',
-                                 '_Ncomponents_vec',
-                                 '_axis1_array', '_axis2_array', '_nside', '_ordering',
-                                 '_pixel_array', '_feed_array', '_polarization_array',
-                                 '_basis_vector_array',
-                                 '_extra_keywords', '_Nelements',
-                                 '_element_coordinate_system',
-                                 '_element_location_array', '_delay_array',
-                                 '_x_orientation',
-                                 '_interpolation_function', '_freq_interp_kind',
-                                 '_gain_array', '_coupling_matrix',
-                                 '_reference_impedance',
-                                 '_receiver_temperature_array',
-                                 '_loss_array', '_mismatch_array',
-                                 '_s_parameters']
+    extra_parameters = ['_Naxes1', '_Naxes2', '_Npixels', '_Nfeeds', '_Npols',
+                        '_Ncomponents_vec',
+                        '_axis1_array', '_axis2_array', '_nside', '_ordering',
+                        '_pixel_array', '_feed_array', '_polarization_array',
+                        '_basis_vector_array',
+                        '_extra_keywords', '_Nelements',
+                        '_element_coordinate_system',
+                        '_element_location_array', '_delay_array',
+                        '_x_orientation',
+                        '_interpolation_function', '_freq_interp_kind',
+                        '_gain_array', '_coupling_matrix',
+                        '_reference_impedance',
+                        '_receiver_temperature_array',
+                        '_loss_array', '_mismatch_array',
+                        '_s_parameters']
 
-        self.extra_properties = ['Naxes1', 'Naxes2', 'Npixels', 'Nfeeds', 'Npols',
-                                 'Ncomponents_vec',
-                                 'axis1_array', 'axis2_array', 'nside', 'ordering',
-                                 'pixel_array', 'feed_array', 'polarization_array',
-                                 'basis_vector_array', 'extra_keywords', 'Nelements',
-                                 'element_coordinate_system',
-                                 'element_location_array', 'delay_array',
-                                 'x_orientation',
-                                 'interpolation_function', 'freq_interp_kind',
-                                 'gain_array', 'coupling_matrix',
-                                 'reference_impedance',
-                                 'receiver_temperature_array',
-                                 'loss_array', 'mismatch_array',
-                                 's_parameters']
+    extra_properties = ['Naxes1', 'Naxes2', 'Npixels', 'Nfeeds', 'Npols',
+                        'Ncomponents_vec',
+                        'axis1_array', 'axis2_array', 'nside', 'ordering',
+                        'pixel_array', 'feed_array', 'polarization_array',
+                        'basis_vector_array', 'extra_keywords', 'Nelements',
+                        'element_coordinate_system',
+                        'element_location_array', 'delay_array',
+                        'x_orientation',
+                        'interpolation_function', 'freq_interp_kind',
+                        'gain_array', 'coupling_matrix',
+                        'reference_impedance',
+                        'receiver_temperature_array',
+                        'loss_array', 'mismatch_array',
+                        's_parameters']
 
-        self.other_properties = ['pyuvdata_version_str']
+    other_properties = ['pyuvdata_version_str']
 
-        self.beam_obj = UVBeam()
+    beam_obj = UVBeam()
 
-    def teardown(self):
-        """Test teardown: delete object."""
-        del(self.beam_obj)
+    yield beam_obj, required_parameters, required_properties, extra_parameters, extra_properties, other_properties
+    del(beam_obj)
 
-    def test_parameter_iter(self):
-        "Test expected parameters."
-        all = []
-        for prop in self.beam_obj:
-            all.append(prop)
-        for a in self.required_parameters + self.extra_parameters:
-            assert a in all, 'expected attribute ' + a + ' not returned in object iterator'
+    return
 
-    def test_required_parameter_iter(self):
-        "Test expected required parameters."
-        required = []
-        for prop in self.beam_obj.required():
-            required.append(prop)
-        for a in self.required_parameters:
-            assert a in required, 'expected attribute ' + a + ' not returned in required iterator'
 
-    def test_extra_parameter_iter(self):
-        "Test expected optional parameters."
-        extra = []
-        for prop in self.beam_obj.extra():
-            extra.append(prop)
-        for a in self.extra_parameters:
-            assert a in extra, 'expected attribute ' + a + ' not returned in extra iterator'
+def test_parameter_iter(uvbeam_data, request):
+    """Test expected parameters."""
+    (beam_obj, required_parameters, required_properties,
+     extra_parameters, extra_properties, other_properties) = uvbeam_data
+    all = []
+    for prop in beam_obj:
+        all.append(prop)
+    for a in required_parameters + extra_parameters:
+        assert a in all, 'expected attribute ' + a + ' not returned in object iterator'
 
-    def test_unexpected_parameters(self):
-        "Test for extra parameters."
-        expected_parameters = self.required_parameters + self.extra_parameters
-        attributes = [i for i in self.beam_obj.__dict__.keys() if i[0] == '_']
-        for a in attributes:
-            assert a in expected_parameters, 'unexpected parameter ' + a + ' found in UVBeam'
 
-    def test_unexpected_attributes(self):
-        "Test for extra attributes."
-        expected_attributes = self.required_properties + \
-            self.extra_properties + self.other_properties
-        attributes = [i for i in self.beam_obj.__dict__.keys() if i[0] != '_']
-        for a in attributes:
-            assert a in expected_attributes, 'unexpected attribute ' + a + ' found in UVBeam'
+def test_required_parameter_iter(uvbeam_data):
+    """Test expected required parameters."""
+    (beam_obj, required_parameters, required_properties,
+     extra_parameters, extra_properties, other_properties) = uvbeam_data
+    required = []
+    for prop in beam_obj.required():
+        required.append(prop)
+    for a in required_parameters:
+        assert a in required, 'expected attribute ' + a + ' not returned in required iterator'
 
-    def test_properties(self):
-        "Test that properties can be get and set properly."
-        prop_dict = dict(list(zip(self.required_properties + self.extra_properties,
-                                  self.required_parameters + self.extra_parameters)))
-        for k, v in prop_dict.items():
-            rand_num = np.random.rand()
-            setattr(self.beam_obj, k, rand_num)
-            this_param = getattr(self.beam_obj, v)
-            try:
-                assert rand_num == this_param.value
-            except(AssertionError):
-                print('setting {prop_name} to a random number failed'.format(prop_name=k))
-                raise(AssertionError)
+
+def test_extra_parameter_iter(uvbeam_data):
+    """Test expected optional parameters."""
+    (beam_obj, required_parameters, required_properties,
+     extra_parameters, extra_properties, other_properties) = uvbeam_data
+    extra = []
+    for prop in beam_obj.extra():
+        extra.append(prop)
+    for a in extra_parameters:
+        assert a in extra, 'expected attribute ' + a + ' not returned in extra iterator'
+
+
+def test_unexpected_parameters(uvbeam_data):
+    """Test for extra parameters."""
+    (beam_obj, required_parameters, required_properties,
+     extra_parameters, extra_properties, other_properties) = uvbeam_data
+    expected_parameters = required_parameters + extra_parameters
+    attributes = [i for i in beam_obj.__dict__.keys() if i[0] == '_']
+    for a in attributes:
+        assert a in expected_parameters, 'unexpected parameter ' + a + ' found in UVBeam'
+
+
+def test_unexpected_attributes(uvbeam_data):
+    """Test for extra attributes."""
+    (beam_obj, required_parameters, required_properties,
+     extra_parameters, extra_properties, other_properties) = uvbeam_data
+    expected_attributes = required_properties + \
+        extra_properties + other_properties
+    attributes = [i for i in beam_obj.__dict__.keys() if i[0] != '_']
+    for a in attributes:
+        assert a in expected_attributes, 'unexpected attribute ' + a + ' found in UVBeam'
+
+
+def test_properties(uvbeam_data):
+    "Test that properties can be get and set properly."
+    (beam_obj, required_parameters, required_properties,
+     extra_parameters, extra_properties, other_properties) = uvbeam_data
+    prop_dict = dict(list(zip(required_properties + extra_properties,
+                              required_parameters + extra_parameters)))
+    for k, v in prop_dict.items():
+        rand_num = np.random.rand()
+        setattr(beam_obj, k, rand_num)
+        this_param = getattr(beam_obj, v)
+        try:
+            assert rand_num == this_param.value
+        except(AssertionError):
+            print('setting {prop_name} to a random number failed'.format(prop_name=k))
+            raise(AssertionError)
 
 
 def test_errors():
