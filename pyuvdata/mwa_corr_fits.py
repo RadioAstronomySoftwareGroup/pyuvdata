97f0e7092898e0af17e47fdbbf33393fdb64ea37
PStar <pyxiestar@gmail.com>
2019-07-03 11:37:31 -0700

    added file reader

--- pyuvdata/mwa_corr_fits.py
+++ pyuvdata/mwa_corr_fits.py
@@ -42,8 +42,6 @@ def input_output_mapping():
                     index2=inp2*2+p2
                     corr_mapper[(pfb_outputs_to_inputs[index1],pfb_outputs_to_inputs[index2])]=[inp1,inp2,p1,p2]
     return corr_mapper
-#what to do with the metafits_ppds file?
-#also flag files?
 
 class MWACorrFITS(UVData):
     """
@@ -67,22 +65,44 @@ class MWACorrFITS(UVData):
                 parameters after reading in the file. Default is True.
         """
         
-        #TODO: check that data for all the contiguous coarse channels are present
-        
-        #check that have a metafits file
-        #TODO: ***fix this so that it allows for multiple metafits files!***
-        #for now, force only one metafits file
         metafits_file=None
+        file_dict={}
+        
         for file in filelist:
             if file.lower().endswith('.metafits'):
+                #check that have a metafits file
+                #TODO: ***fix this so that it allows for multiple metafits files!***
+                #for now, force only one metafits file
                 if metafits_file is not None:
                     raise ValueError('multiple metafits files in filelist')
                 metafits_file = file
+            #organize data files
+            #TOOD: what to do with the metafits_ppds file?
+            elif file.lower().endswith('00.fits'):
+                if '00' not in file_dict.keys():
+                    file_dict['00']=[file]
+                else:
+                    file_dict['00'].append(file)
+            elif file.lower().endswith('01.fits'):
+                if '01' not in file_dict.keys():
+                    file_dict['01']=[file]
+                else:
+                    file_dict['01'].append(file)
+            elif file.lower().endswith('.mwaf'):
+                if 'flags' not in file_dict.keys():
+                    file_dict['flags']=[file]
+                else:
+                    file_dict['flags'].append(file)        
             else:
-                continue #need this?
-                
-        #TODO: check that visibility files are included
-        #TODO: look for flag files; not required but give a warning if they don't exist
+                continue
+        
+        #checks:
+        if len(file_dict['00'])!=len(file_dict['01']):
+            raise ValueError('coarse band file missing')
+        if '01' not in file_dict.keys() and '00' not in file_dict.keys():
+            raise ValueError('no fits files submitted')
+        if 'flags' not in file_dict.keys():
+            warnings.warn('no flag files submitted')
         
         #first set parameters that are always true
         self.Nspws=1
@@ -106,46 +126,106 @@ class MWACorrFITS(UVData):
             self.telescope_name=meta_hdr['TELESCOP']
             self.object_name=meta_hdr['FILENAME']
             #TODO: remove these keys and store remaining keys in extra keywords
-            #TODO: check that coarse channels are contiguous
-#            chans=meta[0].header['CHANNELS'].split(',') 
-#            chans=np.array([int(i) for i in chans])
+            chans=meta_hdr['CHANNELS'].split(',') 
             
             #get data from metafits file table
             meta_tbl=meta[1].data
             self.antenna_numbers=meta_tbl['Tile'][1::2]#because of polarization, each antenna # is listed twice
             self.antenna_names=meta_tbl['TileName'][1::2]
-            self.Nants_data=len(self.antenna_numbers)
-            self.Nants_telescope=len(self.antenna_numbers)
-            self.Nbls=len(self.antenna_numbers)*(len(self.antenna_numbers+1))/2
-            self.Nblts=self.Nbls*self.Ntimes
-            #TODO: self.freq_array
-            
+            #TODO: self.freq_array; this might be tricky for non-contiguous frequencies
             #TODO: self.antenna_positions
             #TODO: self.antenna_diameters
             #TODO: self.x_orientation
             
-            #TODO:
-            self.baseline_array=np.zeros(self.Nblts)
-            #TODO:
-            self.ant_1_array=np.zeros(self.Nblts)
-            #TODO:
-            self.ant_2_array=np.zeros(self.Nblts)
-            #TODO:
-            self.data_array=np.zeros((self.Nblts,self.Nspws,self.Nfreqs,self.Npols))
-            #TODO:
-            self.flag_array=np.zeros((self.Nblts,self.Nspws,self.Nfreqs,self.Npols))
-            #TODO:
-            self.lst_array=np.zeros(self.Nblts)
-            #TODO:
-            self.nsample_array=np.zeros((self.Nblts,self.Nspws,self.Nfreqs,self.Npols))
-            #TODO
-            self.polarization_array=np.zeros(self.Npols)
-            #TODO ask Bryna what is the shape of this?
-            #self.telescope_location
-            #TODO
-            self.time_array=np.zeros(self.Nblts)
-            #TODO
-            self.uvw_array=np.zeros((self.Nblts,3))
+        self.Nants_data=len(self.antenna_numbers)
+        self.Nants_telescope=len(self.antenna_numbers)
+        self.Nbls=len(self.antenna_numbers)*(len(self.antenna_numbers+1))/2
+        self.Nblts=self.Nbls*self.Ntimes
+        
+        #check that coarse channels are contiguous.
+        #TODO: look at a data file where the coarse channels aren't contiguous to check
+        chans=np.array([int(i) for i in chans])
+        for i in np.diff(chans):
+            if i!=1:
+                warnings.warn('coarse channels are not contiguous for this observation')
+                break
+        
+        #check that all coarse channels in data are contiguous
+        file_chans=np.array([int(file.split('_')[-2][-2:]) for file in file_dict['00']])
+        for i in np.diff(file_chans):
+            if i!=1:
+                warnings.warn('coarse channels are not contiguous for included data')
+                break
+        
+        #check that all channels are included
+        if len(chans)!=len(file_chans):
+            warnings.warn('only a subset of coarse channels are being processed')
+            
+        #read in the data!
+        #read into a data array with dimensions (time, frequency, baselines/pols)
+        #and a times array holds the unix timestamp from each hdu
+        #TODO: fix this to deal with a frequency or time subset
+        data_dump=np.zeros((self.Ntimes, self.Nfreqs, self.Nbls*self.Npols),dtype=np.complex)
+        times_dump=np.zeros(self.Ntimes)
+        #get a file from '00'
+        for file in file_dict['00']:
+            #get the course channel index from the file name
+            course=int(file.split('_')[-2][-2:])
+            with fits.open(file,memmap=True,do_not_scale_image_data=True) as hdu_list:
+                end_time=len(hdu_list)
+                #get number of fine channels
+                fine=hdu_list[1].header['NAXIS2']
+                freq_ind=(course-1)*fine
+                for i in range(1,end_time):
+                    #this takes data from real to complex numbers
+                    data_dump[i-1,freq_ind:freq_ind+fine,:]=hdu_list[i].data[:,0::2]+1j*hdu_list[i].data[:,1::2]
+                    times_dump[i-1]=(hdu_list[i].header['TIME']+hdu_list[i].header['MILLITIM']/1000)
+        start_time=end_time
+        for file in file_dict['01']:
+            #get the course channel index from the file name
+            course=int(file.split('_')[-2][-2:])
+            with fits.open(file,memmap=True,do_not_scale_image_data=True) as hdu_list:
+                end_time=len(hdu_list)
+                #get number of fine channels
+                fine=hdu_list[1].header['NAXIS2']
+                freq_ind=(course-1)*fine
+                for i in range(1,end_time):
+                    #this takes data from real to complex numbers
+                    data_dump[start_time+i-2,freq_ind:freq_ind+fine,:]=hdu_list[i].data[:,0::2]+1j*hdu_list[i].data[:,1::2]
+                    times_dump[start_time+i-2]=(hdu_list[i].header['TIME']+hdu_list[i].header['MILLITIM']/1000)
+        #TODO: read in flag files
+
+        #TODO: reindex the data_dump!
+        #TODO: convert times_dump to julian dates
+    
+            
+        
+        
+        
+        
+        
+        #TODO:
+        self.baseline_array=np.zeros(self.Nblts, dtype=np.complex)
+        #TODO:
+        self.ant_1_array=np.zeros(self.Nblts)
+        #TODO:
+        self.ant_2_array=np.zeros(self.Nblts)
+        #TODO:
+        self.data_array=np.zeros((self.Nblts, self.Nspws, self.Nfreqs, self.Npols))
+        #TODO:
+        self.flag_array=np.zeros((self.Nblts, self.Nspws, self.Nfreqs, self.Npols))
+        #TODO:
+        self.lst_array=np.zeros(self.Nblts)
+        #TODO:
+        self.nsample_array=np.zeros((self.Nblts, self.Nspws, self.Nfreqs, self.Npols))
+        #TODO
+        self.polarization_array=np.zeros(self.Npols)
+        #TODO ask Bryna what is the shape of this?
+        #self.telescope_location
+        #TODO talk to Bryna about this!
+        self.time_array=np.zeros(self.Nblts)
+        #TODO
+        self.uvw_array=np.zeros((self.Nblts, 3))
             
             
             
