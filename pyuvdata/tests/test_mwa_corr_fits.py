79ddcaa8d4c83849f2ed4df509b191eaad57f116
pyxieloustar <pyxiestar@gmail.com>
2019-11-22 08:10:01 -0800

    add test for different obsids

--- pyuvdata/tests/test_mwa_corr_fits.py
+++ pyuvdata/tests/test_mwa_corr_fits.py
@@ -56,16 +56,16 @@ def test_ReadMWA_ReadCotter():
     cotter_uv.read(filelist[6])
     # first check the xx and yy polarizations
     assert np.allclose(mwa_uv.data_array[:, :, :, 0:2],
-                       cotter_uv.data_array[:, :, :, 0:2], 1e-6)
+                       cotter_uv.data_array[:, :, :, 0:2], atol=1e-4,rtol=0)
     # cotter doesn't record the auto xy polarizations
     autos = np.isclose(mwa_uv.ant_1_array - mwa_uv.ant_2_array, 0.0)
     crosses = np.invert(autos)
     assert np.allclose(mwa_uv.data_array[crosses, :, :, 2],
-                       cotter_uv.data_array[crosses, :, :, 2], 1e-6)
+                       cotter_uv.data_array[crosses, :, :, 2], atol=1e-4,rtol=0)
     # due to a possible bug in cotter, the auto yx polarizations are conjugated
     cotter_uv.data_array[autos, :, :, 3] = np.conj(cotter_uv.data_array[autos, :, :, 3])
     assert np.allclose(mwa_uv.data_array[:, :, :, 3],
-                       cotter_uv.data_array[:, :, :, 3], 1e-6)
+                       cotter_uv.data_array[:, :, :, 3], atol=1e-4,rtol=0)
 
 
 def test_ReadMWAWriteUVFits_meta_mod():
@@ -211,4 +211,20 @@ def test_file_extension():
     with pytest.raises(ValueError) as cm:
         mwa_uv.read_mwa_corr_fits(bad_ext)
     assert str(cm.value).startswith('only fits, metafits, and mwaf files supported')
+    del(mwa_uv)
+
+
+def test_diff_obs():
+    """
+    Test that error is raised if files from different observations are
+    submitted in the same file list.
+    """
+    mwa_uv = UVData()
+    bad_obs = os.path.join(DATA_PATH, 'test/bad2_gpubox06_01.fits')
+    with fits.open(filelist[2]) as mini6:
+        mini6[0].header['OBSID'] = '1131733555'
+        mini6.writeto(bad_obs)
+    with pytest.raises(ValueError) as cm:
+        mwa_uv.read_mwa_corr_fits([bad_obs, filelist[0], filelist[1]])
+    assert str(cm.value).startswith('files from different observations')
     del(mwa_uv)
