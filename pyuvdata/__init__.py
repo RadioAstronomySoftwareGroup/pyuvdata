d491706be890af87fe3c277a54107f8547f22e02
Matthew Kolopanis <Matthew.Kolopanis@gmail.com>
2019-12-03 13:08:10 -0700

    explicitly import from the dir not the module. clean up the top namespace a little
    list all the telescope imports like the other modules

--- pyuvdata/__init__.py
+++ pyuvdata/__init__.py
@@ -11,11 +11,45 @@ import warnings
 warnings.filterwarnings("ignore", message="numpy.dtype size changed")
 warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
 
-from .uvdata.uvdata import UVData  # noqa
-from .telescopes import *  # noqa
-from .uvcal.uvcal import UVCal  # noqa
-from .uvbeam.uvbeam import UVBeam  # noqa
+from .uvdata import UVData  # noqa
+from .telescopes import known_telescopes, Telescope, get_telescope  # noqa
+from .uvcal import UVCal  # noqa
+from .uvbeam import UVBeam  # noqa
 from . import version  # noqa
-from .uvflag.uvflag import UVFlag  # noqa
+from .uvflag import UVFlag  # noqa
+
 
 __version__ = version.version
+__all__ = [
+    "UVData",
+    "UVCal",
+    "UVFlag",
+    "UVBeam",
+    "version",
+    "Telescope",
+    "known_telescopes",
+    "get_telescope"
+]
+
+
+# adapted from https://github.com/astropy/astropy/__init__.py
+# please consult astropy/__init__.py for clarification on logic details
+
+# Cleanup the top-level namespace.
+# Delete everything that is not in __all__, a magic function,
+# or is a submodule of this package
+from types import ModuleType as __module_type__  # noqa
+for varname in dir():
+    if not (
+        varname in __all__
+        or varname == "version"
+        or (varname.startswith("__") and varname.endswith("__"))
+        or (
+            varname[0] != "_"
+            and isinstance(locals()[varname], __module_type__)
+            and locals()[varname].__name__.startswith(__name__ + '.')
+        )
+    ):
+        del locals()[varname]
+
+del varname, __module_type__
