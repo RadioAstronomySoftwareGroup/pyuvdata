8a71988df4c1b20199b07bf08a203f431e108726
Matthew Kolopanis <Matthew.Kolopanis@gmail.com>
2019-12-23 09:54:00 -0700

    fix an import introduced in test_utils

--- pyuvdata/tests/test_utils.py
+++ pyuvdata/tests/test_utils.py
@@ -17,11 +17,15 @@ from astropy.coordinates import SkyCoord, Angle
 from astropy.io import fits
 import copy
 
-from pyuvdata import UVData, UVFlag, UVCal
+from pyuvdata import (
+    UVData,
+    UVFlag,
+    UVCal,
+    utils as uvutils,
+    tests as uvtest,
+    version as uvversion
+)
 from pyuvdata.data import DATA_PATH
-import pyuvdata.utils as uvutils
-import pyuvdata.tests as uvtest
-import pyuvdata.version as uvversion
 
 
 ref_latlonalt = (-26.7 * np.pi / 180.0, 116.7 * np.pi / 180.0, 377.8)
@@ -575,7 +579,7 @@ def test_high_tolerance_redundancy_error():
     Confirm that an error is raised if the redundancy tolerance is set too high,
     such that baselines end up in multiple
     """
-    uvd = pyuvdata.UVData()
+    uvd = UVData()
     uvd.read_uvfits(os.path.join(DATA_PATH, 'fewant_randsrc_airybeam_Nsrc100_10MHz.uvfits'))
 
     uvd.select(times=uvd.time_array[0])
