772c1e0582e9089792ed717bd3a8d55fca4d99c6
Steven Murray <steven.murray@curtin.edu.au>
2019-12-23 16:48:27 -0700

    Fix ref to _unicode_to_str

--- pyuvdata/tests/test_version.py
+++ pyuvdata/tests/test_version.py
@@ -32,7 +32,7 @@ def test_get_gitinfo_file():
         git_file = temp_git_file
 
     with open(git_file) as data_file:
-        data = [pyuvdata.version._unicode_to_str(x) for x in json.loads(data_file.read().strip())]
+        data = [x for x in json.loads(data_file.read().strip())]
         git_origin = data[0]
         git_hash = data[1]
         git_description = data[2]
@@ -73,9 +73,6 @@ def test_construct_version_info():
 
         return data.decode('utf8')
 
-    def unicode_to_str(u):
-        return u
-
     try:
         git_origin = get_git_output(['config', '--get', 'remote.origin.url'], capture_stderr=True)
 
@@ -91,7 +88,7 @@ def test_construct_version_info():
             # Check if a GIT_INFO file was created when installing package
             git_file = os.path.join(pyuvdata_dir, 'GIT_INFO')
             with open(git_file) as data_file:
-                data = [unicode_to_str(x) for x in json.loads(data_file.read().strip())]
+                data = [x for x in json.loads(data_file.read().strip())]
                 git_origin = data[0]
                 git_hash = data[1]
                 git_description = data[2]
