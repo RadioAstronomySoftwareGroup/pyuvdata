ac2c43ca4766fcae7ea3f0e5631ebbe590671e6a
Nicholas Kern <nkern@berkeley.edu>
2019-12-30 09:49:18 -0800

    fixed uvcalibrate w/ x_orientation update

--- pyuvdata/tests/test_utils.py
+++ pyuvdata/tests/test_utils.py
@@ -955,6 +955,8 @@ def test_uvcalibrate_apply_gains():
     # read data
     uvd = pyuvdata.UVData()
     uvd.read(os.path.join(DATA_PATH, 'zen.2457698.40355.xx.HH.uvcAA'))
+    # give it an x_orientation
+    uvd.x_orientation = 'e'
     uvc = pyuvdata.UVCal()
     uvc.read_calfits(os.path.join(DATA_PATH, 'zen.2457698.40355.xx.gain.calfits'))
     # assign gain scale manually
