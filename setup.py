abd32eca898c2531a6693e5ca04def8b4c68bf37
Steven Murray <steven.murray@curtin.edu.au>
2019-12-30 13:52:07 -0700

    Remove six again

--- setup.py
+++ setup.py
@@ -113,8 +113,8 @@ setup_args = {
         'local_scheme': branch_scheme
     },
     'include_package_data': True,
-    'setup_requires': ['pytest-runner', 'numpy>=1.15'],
-    'install_requires': ['numpy>=1.15', 'six>=1.10', 'scipy', 'astropy>=2.0', 'h5py'],
+    # 'setup_requires': ['pytest-runner', 'numpy>=1.15'],
+    'install_requires': ['numpy>=1.15', 'scipy', 'astropy>=2.0', 'h5py'],
     'tests_require': ['pytest', 'pytest-cases'],
     'extras_require': {
         'casa': casa_reqs,
