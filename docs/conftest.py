d9a9e578b434f0b6553a16bcca76dd7d193b494d
Matthew Kolopanis <Matthew.Kolopanis@gmail.com>
2020-01-30 09:23:02 -0700

    added a confest.py to the docs folder to handle setup/cleanup of output

---
+++ docs/conftest.py
@@ -0,0 +1,25 @@
+# -*- mode: python; coding: utf-8 -*-
+# Copyright (c) 2020 Radio Astronomy Software Group
+# Licensed under the 2-clause BSD License
+
+"""Testing environment setup and teardown for pytest."""
+from __future__ import absolute_import, division, print_function
+
+import os
+import shutil
+
+import pytest
+from pyuvdata.data import DATA_PATH
+
+
+@pytest.fixture(autouse=True, scope="session")
+def setup_and_teardown_package():
+    """Make data/test directory to put test output files in."""
+    testdir = os.path.join(DATA_PATH, 'tutorial_output/')
+    if not os.path.exists(testdir):
+        print('making test directory')
+        os.mkdir(testdir)
+
+    yield
+
+    shutil.rmtree(testdir)
