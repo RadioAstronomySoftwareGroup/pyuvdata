8a1a7a115d72a0a2db19fedf1c2d55b8006d7de9
Bryna Hazelton <bryna.hazelton@gmail.com>
2020-02-13 15:43:19 -0800

    Add github actions badge to RTD docs, tweak the readme badge target

--- docs/make_index.py
+++ docs/make_index.py
@@ -31,6 +31,8 @@ def write_index_rst(readme_file=None, write_file=None):
         'pyuvdata\n========\n\n'
         '.. image:: https://circleci.com/gh/RadioAstronomySoftwareGroup/pyuvdata.svg?style=svg\n'
         '    :target: https://circleci.com/gh/RadioAstronomySoftwareGroup/pyuvdata\n\n'
+        '.. image:: https://github.com/RadioAstronomySoftwareGroup/pyuvdata/workflows/Run%20Tests/badge.svg?branch=master\n'
+        '    :target: https://github.com/RadioAstronomySoftwareGroup/pyuvdata/actions\n\n'
         '.. image:: https://dev.azure.com/radioastronomysoftwaregroup/pyuvdata/_apis/build/status/RadioAstronomySoftwareGroup.pyuvdata?branchName=master\n'
         '    :target: https://dev.azure.com/radioastronomysoftwaregroup/pyuvdata/_build/latest?definitionId=1&branchName=master\n\n'
         '.. image:: https://codecov.io/gh/RadioAstronomySoftwareGroup/pyuvdata/branch/master/graph/badge.svg\n'
