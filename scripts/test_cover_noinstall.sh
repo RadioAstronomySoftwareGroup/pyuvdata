1c46fab99ca7f4005b5ebc88985e0f4f9091335f
Matthew Kolopanis <Matthew.Kolopanis@gmail.com>
2020-04-28 14:17:18 -0700

    update test coverage scripts

--- scripts/test_cover_noinstall.sh
+++ scripts/test_cover_noinstall.sh
@@ -3,7 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/..
 
-cd pyuvdata/tests
-python -m pytest --cov=pyuvdata --cov-config=../../.coveragerc\
-       --cov-report term --cov-report html:cover \
+cd pyuvdata
+python -m pytest --cov=pyuvdata --cov-config=../.coveragerc\
+       --cov-report term --cov-report html:tests/cover \
        "$@"
