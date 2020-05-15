fix averaging of redundant baselines

pyuvdata/uvdata/uvdata.py
@@ -5724,9 +5724,9 @@ class UVData(UVBase):
 
                         nsample_to_avg = np.ma.masked_array(nsample_to_avg, mask=mask)
 
-                        avg_vis = np.average(vis_to_avg, weights=nsample_to_avg)
-                        avg_nsample = np.average(nsample_to_avg)
-                        avg_flag = np.all(flags_to_avg)
+                        avg_vis = np.average(vis_to_avg, weights=nsample_to_avg, axis=0)
+                        avg_nsample = np.sum(nsample_to_avg, axis=0)
+                        avg_flag = np.all(flags_to_avg, axis=0)
 
                         temp_data_array[this_obj_ind, :, :, :] = avg_vis
                         temp_nsample_array[this_obj_ind, :, :, :] = avg_nsample