bf0cd142c6f6c7979c64ad9cf17cb82c89691056
pyxieloustar <pyxiestar@gmail.com>
2020-08-19 13:51:05 -0700

    code clean up; initial implementation of cheby_approx

--- pyuvdata/uvdata/uvdata.py
+++ pyuvdata/uvdata/uvdata.py
@@ -6408,6 +6408,7 @@ class UVData(UVBase):
         remove_coarse_band=True,
         correct_cable_len=False,
         correct_van_vleck=False,
+        cheby_approx=True,
         propagate_coarse_flags=True,
         flag_init=True,
         edge_width=80e3,
@@ -6456,6 +6457,9 @@ class UVData(UVBase):
             Option to apply a cable delay correction.
         correct_van_vleck : bool
             Option to apply a van vleck correction.
+        cheby_approx : bool
+            Only used if correct_van_vleck is True. Option to implement the van
+            vleck correction with a chebyshev polynomial approximation.
         propagate_coarse_flags : bool
             Option to propagate flags for missing coarse channel integrations
             across frequency.
@@ -6543,6 +6547,7 @@ class UVData(UVBase):
             remove_coarse_band=remove_coarse_band,
             correct_cable_len=correct_cable_len,
             correct_van_vleck=correct_van_vleck,
+            cheby_approx=cheby_approx,
             propagate_coarse_flags=propagate_coarse_flags,
             flag_init=flag_init,
             edge_width=edge_width,
@@ -6929,6 +6934,7 @@ class UVData(UVBase):
         remove_coarse_band=True,
         correct_cable_len=False,
         correct_van_vleck=False,
+        cheby_approx=True,
         propagate_coarse_flags=True,
         flag_init=True,
         edge_width=80e3,
@@ -7102,6 +7108,10 @@ class UVData(UVBase):
         correct_van_vleck : bool
             Flag to apply a van vleck correction. Only used if file_type is
             'mwa_corr_fits'.
+        cheby_approx : bool
+            Only used if file_type is 'mwa_corr_fits' and correct_van_vleck is True.
+            Option to implement the van vleck correction with a chebyshev polynomial
+            approximation. Set to False to run the integral version of the correction.
         propogate_coarse_flags : bool
             Option to propogate flags for missing coarse channel integrations
             across frequency. Only used if file_type is 'mwa_corr_fits'.
@@ -7586,6 +7596,7 @@ class UVData(UVBase):
                     remove_coarse_band=remove_coarse_band,
                     correct_cable_len=correct_cable_len,
                     correct_van_vleck=correct_van_vleck,
+                    cheby_approx=cheby_approx,
                     propagate_coarse_flags=propagate_coarse_flags,
                     flag_init=flag_init,
                     edge_width=edge_width,
