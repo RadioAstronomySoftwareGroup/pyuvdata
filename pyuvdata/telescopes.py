34d0a50cff1d7a62c9fe089b2ddf209e121a0727
Bryna Hazelton <bryna.hazelton@gmail.com>
2020-09-27 17:20:38 -0700

    use astropy sites for known telescopes, include HERA antenna positions
    update tests

--- pyuvdata/telescopes.py
+++ pyuvdata/telescopes.py
@@ -3,16 +3,24 @@
 # Licensed under the 2-clause BSD License
 
 """Telescope information and known telescope list."""
+import os
 import numpy as np
-from astropy.coordinates import Angle
+from astropy.coordinates import Angle, EarthLocation
 
 from . import uvbase
 from . import parameter as uvp
+from pyuvdata.data import DATA_PATH
 
 __all__ = ["Telescope", "known_telescopes", "get_telescope"]
 
+# We use astropy sites for telescope locations. The dict below is for
+# telescopes not in astropy sites, or to include extra information for a telescope.
 
-# center_xyz is the location of the telescope in ITRF (earth-centered frame)
+# The center_xyz is the location of the telescope in ITRF (earth-centered frame)
+
+# Antenna positions can be specified via a csv file with the following columns:
+# "name" -- antenna name, "number" -- antenna number, "x", "y", "z" -- ECEF coordinates
+# relative to the telescope location.
 KNOWN_TELESCOPES = {
     "PAPER": {
         "center_xyz": None,
@@ -30,17 +38,11 @@ KNOWN_TELESCOPES = {
         "longitude": Angle("21.42830382686301d").radian,
         "altitude": 1051.69,
         "diameters": 14.0,
+        "antenna_positions_file": "hera_ant_pos.csv",
         "citation": (
             "value taken from hera_mc geo.py script "
             "(using hera_cm_db_updates under the hood.)"
         ),
-    },
-    "MWA": {
-        "center_xyz": None,
-        "latitude": Angle("-26d42m11.94986s").radian,
-        "longitude": Angle("116d40m14.93485s").radian,
-        "altitude": 377.827,
-        "citation": "Tingay et al., 2013",
     },
     "SMA": {
         "center_xyz": None,
@@ -100,8 +102,50 @@ class Telescope(uvbase.UVBase):
             expected_type=np.float,
             tols=1e-3,  # 1 mm
         )
-        # possibly add in future versions:
-        # Antenna positions (but what about reconfigurable/growing telescopes?)
+
+        desc = "Number of antennas in the array."
+        self._Nants_telescope = uvp.UVParameter(
+            "Nants_telescope", required=False, description=desc, expected_type=int
+        )
+
+        desc = (
+            "List of antenna names, shape (Nants_telescope), "
+            "with numbers given by antenna_numbers."
+        )
+        self._antenna_names = uvp.UVParameter(
+            "antenna_names",
+            required=False,
+            description=desc,
+            form=("Nants_telescope",),
+            expected_type=str,
+        )
+
+        desc = (
+            "List of integer antenna numbers corresponding to antenna_names, "
+            "shape (Nants_telescope)."
+        )
+        self._antenna_numbers = uvp.UVParameter(
+            "antenna_numbers",
+            required=False,
+            description=desc,
+            form=("Nants_telescope",),
+            expected_type=int,
+        )
+
+        desc = (
+            "Array giving coordinates of antennas relative to "
+            "telescope_location (ITRF frame), shape (Nants_telescope, 3), "
+            "units meters. See the tutorial page in the documentation "
+            "for an example of how to convert this to topocentric frame."
+        )
+        self._antenna_positions = uvp.UVParameter(
+            "antenna_positions",
+            required=False,
+            description=desc,
+            form=("Nants_telescope", 3),
+            expected_type=np.float,
+            tols=1e-3,  # 1 mm
+        )
 
         super(Telescope, self).__init__()
 
@@ -115,7 +159,50 @@ def known_telescopes():
     list of str
         List of known telescope names.
     """
-    return list(KNOWN_TELESCOPES.keys())
+    astropy_sites = EarthLocation.get_site_names()
+    while "" in astropy_sites:
+        astropy_sites.remove("")
+    known_telescopes = list(set(astropy_sites + list(KNOWN_TELESCOPES.keys())))
+    return known_telescopes
+
+
+def _parse_antpos_file(antenna_positions_file):
+    """
+    Interpret the antenna positions file.
+
+    Parameters
+    ----------
+    antenna_positions_file : str
+        Name of the antenna_positions_file, which is assumed to be in DATA_PATH.
+        Should contain antenna names, numbers and ECEF positions relative to the
+        telescope location.
+
+    Returns
+    -------
+    antenna_names : array of str
+        Antenna names.
+    antenna_names : array of int
+        Antenna numbers.
+    antenna_positions : array of float
+        Antenna positions in ECEF relative to the telescope location.
+
+    """
+    columns = ["name", "number", "x", "y", "z"]
+    formats = ["U10", "i8", "f16", "f16", "f16"]
+
+    dt = np.format_parser(formats, columns, [])
+    ant_array = np.genfromtxt(
+        antenna_positions_file,
+        delimiter=",",
+        autostrip=True,
+        skip_header=1,
+        dtype=dt.dtype,
+    )
+    antenna_names = ant_array["name"]
+    antenna_numbers = ant_array["number"]
+    antenna_positions = np.stack((ant_array["x"], ant_array["y"], ant_array["z"])).T
+
+    return antenna_names, antenna_numbers, antenna_positions.astype("float")
 
 
 def get_telescope(telescope_name, telescope_dict_in=None):
@@ -138,14 +225,25 @@ def get_telescope(telescope_name, telescope_dict_in=None):
     if telescope_dict_in is None:
         telescope_dict_in = KNOWN_TELESCOPES
 
-    telescope_list = list(telescope_dict_in.keys())
-    uc_telescope_list = [item.upper() for item in telescope_list]
-    if telescope_name.upper() in uc_telescope_list:
-        telescope_index = uc_telescope_list.index(telescope_name.upper())
-        telescope_dict = telescope_dict_in[telescope_list[telescope_index]]
+    astropy_sites = EarthLocation.get_site_names()
+    telescope_keys = list(telescope_dict_in.keys())
+    telescope_list = [tel.lower() for tel in telescope_keys]
+    if telescope_name in astropy_sites:
+        telescope_loc = EarthLocation.of_site(telescope_name)
+
+        obj = Telescope()
+        obj.telescope_name = telescope_name
+        obj.citation = "astropy sites"
+        obj.telescope_location = np.array(
+            [telescope_loc.x.value, telescope_loc.y.value, telescope_loc.z.value]
+        )
+
+    elif telescope_name.lower() in telescope_list:
+        telescope_index = telescope_list.index(telescope_name.lower())
+        telescope_dict = telescope_dict_in[telescope_keys[telescope_index]]
         obj = Telescope()
         obj.citation = telescope_dict["citation"]
-        obj.telescope_name = telescope_list[telescope_index]
+        obj.telescope_name = telescope_keys[telescope_index]
         if telescope_dict["center_xyz"] is not None:
             obj.telescope_location = telescope_dict["center_xyz"]
         else:
@@ -164,12 +262,29 @@ def get_telescope(telescope_name, telescope_dict_in=None):
                 telescope_dict["longitude"],
                 telescope_dict["altitude"],
             )
-
-            obj.check(run_check_acceptability=True)
-        if "diameters" in telescope_dict.keys():
-            obj.antenna_diameters = telescope_dict["diameters"]
     else:
         # no telescope matching this name
         return False
+
+    # check for extra info
+    if telescope_name.lower() in telescope_list:
+        telescope_index = telescope_list.index(telescope_name.lower())
+        telescope_dict = telescope_dict_in[telescope_keys[telescope_index]]
+        if "diameters" in telescope_dict.keys():
+            obj.antenna_diameters = telescope_dict["diameters"]
+
+        if "antenna_positions_file" in telescope_dict.keys():
+            antpos_file = os.path.join(
+                DATA_PATH, telescope_dict["antenna_positions_file"]
+            )
+            antenna_names, antenna_numbers, antenna_positions = _parse_antpos_file(
+                antpos_file
+            )
+            obj.Nants_telescope = antenna_names.size
+            obj.antenna_names = antenna_names
+            obj.antenna_numbers = antenna_numbers
+            obj.antenna_positions = antenna_positions
+
+    obj.check(run_check_acceptability=True)
 
     return obj
