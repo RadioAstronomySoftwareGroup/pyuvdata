c521e1156d77f1663735790cffce0bb7c74d076b
Garrett Keating <deca57034@gmail.com>
2020-11-12 08:50:34 -0500

    Adding dependencies to setup file

--- setup.py
+++ setup.py
@@ -133,6 +133,8 @@ setup_args = {
     "install_requires": [
         "numpy>=1.18",
         "scipy",
+        "novas",
+        "novas_de405",
         "astropy>=3.2.3",
         "h5py",
         "setuptools_scm",
