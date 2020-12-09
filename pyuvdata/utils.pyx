4ea5dee48b7a39958ca332ed3f6c3f245d73719e
Matthew Kolopanis <Matthew.Kolopanis@gmail.com>
2020-12-09 13:11:05 -0700

    harmonize tabbing in utils.pyx

--- pyuvdata/utils.pyx
+++ pyuvdata/utils.pyx
@@ -113,39 +113,39 @@ cpdef numpy.ndarray[dtype=numpy.int64_t] antnums_to_baseline(
 @cython.wraparound(False)
 @cython.cdivision(True)
 cpdef numpy.ndarray[dtype=numpy.float64_t, ndim=2] _lla_from_xyz(
-    numpy.float64_t[:, ::1] xyz,
+  numpy.float64_t[:, ::1] xyz,
 ):
-    cdef Py_ssize_t ind
-    cdef int n_pts = xyz.shape[1]
-    cdef numpy.float64_t gps_p, gps_theta
+  cdef Py_ssize_t ind
+  cdef int n_pts = xyz.shape[1]
+  cdef numpy.float64_t gps_p, gps_theta
 
-    cdef numpy.ndarray[dtype=numpy.float64_t, ndim=2] lla = np.empty((3, n_pts), dtype=np.float64)
-    cdef numpy.float64_t[:, ::1] _lla = lla
+  cdef numpy.ndarray[dtype=numpy.float64_t, ndim=2] lla = np.empty((3, n_pts), dtype=np.float64)
+  cdef numpy.float64_t[:, ::1] _lla = lla
 
-    # see wikipedia geodetic_datum and Datum transformations of
-    # GPS positions PDF in docs/references folder
-    for ind in range(n_pts):
-        gps_p = sqrt(xyz[0, ind] ** 2 + xyz[1, ind] ** 2)
-        gps_theta = atan2(xyz[2, ind] * _gps_a, gps_p * _gps_b)
+  # see wikipedia geodetic_datum and Datum transformations of
+  # GPS positions PDF in docs/references folder
+  for ind in range(n_pts):
+    gps_p = sqrt(xyz[0, ind] ** 2 + xyz[1, ind] ** 2)
+    gps_theta = atan2(xyz[2, ind] * _gps_a, gps_p * _gps_b)
 
-        _lla[0, ind] = atan2(
-            xyz[2, ind] + _ep2 * _gps_b * sin(gps_theta) ** 3,
-            gps_p - _e2 * _gps_a * cos(gps_theta) ** 3,
-        )
+    _lla[0, ind] = atan2(
+      xyz[2, ind] + _ep2 * _gps_b * sin(gps_theta) ** 3,
+      gps_p - _e2 * _gps_a * cos(gps_theta) ** 3,
+    )
 
-        _lla[1, ind] = atan2(xyz[1, ind], xyz[0, ind])
+    _lla[1, ind] = atan2(xyz[1, ind], xyz[0, ind])
 
-        _lla[2, ind] = (gps_p / cos(lla[0, ind])) - _gps_a / sqrt(1.0 - _e2 * sin(lla[0, ind]) ** 2)
+    _lla[2, ind] = (gps_p / cos(lla[0, ind])) - _gps_a / sqrt(1.0 - _e2 * sin(lla[0, ind]) ** 2)
 
-    return lla
+  return lla
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
 @cython.cdivision(True)
 cpdef numpy.ndarray[dtype=numpy.float64_t, ndim=2] _xyz_from_latlonalt(
-    numpy.float64_t[::1] _lat,
-    numpy.float64_t[::1] _lon,
-    numpy.float64_t[::1] _alt,
+  numpy.float64_t[::1] _lat,
+  numpy.float64_t[::1] _lon,
+  numpy.float64_t[::1] _alt,
 ):
   cdef Py_ssize_t i
   cdef int n_pts = _lat.shape[0]
@@ -155,18 +155,18 @@ cpdef numpy.ndarray[dtype=numpy.float64_t, ndim=2] _xyz_from_latlonalt(
   cdef numpy.float64_t  sin_lat, cos_lat, sin_lon, cos_lon, gps_n
 
   for ind in range(n_pts):
-      sin_lat = sin(_lat[ind])
-      sin_lon = sin(_lon[ind])
+    sin_lat = sin(_lat[ind])
+    sin_lon = sin(_lon[ind])
 
-      cos_lat = cos(_lat[ind])
-      cos_lon = cos(_lon[ind])
+    cos_lat = cos(_lat[ind])
+    cos_lon = cos(_lon[ind])
 
-      gps_n = _gps_a / sqrt(1.0 - _e2 * sin_lat ** 2)
+    gps_n = _gps_a / sqrt(1.0 - _e2 * sin_lat ** 2)
 
-      _xyz[0, ind] = (gps_n + _alt[ind]) * cos_lat * cos_lon
-      _xyz[1, ind] = (gps_n + _alt[ind]) * cos_lat * sin_lon
+    _xyz[0, ind] = (gps_n + _alt[ind]) * cos_lat * cos_lon
+    _xyz[1, ind] = (gps_n + _alt[ind]) * cos_lat * sin_lon
 
-      _xyz[2, ind] = (b_div_a2 * gps_n + _alt[ind]) * sin_lat
+    _xyz[2, ind] = (b_div_a2 * gps_n + _alt[ind]) * sin_lat
   return xyz
 
 # this function takes memoryviews as inputs
@@ -174,93 +174,93 @@ cpdef numpy.ndarray[dtype=numpy.float64_t, ndim=2] _xyz_from_latlonalt(
 @cython.boundscheck(False)
 @cython.wraparound(False)
 cpdef numpy.ndarray[numpy.float64_t, ndim=2] _ENU_from_ECEF(
-    numpy.float64_t[:, ::1] xyz,
-    numpy.float64_t[::1] _lat,
-    numpy.float64_t[::1] _lon,
-    numpy.float64_t[::1] _alt,
+  numpy.float64_t[:, ::1] xyz,
+  numpy.float64_t[::1] _lat,
+  numpy.float64_t[::1] _lon,
+  numpy.float64_t[::1] _alt,
 ):
-    cdef Py_ssize_t i
-    cdef int nblts = xyz.shape[1]
-    cdef numpy.float64_t xyz_use[3]
+  cdef Py_ssize_t i
+  cdef int nblts = xyz.shape[1]
+  cdef numpy.float64_t xyz_use[3]
 
-    cdef numpy.float64_t sin_lat, cos_lat, sin_lon, cos_lon
+  cdef numpy.float64_t sin_lat, cos_lat, sin_lon, cos_lon
 
-    # we want a memoryview of the xyz of the center
-    # this looks a little silly but we don't have to define 2 different things
-    cdef numpy.float64_t[:] xyz_center = _xyz_from_latlonalt(_lat, _lon, _alt)[:, 0]
+  # we want a memoryview of the xyz of the center
+  # this looks a little silly but we don't have to define 2 different things
+  cdef numpy.float64_t[:] xyz_center = _xyz_from_latlonalt(_lat, _lon, _alt)[:, 0]
 
-    cdef numpy.ndarray[numpy.float64_t, ndim=2] _enu = np.empty((3, nblts), dtype=np.float64)
-    cdef numpy.float64_t[:, ::1] enu = _enu
+  cdef numpy.ndarray[numpy.float64_t, ndim=2] _enu = np.empty((3, nblts), dtype=np.float64)
+  cdef numpy.float64_t[:, ::1] enu = _enu
 
-    sin_lat = sin(_lat[0])
-    cos_lat = cos(_lat[0])
+  sin_lat = sin(_lat[0])
+  cos_lat = cos(_lat[0])
 
-    sin_lon = sin(_lon[0])
-    cos_lon = cos(_lon[0])
+  sin_lon = sin(_lon[0])
+  cos_lon = cos(_lon[0])
 
-    for i in range(nblts):
-        xyz_use[0] = xyz[0, i] - xyz_center[0]
-        xyz_use[1] = xyz[1, i] - xyz_center[1]
-        xyz_use[2] = xyz[2, i] - xyz_center[2]
+  for i in range(nblts):
+    xyz_use[0] = xyz[0, i] - xyz_center[0]
+    xyz_use[1] = xyz[1, i] - xyz_center[1]
+    xyz_use[2] = xyz[2, i] - xyz_center[2]
 
-        enu[0, i] = -sin_lon * xyz_use[0] + cos_lon * xyz_use[1]
-        enu[1, i] = (
-          - sin_lat * cos_lon * xyz_use[0]
-          - sin_lat * sin_lon * xyz_use[1]
-          + cos_lat * xyz_use[2]
-        )
-        enu[2, i] = (
-          cos_lat * cos_lon * xyz_use[0]
-          + cos_lat * sin_lon * xyz_use[1]
-          + sin_lat * xyz_use[2]
-        )
+    enu[0, i] = -sin_lon * xyz_use[0] + cos_lon * xyz_use[1]
+    enu[1, i] = (
+      - sin_lat * cos_lon * xyz_use[0]
+      - sin_lat * sin_lon * xyz_use[1]
+      + cos_lat * xyz_use[2]
+    )
+    enu[2, i] = (
+      cos_lat * cos_lon * xyz_use[0]
+      + cos_lat * sin_lon * xyz_use[1]
+      + sin_lat * xyz_use[2]
+    )
 
-    return _enu
+  return _enu
 
 # this function takes memoryviews as inputs
 # that is why _lat, _lon, and _alt are indexed below to get the 0th entry
 @cython.boundscheck(False)
 @cython.wraparound(False)
 cpdef numpy.ndarray[dtype=numpy.float64_t] _ECEF_FROM_ENU(
-    numpy.float64_t[:, ::1] enu,
-    numpy.float64_t[::1] _lat,
-    numpy.float64_t[::1] _lon,
-    numpy.float64_t[::1] _alt,
+  numpy.float64_t[:, ::1] enu,
+  numpy.float64_t[::1] _lat,
+  numpy.float64_t[::1] _lon,
+  numpy.float64_t[::1] _alt,
 ):
-    cdef Py_ssize_t i
-    cdef int nblts = enu.shape[1]
-    cdef numpy.float64_t sin_lat, cos_lat, sin_lon, cos_lon
+  cdef Py_ssize_t i
+  cdef int nblts = enu.shape[1]
+  cdef numpy.float64_t sin_lat, cos_lat, sin_lon, cos_lon
 
-    # allocate memory then make memory view for faster access
-    cdef numpy.ndarray[dtype=numpy.float64_t, ndim=2] _xyz = np.zeros((3, nblts), dtype=np.float64)
-    cdef numpy.float64_t[:, ::1] xyz = _xyz
+  # allocate memory then make memory view for faster access
+  cdef numpy.ndarray[dtype=numpy.float64_t, ndim=2] _xyz = np.zeros((3, nblts), dtype=np.float64)
+  cdef numpy.float64_t[:, ::1] xyz = _xyz
 
-    # we want a memoryview of the xyz of the center
-    # this looks a little silly but we don't have to define 2 different things
-    cdef numpy.float64_t[:] xyz_center = _xyz_from_latlonalt(_lat, _lon, _alt)[:, 0]
+  # we want a memoryview of the xyz of the center
+  # this looks a little silly but we don't have to define 2 different things
+  cdef numpy.float64_t[:] xyz_center = _xyz_from_latlonalt(_lat, _lon, _alt)[:, 0]
 
-    sin_lat = sin(_lat[0])
-    cos_lat = cos(_lat[0])
+  sin_lat = sin(_lat[0])
+  cos_lat = cos(_lat[0])
 
-    sin_lon = sin(_lon[0])
-    cos_lon = cos(_lon[0])
+  sin_lon = sin(_lon[0])
+  cos_lon = cos(_lon[0])
 
-    for i in range(nblts):
-        xyz[0, i] = (
-            - sin_lat * cos_lon * enu[1, i]
-            - sin_lon * enu[0, i]
-            + cos_lat * cos_lon * enu[2, i]
-            + xyz_center[0]
-        )
-        xyz[1, i] = (
-            - sin_lat * sin_lon * enu[1, i]
-            + cos_lon * enu[0, i]
-            + cos_lat * sin_lon * enu[2, i]
-            + xyz_center[1]
-        )
-        xyz[2, i] = cos_lat * enu[1, i] + sin_lat * enu[2, i] + xyz_center[2]
+  for i in range(nblts):
+    xyz[0, i] = (
+      - sin_lat * cos_lon * enu[1, i]
+      - sin_lon * enu[0, i]
+      + cos_lat * cos_lon * enu[2, i]
+      + xyz_center[0]
+    )
+    xyz[1, i] = (
+      - sin_lat * sin_lon * enu[1, i]
+      + cos_lon * enu[0, i]
+      + cos_lat * sin_lon * enu[2, i]
+      + xyz_center[1]
+    )
+    xyz[2, i] = cos_lat * enu[1, i] + sin_lat * enu[2, i] + xyz_center[2]
 
-    return _xyz
+  return _xyz
 
 # inital_uvw is a memoryviewed array as an input
 @cython.boundscheck(False)
