e49f8956bc1ded419a72243ad56f664f770c53e2
Matthew Kolopanis <Matthew.Kolopanis@gmail.com>
2020-12-16 09:58:39 -0700

    update CI yamls and setup to test against 3.9; drop 3.6 active testing

--- setup.py
+++ setup.py
@@ -167,6 +167,7 @@ setup_args = {
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Topic :: Scientific/Engineering :: Astronomy",
     ],
     "keywords": "radio astronomy interferometry",
