8b7949b7680b22ff12bd06706b70efd461f1d37c
Matthew Kolopanis <Matthew.Kolopanis@gmail.com>
2021-02-10 09:47:48 -0700

    add C flags for cython line tracing the test coverage scripts

--- scripts/test_cover_noinstall.sh
+++ scripts/test_cover_noinstall.sh
@@ -3,6 +3,9 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/..
 
+# We want to make sure we build the Cython extensions now.
+CFLAGS="-DCYTHON_TRACE=1 -DCYTHON_TRACE_NOGIL=1" pip install -e . --no-deps
+
 cd pyuvdata
 python -m pytest --cov=pyuvdata --cov-config=../.coveragerc\
        --cov-report term --cov-report html:tests/cover \
