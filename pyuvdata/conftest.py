8383696320850233f91915960e5bf6768e744d0b
Bryna Hazelton <bryna.hazelton@gmail.com>
2021-07-20 12:09:17 -0700

    First pass at initializing UVCal from UVData

--- pyuvdata/conftest.py
+++ pyuvdata/conftest.py
@@ -3,10 +3,16 @@
 # Licensed under the 2-clause BSD License
 
 """Testing environment setup and teardown for pytest."""
-import pytest
+import os
+
 from astropy.utils import iers
 from astropy.time import Time
 from astropy.coordinates import EarthLocation
+import numpy as np
+import pytest
+
+from pyuvdata import UVData, UVCal
+from pyuvdata.data import DATA_PATH
 
 
 @pytest.fixture(autouse=True, scope="session")
@@ -31,3 +37,57 @@ def setup_and_teardown_package():
     yield
 
     iers.conf.auto_max_age = 30
+
+
+@pytest.fixture(scope="session")
+def uvcalibrate_init_data_main():
+    """Make initial uvcalibrate inputs."""
+    uvdata = UVData()
+    uvdata.read(
+        os.path.join(DATA_PATH, "zen.2458098.45361.HH.uvh5_downselected"),
+        file_type="uvh5",
+    )
+    uvcal = UVCal()
+    uvcal.read_calfits(
+        os.path.join(DATA_PATH, "zen.2458098.45361.HH.omni.calfits_downselected")
+    )
+
+    yield uvdata, uvcal
+
+
+@pytest.fixture(scope="function")
+def uvcalibrate_init_data(uvcalibrate_init_data_main):
+    """Make function level initial uvcalibrate inputs."""
+    uvdata_in, uvcal_in = uvcalibrate_init_data_main
+
+    uvdata = uvdata_in.copy()
+    uvcal = uvcal_in.copy()
+
+    yield uvdata, uvcal
+
+
+@pytest.fixture(scope="session")
+def uvcalibrate_data_main(uvcalibrate_init_data_main):
+    """Make uvcalibrate inputs."""
+    uvdata_in, uvcal_in = uvcalibrate_init_data_main
+
+    uvdata = uvdata_in.copy()
+    uvcal = uvcal_in.copy()
+
+    # fix the antenna names in the uvcal object to match the uvdata object
+    uvcal.antenna_names = np.array(
+        [name.replace("ant", "HH") for name in uvcal.antenna_names]
+    )
+
+    yield uvdata, uvcal
+
+
+@pytest.fixture(scope="function")
+def uvcalibrate_data(uvcalibrate_data_main):
+    """Make function level uvcalibrate inputs."""
+    uvdata_in, uvcal_in = uvcalibrate_data_main
+
+    uvdata = uvdata_in.copy()
+    uvcal = uvcal_in.copy()
+
+    yield uvdata, uvcal
