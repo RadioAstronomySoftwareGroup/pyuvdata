b382be644848abe315df035d1cec23f694e65427
Bryna Hazelton <bryna.hazelton@gmail.com>
2021-11-10 09:56:51 -0800

    remove duplicated test fixtures

--- pyuvdata/uvdata/tests/test_uvdata.py
+++ pyuvdata/uvdata/tests/test_uvdata.py
@@ -205,34 +205,6 @@ def uvdata_props():
 
 
 @pytest.fixture(scope="session")
-def hera_uvh5_main():
-    # read in test file for the resampling in time functions
-    uv_object = UVData()
-    testfile = os.path.join(DATA_PATH, "zen.2458661.23480.HH.uvh5")
-    uv_object.read(testfile)
-
-    yield uv_object
-
-    # cleanup
-    del uv_object
-
-    return
-
-
-@pytest.fixture(scope="function")
-def hera_uvh5(hera_uvh5_main):
-    # read in test file for the resampling in time functions
-    uv_object = hera_uvh5_main.copy()
-
-    yield uv_object
-
-    # cleanup
-    del uv_object
-
-    return
-
-
-@pytest.fixture(scope="session")
 def hera_uvh5_split_main(hera_uvh5_main):
     # Get some meta info up from
     unique_times = np.unique(hera_uvh5_main.time_array)
