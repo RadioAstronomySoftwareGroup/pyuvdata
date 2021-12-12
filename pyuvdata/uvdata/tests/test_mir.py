6bdb495413e592f1b6e05daae64afef5c709baf8
Garrett 'Karto' Keating <garrett.keating@cfa.harvard.edu>
2021-12-11 23:06:59 -0500

    Cleaning up tests following moving fixtures

--- pyuvdata/uvdata/tests/test_mir.py
+++ pyuvdata/uvdata/tests/test_mir.py
@@ -22,109 +22,55 @@ from ...uvdata.mir import Mir
 
 
 @pytest.fixture(scope="session")
-def sma_mir_main():
-    # read in test file for the resampling in time functions
-    uv_object = UVData()
+def mir_data():
     testfile = os.path.join(DATA_PATH, "sma_test.mir")
-    uv_object.read(testfile)
+    mir_data = MirParser(
+        testfile, load_vis=True, load_raw=True, load_auto=True,
+    )
 
-    yield uv_object
-
-
-@pytest.fixture(scope="function")
-def sma_mir(sma_mir_main):
-    # read in test file for the resampling in time functions
-    uv_object = sma_mir_main.copy()
-
-    yield uv_object
-
-
-@pytest.fixture(scope="session")
-def sma_mir_filt_main():
-    # read in test file for the resampling in time functions
-    uv_object = UVData()
-    testfile = os.path.join(DATA_PATH, "sma_test.mir")
-    uv_object.read(testfile, pseudo_cont=True, corrchunk=0)
-
-    uv_object.flag_array[:, :, : uv_object.Nfreqs // 2, 0] = True
-    uv_object.flag_array[:, :, uv_object.Nfreqs // 2 :, 1] = True
-    uv_object.set_lsts_from_time_array()
-    uv_object._set_app_coords_helper()
-
-    yield uv_object
-
-
-@pytest.fixture(scope="function")
-def sma_mir_filt(sma_mir_filt_main):
-    # read in test file for the resampling in time functions
-    uv_object = sma_mir_filt_main.copy()
-
-    yield uv_object
+    yield mir_data
 
 
 @pytest.fixture
 def uv_in_ms(tmp_path):
-    uv_in = UVData()
-    testfile = os.path.join(DATA_PATH, "sma_test.mir")
     write_file = os.path.join(tmp_path, "outtest_mir.ms")
-
-    # Currently only one source is supported.
-    uv_in.read(testfile)
     uv_out = UVData()
 
-    yield uv_in, uv_out, write_file
-
-    # cleanup
-    del uv_in, uv_out
+    yield uv_out, write_file
 
 
 @pytest.fixture
 def uv_in_uvfits(tmp_path):
-    uv_in = UVData()
-    testfile = os.path.join(DATA_PATH, "sma_test.mir/")
     write_file = os.path.join(tmp_path, "outtest_mir.uvfits")
 
-    # Currently only one source is supported.
-    uv_in.read(testfile, pseudo_cont=False)
     uv_out = UVData()
 
-    yield uv_in, uv_out, write_file
-
-    # cleanup
-    del uv_in, uv_out
+    yield uv_out, write_file
 
 
 @pytest.fixture
 def uv_in_uvh5(tmp_path):
-    uv_in = UVData()
-    testfile = os.path.join(DATA_PATH, "sma_test.mir")
     write_file = os.path.join(tmp_path, "outtest_mir.uvh5")
-
-    # Currently only one source is supported.
-    uv_in.read(testfile)
     uv_out = UVData()
 
-    yield uv_in, uv_out, write_file
-
-    # cleanup
-    del uv_in, uv_out
+    yield uv_out, write_file
 
 
 @pytest.mark.filterwarnings("ignore:LST values stored in this file are not ")
 @pytest.mark.parametrize("future_shapes", [True, False])
-def test_read_mir_write_uvfits(uv_in_uvfits, future_shapes):
+def test_read_mir_write_uvfits(sma_mir, uv_in_uvfits, future_shapes):
     """
     Mir to uvfits loopback test.
 
     Read in Mir files, write out as uvfits, read back in and check for
     object equality.
     """
-    mir_uv, uvfits_uv, testfile = uv_in_uvfits
+    uvfits_uv, testfile = uv_in_uvfits
 
     if future_shapes:
-        mir_uv.use_future_array_shapes()
+        sma_mir.use_future_array_shapes()
 
-    mir_uv.write_uvfits(testfile, spoof_nonessential=True)
+    sma_mir.write_uvfits(testfile, spoof_nonessential=True)
     uvfits_uv.read_uvfits(testfile)
 
     if future_shapes:
@@ -132,72 +78,72 @@ def test_read_mir_write_uvfits(uv_in_uvfits, future_shapes):
 
     # UVFITS doesn't allow for numbering of spectral windows like MIR does, so
     # we need an extra bit of handling here
-    assert len(np.unique(mir_uv.spw_array)) == len(np.unique(uvfits_uv.spw_array))
+    assert len(np.unique(sma_mir.spw_array)) == len(np.unique(uvfits_uv.spw_array))
 
-    spw_dict = {idx: jdx for idx, jdx in zip(uvfits_uv.spw_array, mir_uv.spw_array)}
+    spw_dict = {idx: jdx for idx, jdx in zip(uvfits_uv.spw_array, sma_mir.spw_array)}
 
     assert np.all(
         [
             idx == spw_dict[jdx]
-            for idx, jdx in zip(mir_uv.flex_spw_id_array, uvfits_uv.flex_spw_id_array,)
+            for idx, jdx in zip(sma_mir.flex_spw_id_array, uvfits_uv.flex_spw_id_array,)
         ]
     )
 
     # Now that we've checked, set this things as equivalent
-    uvfits_uv.spw_array = mir_uv.spw_array
-    uvfits_uv.flex_spw_id_array = mir_uv.flex_spw_id_array
+    uvfits_uv.spw_array = sma_mir.spw_array
+    uvfits_uv.flex_spw_id_array = sma_mir.flex_spw_id_array
 
     # Check the history first via find
     assert 0 == uvfits_uv.history.find(
-        mir_uv.history + "  Read/written with pyuvdata version:"
+        sma_mir.history + "  Read/written with pyuvdata version:"
     )
-    mir_uv.history = uvfits_uv.history
+    sma_mir.history = uvfits_uv.history
 
     # We have to do a bit of special handling for the phase_center_catalog, because
     # _very_ small errors (like last bit in the mantissa) creep in when passing through
     # the util function transform_sidereal_coords (for mutli-phase-ctr datasets). Verify
     # the two match up in terms of their coordinates
-    for cat_name in mir_uv.phase_center_catalog.keys():
+    for cat_name in sma_mir.phase_center_catalog.keys():
         assert np.isclose(
-            mir_uv.phase_center_catalog[cat_name]["cat_lat"],
+            sma_mir.phase_center_catalog[cat_name]["cat_lat"],
             uvfits_uv.phase_center_catalog[cat_name]["cat_lat"],
         )
         assert np.isclose(
-            mir_uv.phase_center_catalog[cat_name]["cat_lon"],
+            sma_mir.phase_center_catalog[cat_name]["cat_lon"],
             uvfits_uv.phase_center_catalog[cat_name]["cat_lon"],
         )
-    uvfits_uv.phase_center_catalog = mir_uv.phase_center_catalog
+    uvfits_uv.phase_center_catalog = sma_mir.phase_center_catalog
 
     # There's a minor difference between what SMA calculates online for app coords
     # and what pyuvdata calculates, to the tune of ~1 arcsec. Check those values here,
     # then set them equal to one another.
     assert np.all(
-        np.abs(mir_uv.phase_center_app_ra - uvfits_uv.phase_center_app_ra) < 1e-5
+        np.abs(sma_mir.phase_center_app_ra - uvfits_uv.phase_center_app_ra) < 1e-5
     )
 
     assert np.all(
-        np.abs(mir_uv.phase_center_app_dec - uvfits_uv.phase_center_app_dec) < 1e-5
+        np.abs(sma_mir.phase_center_app_dec - uvfits_uv.phase_center_app_dec) < 1e-5
     )
 
-    mir_uv._set_app_coords_helper()
+    sma_mir._set_app_coords_helper()
     uvfits_uv._set_app_coords_helper()
 
     # make sure filenames are what we expect
-    assert mir_uv.filename == ["sma_test.mir"]
+    assert sma_mir.filename == ["sma_test.mir"]
     assert uvfits_uv.filename == ["outtest_mir.uvfits"]
-    mir_uv.filename = uvfits_uv.filename
-    assert mir_uv == uvfits_uv
+    sma_mir.filename = uvfits_uv.filename
+    assert sma_mir == uvfits_uv
 
     # Since mir is mutli-phase-ctr by default, this should effectively be a no-op
-    mir_uv._set_multi_phase_center()
+    sma_mir._set_multi_phase_center()
 
-    assert mir_uv == uvfits_uv
+    assert sma_mir == uvfits_uv
 
 
 @pytest.mark.filterwarnings("ignore:Writing in the MS file that the units of the data")
 @pytest.mark.filterwarnings("ignore:LST values stored in this file are not ")
 @pytest.mark.parametrize("future_shapes", [True, False])
-def test_read_mir_write_ms(uv_in_ms, future_shapes):
+def test_read_mir_write_ms(sma_mir, uv_in_ms, future_shapes):
     """
     Mir to uvfits loopback test.
 
@@ -205,12 +151,12 @@ def test_read_mir_write_ms(uv_in_ms, future_shapes):
     object equality.
     """
     pytest.importorskip("casacore")
-    mir_uv, ms_uv, testfile = uv_in_ms
+    ms_uv, testfile = uv_in_ms
 
     if future_shapes:
-        mir_uv.use_future_array_shapes()
+        sma_mir.use_future_array_shapes()
 
-    mir_uv.write_ms(testfile, clobber=True)
+    sma_mir.write_ms(testfile, clobber=True)
     ms_uv.read(testfile)
 
     # Single integration with 1 phase center = single scan number
@@ -223,79 +169,78 @@ def test_read_mir_write_ms(uv_in_ms, future_shapes):
     # There are some minor differences between the values stored by MIR and that
     # calculated by UVData. Since MS format requires these to be calculated on the fly,
     # we calculate them here just to verify that everything is looking okay.
-    mir_uv.set_lsts_from_time_array()
-    mir_uv._set_app_coords_helper()
+    sma_mir.set_lsts_from_time_array()
+    sma_mir._set_app_coords_helper()
 
     # These reorderings just make sure that data from the two formats are lined up
     # correctly.
-    mir_uv.reorder_freqs(spw_order="number")
+    sma_mir.reorder_freqs(spw_order="number")
     ms_uv.reorder_blts()
 
     # MS doesn't have the concept of an "instrument" name like FITS does, and instead
     # defaults to the telescope name. Make sure that checks out here.
-    assert mir_uv.instrument == "SWARM"
+    assert sma_mir.instrument == "SWARM"
     assert ms_uv.instrument == "SMA"
-    mir_uv.instrument = ms_uv.instrument
+    sma_mir.instrument = ms_uv.instrument
 
     # Quick check for history here
-    assert ms_uv.history != mir_uv.history
-    ms_uv.history = mir_uv.history
+    assert ms_uv.history != sma_mir.history
+    ms_uv.history = sma_mir.history
 
     # Only MS has extra keywords, verify those look as expected.
     assert ms_uv.extra_keywords == {"DATA_COL": "DATA", "observer": "SMA"}
-    assert mir_uv.extra_keywords == {}
-    mir_uv.extra_keywords = ms_uv.extra_keywords
+    assert sma_mir.extra_keywords == {}
+    sma_mir.extra_keywords = ms_uv.extra_keywords
 
     # Make sure the filenames line up as expected.
-    assert mir_uv.filename == ["sma_test.mir"]
+    assert sma_mir.filename == ["sma_test.mir"]
     assert ms_uv.filename == ["outtest_mir.ms"]
-    mir_uv.filename = ms_uv.filename = None
+    sma_mir.filename = ms_uv.filename = None
 
     # Finally, with all exceptions handled, check for equality.
-    assert ms_uv.__eq__(mir_uv, allowed_failures=["filename"])
+    assert ms_uv.__eq__(sma_mir, allowed_failures=["filename"])
 
 
 @pytest.mark.filterwarnings("ignore:LST values stored ")
-def test_read_mir_write_uvh5(uv_in_uvh5):
+def test_read_mir_write_uvh5(sma_mir, uv_in_uvh5):
     """
     Mir to uvfits loopback test.
 
     Read in Mir files, write out as uvfits, read back in and check for
     object equality.
     """
-    mir_uv, uvh5_uv, testfile = uv_in_uvh5
+    uvh5_uv, testfile = uv_in_uvh5
 
-    mir_uv.write_uvh5(testfile)
+    sma_mir.write_uvh5(testfile)
     uvh5_uv.read_uvh5(testfile)
 
     # Check the history first via find
     assert 0 == uvh5_uv.history.find(
-        mir_uv.history + "  Read/written with pyuvdata version:"
+        sma_mir.history + "  Read/written with pyuvdata version:"
     )
 
     # test fails because of updated history, so this is our workaround for now.
-    mir_uv.history = uvh5_uv.history
+    sma_mir.history = uvh5_uv.history
 
     # make sure filenames are what we expect
-    assert mir_uv.filename == ["sma_test.mir"]
+    assert sma_mir.filename == ["sma_test.mir"]
     assert uvh5_uv.filename == ["outtest_mir.uvh5"]
-    mir_uv.filename = uvh5_uv.filename
+    sma_mir.filename = uvh5_uv.filename
 
-    assert mir_uv == uvh5_uv
+    assert sma_mir == uvh5_uv
 
 
-def test_write_mir(uv_in_uvfits, err_type=NotImplementedError):
+def test_write_mir(hera_uvh5, err_type=NotImplementedError):
     """
     Mir writer test
 
     Check and make sure that attempts to use the writer return a
     'not implemented' error.
     """
-    mir_uv, uvfits_uv, testfile = uv_in_uvfits
 
     # Check and see if the correct error is raised
     with pytest.raises(err_type):
-        mir_uv.write_mir("dummy.mir")
+        hera_uvh5.write_mir("dummy.mir")
 
 
 def test_multi_nchan_spw_read(tmp_path):
@@ -335,7 +280,7 @@ def test_read_mir_no_records():
         uv_in.read_mir(testfile, corrchunk=999)
 
 
-def test_read_mir_sideband_select():
+def test_read_mir_sideband_select(sma_mir):
     """
     Mir sideband read check
 
@@ -343,12 +288,11 @@ def test_read_mir_sideband_select():
     stitch them back together as though they were read together from the start.
     """
     testfile = os.path.join(DATA_PATH, "sma_test.mir")
-    mir_dsb = UVData()
-    mir_dsb.read(testfile)
+
     # Re-order here so that we can more easily compare the two
-    mir_dsb.reorder_freqs(channel_order="freq", spw_order="freq")
+    sma_mir.reorder_freqs(channel_order="freq", spw_order="freq")
     # Drop the history
-    mir_dsb.history = ""
+    sma_mir.history = ""
 
     mir_lsb = UVData()
     mir_lsb.read(testfile, isb=[0])
@@ -362,7 +306,7 @@ def test_read_mir_sideband_select():
     # Drop the history
     mir_recomb.history = ""
 
-    assert mir_dsb == mir_recomb
+    assert sma_mir == mir_recomb
 
 
 def test_mir_auto_read(
