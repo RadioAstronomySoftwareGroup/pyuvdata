Fixing conjugation issue and weights calculation in MIR

pyuvdata/uvdata/mir.py
@@ -402,16 +402,18 @@ class Mir(UVData):
         ):
             blt_idx = blhid_blt_order[sp_rec["blhid"]]
             spdx = spdx_dict[window]
-            vis_data[(blt_idx, spdx["pol_idx"], spdx["ch_slice"])] = vis_rec["vis_data"]
+            vis_data[(blt_idx, spdx["pol_idx"], spdx["ch_slice"])] = np.conj(
+                vis_rec["vis_data"]
+            )
             vis_flags[(blt_idx, spdx["pol_idx"], spdx["ch_slice"])] = vis_rec[
                 "vis_flags"
             ]
             # The "wt" column is calcualted as (T_DSB ** 2)/(integ time), but we want
             # units of Jy**-2. To do this, we just need to multiply by the forward gain
-            # of the antenna squared and the channel width. The factor of 2**-2 (0.25)
+            # of the antenna squared and the channel width. The factor of 2**2 (4)
             # arises because we need to convert T_DSB**2 to T_SSB**2.
             vis_weights[(blt_idx, spdx["pol_idx"], spdx["ch_slice"])] = (
-                ((130.0 * 0.5) ** 2.0) * sp_rec["wt"] * np.abs(sp_rec["fres"])
+                ((130.0 * 2.0) ** (2.0)) * sp_rec["wt"] * np.abs(sp_rec["fres"])
             )
 
         # Drop the data from the MirParser object once we have it loaded up.