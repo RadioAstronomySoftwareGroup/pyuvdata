6f527d3c378c67e64f8e064bb7006be81d2ac8c1
Paul La Plante <plaplant@sas.upenn.edu>
2022-06-23 15:36:26 -0700

    Specify setuptools_scm version in more places

--- setup.py
+++ setup.py
@@ -140,7 +140,7 @@ setup_args = {
         "numpy>=1.19",
         "pyerfa>=2.0",
         "scipy>=1.3",
-        "setuptools_scm",
+        "setuptools_scm<7.0",
     ],
     "extras_require": {
         "astroquery": astroquery_reqs,
