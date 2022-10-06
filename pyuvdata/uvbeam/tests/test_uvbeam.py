887986404571080461d85eb2ff3d1720dde6852e
Bryna Hazelton <bryna.hazelton@gmail.com>
2022-10-06 16:56:14 -0700

    attempt to add coverage

--- pyuvdata/uvbeam/tests/test_uvbeam.py
+++ pyuvdata/uvbeam/tests/test_uvbeam.py
@@ -18,7 +18,7 @@ from astropy.io import fits
 
 import pyuvdata.tests as uvtest
 import pyuvdata.utils as uvutils
-from pyuvdata import UVBeam
+from pyuvdata import UVBeam, _uvbeam
 from pyuvdata.data import DATA_PATH
 from pyuvdata.uvbeam.tests.test_cst_beam import cst_files, cst_yaml_file
 from pyuvdata.uvbeam.tests.test_mwa_beam import filename as mwa_beam_file
@@ -1329,6 +1329,21 @@ def test_to_healpix(
 ):
     power_beam = cst_power_2freq_cut
     power_beam_healpix = cst_power_2freq_cut_healpix
+
+    hp_obj = HEALPix(nside=power_beam_healpix.nside)
+    pixels = np.arange(hp_obj.npix)
+    hpx_lon, hpx_lat = hp_obj.healpix_to_lonlat(pixels)
+
+    hpx_theta = (Angle(np.pi / 2, units.radian) - hpx_lat).radian
+    hpx_phi = hpx_lon.radian
+    inds_to_use = _uvbeam.find_healpix_indices(
+        np.ascontiguousarray(cst_power_2freq_cut.axis2_array[::-1], dtype=np.float64),
+        np.ascontiguousarray(cst_power_2freq_cut.axis1_array, dtype=np.float64),
+        np.ascontiguousarray(hpx_theta, dtype=np.float64),
+        np.ascontiguousarray(hpx_phi, dtype=np.float64),
+        np.float64(hp_obj.pixel_resolution.to_value(units.radian)),
+    )
+    assert np.array_equal(pixels[inds_to_use], power_beam_healpix.pixel_array)
 
     sky_area_reduction_factor = (1.0 - np.cos(np.deg2rad(10))) / 2.0
 
