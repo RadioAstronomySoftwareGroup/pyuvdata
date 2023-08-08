c10c105e6f60731cf440de08b2778ab224e7075a
Bryna Hazelton <bryna.hazelton@gmail.com>
2023-08-08 11:54:08 -0700

    update RTD builds

--- docs/conf.py
+++ docs/conf.py
@@ -48,6 +48,7 @@ extensions = [
     "sphinx.ext.coverage",
     "sphinx.ext.viewcode",
     "sphinx.ext.napoleon",
+    "sphinx_rtd_theme",
 ]
 
 # set this to properly handle multiple input params with the same type/shape
@@ -154,9 +155,7 @@ todo_include_todos = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-# html_theme = 'alabaster'
-html_theme = "default"
-html_theme_options = {"rightsidebar": "false", "relbarbgcolor": "black"}
+html_theme = "sphinx_rtd_theme"
 
 
 # Theme options are theme-specific and customize the look and feel of a theme
