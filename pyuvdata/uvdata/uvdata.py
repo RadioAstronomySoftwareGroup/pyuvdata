93197e3b6cfc3e2a4ec8c6c5bd6b1f1b3c20c3c6
Bryna Hazelton <bryna.hazelton@gmail.com>
2023-09-07 10:09:41 -0700

    address reviewer comments

--- pyuvdata/uvdata/uvdata.py
+++ pyuvdata/uvdata/uvdata.py
@@ -4683,7 +4683,7 @@ class UVData(UVBase):
         for key in antpairpols:
             yield (key, self.get_data(key, squeeze=squeeze))
 
-    def conjugate_bls(self, *, convention="ant1<ant2", use_enu=True, uvw_tol=0.0):
+    def conjugate_bls(self, convention="ant1<ant2", *, use_enu=True, uvw_tol=0.0):
         """
         Conjugate baselines according to one of the supported conventions.
 
@@ -5051,9 +5051,7 @@ class UVData(UVBase):
 
         if conj_convention is not None:
             self.conjugate_bls(
-                convention=conj_convention,
-                use_enu=conj_convention_use_enu,
-                uvw_tol=uvw_tol,
+                conj_convention, use_enu=conj_convention_use_enu, uvw_tol=uvw_tol
             )
 
         if isinstance(order, str):
@@ -10180,7 +10178,7 @@ class UVData(UVBase):
                 use_grid_alg=use_grid_alg,
             )
             if conjugate_bls:
-                self.conjugate_bls(convention="u>0", uvw_tol=tol)
+                self.conjugate_bls("u>0", uvw_tol=tol)
 
             if include_conjugates:
                 result = result + (None,)
@@ -10488,7 +10486,7 @@ class UVData(UVBase):
             to find redundancies rather than the older clustering algorithm.
 
         """
-        self.conjugate_bls(convention="u>0")
+        self.conjugate_bls("u>0")
         red_gps, centers, lengths = self.get_redundancies(
             tol=tol, use_antpos=True, conjugate_bls=True, use_grid_alg=use_grid_alg
         )
