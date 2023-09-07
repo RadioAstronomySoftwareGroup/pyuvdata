8d40338b1d894d5671512595e3ec1bb26da68c27
Bryna Hazelton <bryna.hazelton@gmail.com>
2023-09-07 16:01:41 -0700

    don't require order to be passed by name for reorder methods

--- pyuvdata/uvdata/uvdata.py
+++ pyuvdata/uvdata/uvdata.py
@@ -4818,8 +4818,8 @@ class UVData(UVBase):
 
     def reorder_pols(
         self,
-        *,
         order="AIPS",
+        *,
         run_check=True,
         check_extra=True,
         run_check_acceptability=True,
@@ -4929,8 +4929,8 @@ class UVData(UVBase):
 
     def reorder_blts(
         self,
-        *,
         order="time",
+        *,
         minor_order=None,
         autos_first=False,
         conj_convention=None,
@@ -6800,7 +6800,7 @@ class UVData(UVBase):
                     this_pol_ind[other_argsort]
                 ]
 
-                this.reorder_pols(order=temp_ind)
+                this.reorder_pols(temp_ind)
 
         # Pad out self to accommodate new data
         blt_order = None
