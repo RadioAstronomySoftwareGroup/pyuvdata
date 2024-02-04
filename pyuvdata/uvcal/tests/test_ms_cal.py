fb3facb2d7c57b924df30604330851fe7053a42b
Garrett 'Karto' Keating <garrett.keating@cfa.harvard.edu>
2024-02-04 14:06:16 -0500

    Added MS gains writer, first successful loopback (now in tests)

--- pyuvdata/uvcal/tests/test_ms_cal.py
+++ pyuvdata/uvcal/tests/test_ms_cal.py
@@ -3,13 +3,56 @@
 # Licensed under the 2-clause BSD License
 
 """Tests for ms_cal object."""
+import os
+
 import pytest
 
-import pyuvdata.tests as uvtest
-
 from ... import ms_utils
+from ... import tests as uvtest
+from ...data import DATA_PATH
+from ..uvcal import UVCal
 
 pytest.importorskip("casacore")
+
+allowed_failures = ["_filename", "_history"]
+
+
+@pytest.fixture(scope="session")
+def sma_pcal_main():
+    uvobj = UVCal()
+    testfile = os.path.join(DATA_PATH, "sma.ms.pha.gcal")
+    uvobj.read(testfile)
+
+    yield uvobj
+
+
+@pytest.fixture(scope="function")
+def sma_pcal(sma_pcal_main):
+    """Make function level NRAO ms object."""
+    uvobj = sma_pcal_main.copy()
+    yield uvobj
+
+
+@pytest.mark.parametrize("check_warning", [True, False])
+@pytest.mark.parametrize(
+    "frame,epoch,msg",
+    (
+        ["fk5", 1991.1, "Frame fk5 (epoch 1991.1) does not have a corresponding match"],
+        ["fk4", 1991.1, "Frame fk4 (epoch 1991.1) does not have a corresponding match"],
+        ["icrs", 2021.0, "Frame icrs (epoch 2021) does not have a corresponding"],
+    ),
+)
+def test_parse_pyuvdata_frame_ref_errors(check_warning, frame, epoch, msg):
+    """
+    Test errors with matching CASA frames to astropy frame/epochs
+    """
+    if check_warning:
+        with uvtest.check_warnings(UserWarning, match=msg):
+            ms_utils._parse_pyuvdata_frame_ref(frame, epoch, raise_error=False)
+    else:
+        with pytest.raises(ValueError) as cm:
+            ms_utils._parse_pyuvdata_frame_ref(frame, epoch)
+        assert str(cm.value).startswith(msg)
 
 
 @pytest.mark.parametrize("check_warning", [True, False])
@@ -34,3 +77,14 @@ def test_parse_casa_frame_ref_errors(check_warning, frame, errtype, msg):
         with pytest.raises(errtype) as cm:
             ms_utils._parse_casa_frame_ref(frame)
         assert str(cm.value).startswith(msg)
+
+
+def test_ms_cal_wideband_loopback(sma_pcal, tmp_path):
+    uvcal = UVCal()
+    testfile = os.path.join(tmp_path, "ms_cal_loopback.ms")
+    sma_pcal.write_ms_cal(testfile, clobber=True)
+
+    uvcal.read(testfile)
+    # Check that the histories line up
+    assert sma_pcal.history in uvcal.history
+    assert sma_pcal.__eq__(uvcal, allowed_failures=allowed_failures)
