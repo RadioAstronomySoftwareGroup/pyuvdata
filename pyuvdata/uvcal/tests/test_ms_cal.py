6bf95a65bdb6689c6e0cec0e0bf9758e7c36a486
Garrett 'Karto' Keating <garrett.keating@cfa.harvard.edu>
2024-02-05 14:29:10 -0500

    Adding MS delay/bandpass loopback tests, fixing minor bugs/warnings

--- pyuvdata/uvcal/tests/test_ms_cal.py
+++ pyuvdata/uvcal/tests/test_ms_cal.py
@@ -28,8 +28,40 @@ def sma_pcal_main():
 
 @pytest.fixture(scope="function")
 def sma_pcal(sma_pcal_main):
-    """Make function level NRAO ms object."""
+    """Make function level SMA MS phase cal object."""
     uvobj = sma_pcal_main.copy()
+    yield uvobj
+
+
+@pytest.fixture(scope="session")
+def sma_dcal_main():
+    uvobj = UVCal()
+    testfile = os.path.join(DATA_PATH, "sma.ms.dcal")
+    uvobj.read(testfile)
+
+    yield uvobj
+
+
+@pytest.fixture(scope="function")
+def sma_dcal(sma_dcal_main):
+    """Make function level SMA MS delay cal object."""
+    uvobj = sma_dcal_main.copy()
+    yield uvobj
+
+
+@pytest.fixture(scope="session")
+def sma_bcal_main():
+    uvobj = UVCal()
+    testfile = os.path.join(DATA_PATH, "sma.ms.bcal")
+    uvobj.read(testfile)
+
+    yield uvobj
+
+
+@pytest.fixture(scope="function")
+def sma_bcal(sma_bcal_main):
+    """Make function level SMA MS bandpass cal object."""
+    uvobj = sma_bcal_main.copy()
     yield uvobj
 
 
@@ -88,3 +120,25 @@ def test_ms_cal_wideband_loopback(sma_pcal, tmp_path):
     # Check that the histories line up
     assert sma_pcal.history in uvcal.history
     assert sma_pcal.__eq__(uvcal, allowed_failures=allowed_failures)
+
+
+def test_ms_cal_delay_loopback(sma_dcal, tmp_path):
+    uvcal = UVCal()
+    testfile = os.path.join(tmp_path, "ms_cal_delay.ms")
+    sma_dcal.write_ms_cal(testfile, clobber=True)
+
+    uvcal.read(testfile)
+    # Check that the histories line up
+    assert sma_dcal.history in uvcal.history
+    assert sma_dcal.__eq__(uvcal, allowed_failures=allowed_failures)
+
+
+def test_ms_cal_bandpass_loopback(sma_bcal, tmp_path):
+    uvcal = UVCal()
+    testfile = os.path.join(tmp_path, "ms_cal_bandpass.ms")
+    sma_bcal.write_ms_cal(testfile, clobber=True)
+
+    uvcal.read(testfile)
+    # Check that the histories line up
+    assert sma_bcal.history in uvcal.history
+    assert sma_bcal.__eq__(uvcal, allowed_failures=allowed_failures)
