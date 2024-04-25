8209082358cfd43270bbe149c28bbb94735d5ccc
Bryna Hazelton <bryna.hazelton@gmail.com>
2024-04-25 12:58:37 -0700

    Try to fix mac builds
    Also clarify setuptools_scm requirements since our minimum test version is 7.0.3

--- setup.py
+++ setup.py
@@ -151,7 +151,7 @@ setup_args = {
         "pyyaml>=5.4.1",
         "scipy>=1.7.3",
         "setuptools>=61",
-        "setuptools_scm!=7.0.0,!=7.0.1,!=7.0.2",
+        "setuptools_scm>=7.0.3",
     ],
     "extras_require": {
         "astroquery": astroquery_reqs,
