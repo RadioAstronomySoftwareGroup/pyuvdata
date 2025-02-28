4617216596f8536e4e208d424c3d9b856f8fba12
Matthew Kolopanis <matthew.kolopanis@gmail.com>
2025-02-28 13:59:00 -0700

    add some comments and a better error message if warn fails

--- rust/lib.rs
+++ rust/lib.rs
@@ -11,20 +11,31 @@ mod coordinates;
 fn _warn(msg: &str) {
     Python::with_gil(|py| {
         let user_warning = py.get_type::<pyo3::exceptions::PyUserWarning>();
-        PyErr::warn(py, &user_warning, &CString::new(msg).unwrap(), 0)?;
+        PyErr::warn(
+            py,
+            &user_warning,
+            &CString::new(msg).expect("Warning message cannot be converted to CString."),
+            0,
+        )?;
         Ok::<_, PyErr>(())
     })
     .expect("Unable to issue python warning.");
 }
 
+// we have to do some oddly convolute things to make sure we can
+// import he submodules as expected from the python side
 fn register_child_module(
     parent_module: &Bound<'_, PyModule>,
     child_name: &str,
     fn_mod: fn(m: &Bound<'_, PyModule>) -> PyResult<()>,
 ) -> PyResult<()> {
+    // initialize a submodule with the given name
     let child_module = PyModule::new(parent_module.py(), child_name)?;
+    // init the actual submodule
     fn_mod(&child_module)?;
+    // attach the submodule to the name defined above
     parent_module.add_submodule(&child_module)?;
+    // add the correct entries into the __dict__ so it can be imported
     parent_module
         .py()
         .import("sys")?
