4068828cd8194a1f03d333f964963c98f3d41913
Matthew Kolopanis <matthew.kolopanis@gmail.com>
2025-02-28 13:14:11 -0700

    bumpy pyO3 to 0.23.0; restructure to use Bounds api

--- rust/lib.rs
+++ rust/lib.rs
@@ -1,5 +1,9 @@
-use pyo3::{pymodule, types::PyModule, PyErr, PyResult, Python};
-
+use pyo3::{
+    pymodule,
+    types::{PyAnyMethods, PyModule, PyModuleMethods},
+    Bound, PyErr, PyResult, Python,
+};
+use std::ffi::CString;
 mod bls;
 
 mod coordinates;
@@ -7,27 +11,32 @@ mod coordinates;
 fn _warn(msg: &str) {
     Python::with_gil(|py| {
         let user_warning = py.get_type::<pyo3::exceptions::PyUserWarning>();
-        PyErr::warn(py, user_warning, msg, 0)?;
+        PyErr::warn(py, &user_warning, &CString::new(msg).unwrap(), 0)?;
         Ok::<_, PyErr>(())
     })
     .expect("Unable to issue python warning.");
 }
 
+fn register_child_module(
+    parent_module: &Bound<'_, PyModule>,
+    child_name: &str,
+    fn_mod: fn(m: &Bound<'_, PyModule>) -> PyResult<()>,
+) -> PyResult<()> {
+    let child_module = PyModule::new(parent_module.py(), child_name)?;
+    fn_mod(&child_module)?;
+    parent_module.add_submodule(&child_module)?;
+    parent_module
+        .py()
+        .import("sys")?
+        .getattr("modules")?
+        .set_item(child_name, &child_module)?;
+
+    Ok(())
+}
+
 #[pymodule]
-fn _utils_rs<'py>(py: Python<'py>, m: &'py PyModule) -> PyResult<()> {
-    let bls_mod = PyModule::new(py, "_utils_rs._bls")?;
-    bls::_bls(py, bls_mod)?;
-    m.add("_bls", bls_mod)?;
-    py.import("sys")?
-        .getattr("modules")?
-        .set_item("_utils_rs._bls", bls_mod)?;
-
-    let coords = PyModule::new(py, "_utils_rs._coordinates")?;
-    coordinates::_coordinates(py, coords)?;
-    m.add("_coordinates", coords)?;
-    py.import("sys")?
-        .getattr("modules")?
-        .set_item("_utils_rs._coordinates", coords)?;
-
+fn _utils_rs(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    register_child_module(m, "_bls", bls::_bls)?;
+    register_child_module(m, "_coordinates", coordinates::_coordinates)?;
     Ok(())
 }
