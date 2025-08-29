fc062fa2d40273cff66a4e9080814ff612f05e8e
Bryna Hazelton <bryna.hazelton@gmail.com>
2025-08-29 15:10:22 -0700

    Fix imports for older numpy versions

--- src/pyuvdata/utils/types.py
+++ src/pyuvdata/utils/types.py
@@ -3,11 +3,12 @@
 """Define some commonly used numpy array type hint aliases."""
 
 import numpy as np
+from numpy.typing import NDArray
 
-IntArray = np.typing.NDArray[np.integer]
-FloatArray = np.typing.NDArray[np.floating]
-StrArray = np.typing.NDArray[np.str_]
-BoolArray = np.typing.NDArray[np.bool_]
+IntArray = NDArray[np.integer]
+FloatArray = NDArray[np.floating]
+StrArray = NDArray[np.str_]
+BoolArray = NDArray[np.bool_]
 
-Int64Array = np.typing.NDArray[np.int64]
-UInt64Array = np.typing.NDArray[np.uint64]
+Int64Array = NDArray[np.int64]
+UInt64Array = NDArray[np.uint64]
