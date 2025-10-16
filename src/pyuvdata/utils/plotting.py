81d8dfecc0df0fba517bbcfd9d3bfbf8fefb1072
Bryna Hazelton <bryna.hazelton@gmail.com>
2025-10-15 18:55:39 -0700

    refactor plotting code to allow it to be called without an object

--- src/pyuvdata/utils/plotting.py
+++ src/pyuvdata/utils/plotting.py
@@ -10,6 +10,250 @@ from .. import BeamInterface, UVBeam
 from ..analytic_beam import AnalyticBeam, UnpolarizedAnalyticBeam
 from .coordinates import _get_hpix_obj, hpx_latlon_to_zenithangle_azimuth
 from .pol import polnum2str
+from .types import FloatArray
+
+
+def plot_beam_arrays(
+    beam_vals: FloatArray,
+    az_array: FloatArray,
+    za_array: FloatArray,
+    *,
+    complex_type: str = "real",
+    beam_name: str = "",
+    beam_type_label: str = "",
+    feedpol_label: list[str] | None = None,
+    freq_label: float | None = None,
+    logcolor: bool | None = None,
+    plt_kwargs: dict | None = None,
+    norm_kwargs: dict | None = None,
+    savefile: str | None = None,
+):
+    """
+    Make a pretty plot of a beam from arrays.
+
+    This is usually called via UVBeam.plot or AnalyticBeam.plot but can be used
+    for arrays in memory if desired.
+
+    Parameters
+    ----------
+    beam_vals : ndarray of float
+        The beam values to plot. Shape depends on whether it is regularly
+        gridded in az/za or not. For regular grids, the shape is
+        (naxes_vec, nfeedpol, za_grid.size, az_grid.size). For irregular az/za,
+        the shape is (naxes_vec, nfeedpol, n_directions).
+    az_array : ndarray of float
+        The azimuth values. For regular grids, the shape is (za_grid.size,
+        az_grid.size). For irregular grids the shape is (n_directions,).
+    za_array : ndarray of float
+        The zenith angle values. For regular grids, the shape is (za_grid.size,
+        az_grid.size). For irregular grids the shape is (n_directions,).
+    complex_type : str
+        What to plot for complex beams, options are: [real, imag, abs, phase].
+        Defaults to "real" for complex beams. Ignored for real beams
+        (i.e. power beams, same feed).
+    beam_name : str, optional
+        The telescope name or beam name, used to label the plots.
+    freq_label : float, optional
+        The frequency, used only in the plots title. Optional.
+    beam_type_label : str, optional
+       Used for labelling the plots.
+    feedpol_label: list of str, optional
+        Feed or polarization labels, used for plot labelling. If provided, must
+        be the same length as the 1st (not 0th) dimension of beam_vals.
+    logcolor : bool, optional
+        Option to use log scaling for the color. Defaults to True for power
+        beams and False for E-field beams. Results in using
+        matplotlib.colors.LogNorm or matplotlib.colors.SymLogNorm if the data
+        have negative values.
+    plt_kwargs : dict, optional
+        Keywords to be passed into the matplotlib.pyplot.imshow call.
+    norm_kwargs : dict, optional
+        Keywords to be passed into the norm object, typically vmin/vmax, plus
+        linthresh for SymLogNorm.
+    savefile : str
+        File to save the plot to.
+
+    """
+    try:
+        import matplotlib.pyplot as plt
+        from matplotlib.colors import LogNorm, SymLogNorm
+    except ImportError as e:
+        raise ImportError(
+            "matplotlib is not installed but is required for plotting "
+            "functionality. Install 'matplotlib' using conda or pip."
+        ) from e
+
+    allowed_complex_types = ["real", "imag", "abs", "phase"]
+    if complex_type not in allowed_complex_types:
+        raise ValueError(
+            f"complex_type must be one of {allowed_complex_types}, but it "
+            f"is {complex_type}."
+        )
+
+    complex_func = {"real": np.real, "imag": np.imag, "abs": np.abs, "phase": np.angle}
+
+    if freq_label is not None:
+        si_prefix = {"T": 1e12, "G": 1e9, "M": 1e6, "k": 1e3}
+        freq_str = f"{freq_label:.0f} Hz"
+        for prefix, multiplier in si_prefix.items():
+            if freq_label > multiplier:
+                freq_str = f"{freq_label / multiplier:.0f} {prefix}Hz"
+                break
+    else:
+        freq_str = ""
+
+    az_za_radial_val = np.sin(za_array)
+    # get 4 radial ticks with values spaced sinusoidally (so ~linear in the plot),
+    # rounded to the nearest 5 degrees
+    radial_ticks_deg = (
+        np.round(
+            np.degrees(np.arcsin(np.linspace(0, np.sin(np.max(za_array)), 5))) / 5
+        ).astype(int)
+        * 5
+    )[1:]
+
+    if np.any(np.iscomplexobj(beam_vals)):
+        beam_vals = complex_func[complex_type](beam_vals)
+        type_label = ", " + complex_type
+    else:
+        type_label = ""
+
+    if beam_vals.ndim == 4:
+        reg_grid = True
+    elif beam_vals.ndim == 3:
+        reg_grid = False
+    else:
+        raise ValueError("beam_vals must be 3 or 4 dimensional.")
+    beam_shape = beam_vals.shape
+    naxes_vec = beam_shape[0]
+    nfeedpol = beam_shape[1]
+
+    if feedpol_label is None:
+        feedpol_label = np.arange(nfeedpol).astype(str).tolist()
+    else:
+        if len(feedpol_label) != nfeedpol:
+            raise ValueError(
+                "feedpol_label must have the same number of elements as the "
+                "1st (not 0th) dimension of beam_vals."
+            )
+
+    if reg_grid:
+        exp_coord_shape = (beam_shape[2], beam_shape[3])
+        if az_array.shape != exp_coord_shape or za_array.shape != exp_coord_shape:
+            raise ValueError(
+                "az_array and za_array must be shaped like the last 2 dimensions "
+                "of beam_vals for regularly gridded beam_vals (beam_vals has 4 "
+                "dimensions)."
+            )
+    else:
+        exp_coord_shape = (beam_shape[2],)
+        if az_array.shape != exp_coord_shape or za_array.shape != exp_coord_shape:
+            raise ValueError(
+                "az_array and za_array must be shaped like the last dimension "
+                "of beam_vals for irregular beam_vals (beam_vals has 3 dimensions)."
+            )
+
+    norm_use = None
+    colormap = "viridis"
+    if norm_kwargs is None:
+        norm_kwargs = {}
+    if plt_kwargs is None:
+        plt_kwargs = {}
+    if logcolor:
+        if np.min(beam_vals) < 0:
+            min_pos_abs = np.min(np.abs(beam_vals)[np.abs(beam_vals) > 0])
+            default_norm_kwargs = {
+                "linthresh": min_pos_abs,
+                "vmax": np.max(np.abs(beam_vals)),
+                "vmin": -1 * np.max(np.abs(beam_vals)),
+            }
+
+            for key, value in default_norm_kwargs.items():
+                if key not in norm_kwargs:
+                    norm_kwargs[key] = value
+            norm_use = SymLogNorm(**norm_kwargs)
+            colormap = "PRGn"
+        else:
+            norm_use = LogNorm(**norm_kwargs)
+    else:
+        if len(norm_kwargs) > 0:
+            for key in ["vmax", "vmin"]:
+                if key in norm_kwargs:
+                    plt_kwargs[key] = norm_kwargs[key]
+        if np.min(beam_vals) < 0:
+            colormap = "PRGn"
+            default_norm_kwargs = {
+                "vmax": np.max(np.abs(beam_vals)),
+                "vmin": -1 * np.max(np.abs(beam_vals)),
+            }
+            for key, value in default_norm_kwargs.items():
+                if key not in plt_kwargs:
+                    plt_kwargs[key] = value
+
+    if naxes_vec == 2:
+        vec_label = ["azimuth", "zenith angle"]
+    else:
+        vec_label = [beam_type_label]
+
+    nrow = naxes_vec
+    ncol = nfeedpol
+    if naxes_vec == 1 and nfeedpol == 4:
+        nrow = 2
+        ncol = 2
+    fig_size = (5 * ncol, 5 * nrow)
+    fig, ax = plt.subplots(
+        nrow, ncol, subplot_kw={"projection": "polar"}, figsize=fig_size, squeeze=False
+    )
+
+    for row_i in range(nrow):
+        for col_i in range(ncol):
+            ax_use = ax[row_i, col_i]
+            if nrow == naxes_vec and ncol == nfeedpol:
+                vec_i = row_i
+                fp_i = col_i
+            else:
+                vec_i = 0
+                fp_i = row_i * 2 + col_i
+
+            ax_use.grid(True)
+            if reg_grid:
+                pl = ax_use.pcolormesh(
+                    az_array,
+                    az_za_radial_val,
+                    beam_vals[vec_i, fp_i],
+                    cmap=colormap,
+                    norm=norm_use,
+                    **plt_kwargs,
+                )
+            else:
+                pl = ax_use.scatter(
+                    az_array,
+                    az_za_radial_val,
+                    c=beam_vals[vec_i, fp_i],
+                    cmap=colormap,
+                    norm=norm_use,
+                    **plt_kwargs,
+                )
+            ax_use.set_rmax(np.max(az_za_radial_val))
+
+            _ = ax_use.set_title(
+                f"{feedpol_label[fp_i]} {beam_name} {vec_label[vec_i]} "
+                f"response ({freq_str}){type_label}",
+                fontsize="medium",
+            )
+            _ = fig.colorbar(pl, ax=ax_use, shrink=0.5, pad=0.1)
+            _ = ax_use.set_yticks(np.sin(np.deg2rad(radial_ticks_deg)))
+            _ = ax_use.set_yticklabels(
+                [f"{rt}" + r"$\degree$" for rt in radial_ticks_deg]
+            )
+
+    fig.tight_layout()
+
+    if savefile is None:  # pragma: nocover
+        plt.show()
+    else:
+        plt.savefig(savefile, bbox_inches="tight")
+    plt.close()
 
 
 def beam_plot(
@@ -58,24 +302,6 @@ def beam_plot(
         File to save the plot to.
 
     """
-    try:
-        import matplotlib.pyplot as plt
-        from matplotlib.colors import LogNorm, SymLogNorm
-    except ImportError as e:
-        raise ImportError(
-            "matplotlib is not installed but is required for plotting "
-            "functionality. Install 'matplotlib' using conda or pip."
-        ) from e
-
-    allowed_complex_types = ["real", "imag", "abs", "phase"]
-    if complex_type not in allowed_complex_types:
-        raise ValueError(
-            f"complex_type must be one of {allowed_complex_types}, but it "
-            f"is {complex_type}."
-        )
-
-    complex_func = {"real": np.real, "imag": np.imag, "abs": np.abs, "phase": np.angle}
-
     if isinstance(beam_obj, UVBeam):
         beam_type = beam_obj.beam_type
 
@@ -165,131 +391,23 @@ def beam_plot(
             if nfeedpol == 1:
                 feedpol_label = [""]
         beam_vals = beam_vals.reshape(naxes_vec, nfeedpol, za_grid.size, az_grid.size)
-    si_prefix = {"T": 1e12, "G": 1e9, "M": 1e6, "k": 1e3}
-    freq_str = f"{freq_title:.0f} Hz"
-    for prefix, multiplier in si_prefix.items():
-        if freq_title > multiplier:
-            freq_str = f"{freq_title / multiplier:.0f} {prefix}Hz"
-            break
 
-    az_za_radial_val = np.sin(za_array)
-    # get 4 radial ticks with values spaced sinusoidally (so ~linear in the plot),
-    # rounded to the nearest 5 degrees
-    radial_ticks_deg = (
-        np.round(
-            np.degrees(np.arcsin(np.linspace(0, np.sin(np.radians(max_zenith_deg)), 5)))
-            / 5
-        ).astype(int)
-        * 5
-    )[1:]
+    if beam_type == "power":
+        beam_type_label = "power"
+    elif beam_type == "efield":
+        beam_type_label = "E-field"
 
-    if np.any(np.iscomplexobj(beam_vals)):
-        beam_vals = complex_func[complex_type](beam_vals)
-        type_label = ", " + complex_type
-    else:
-        type_label = ""
-
-    norm_use = None
-    colormap = "viridis"
-    if norm_kwargs is None:
-        norm_kwargs = {}
-    if plt_kwargs is None:
-        plt_kwargs = {}
-    if logcolor:
-        if np.min(beam_vals) < 0:
-            min_pos_abs = np.min(np.abs(beam_vals)[np.abs(beam_vals) > 0])
-            default_norm_kwargs = {
-                "linthresh": min_pos_abs,
-                "vmax": np.max(np.abs(beam_vals)),
-                "vmin": -1 * np.max(np.abs(beam_vals)),
-            }
-
-            for key, value in default_norm_kwargs.items():
-                if key not in norm_kwargs:
-                    norm_kwargs[key] = value
-            norm_use = SymLogNorm(**norm_kwargs)
-            colormap = "PRGn"
-        else:
-            norm_use = LogNorm(**norm_kwargs)
-    else:
-        if len(norm_kwargs) > 0:
-            for key in ["vmax", "vmin"]:
-                if key in norm_kwargs:
-                    plt_kwargs[key] = norm_kwargs[key]
-        if np.min(beam_vals) < 0:
-            colormap = "PRGn"
-            default_norm_kwargs = {
-                "vmax": np.max(np.abs(beam_vals)),
-                "vmin": -1 * np.max(np.abs(beam_vals)),
-            }
-            for key, value in default_norm_kwargs.items():
-                if key not in plt_kwargs:
-                    plt_kwargs[key] = value
-
-    if naxes_vec == 2:
-        vec_label = ["azimuth", "zenith angle"]
-    else:
-        if beam_type == "power":
-            vec_label = ["power"]
-        else:
-            vec_label = ["E-field"]
-
-    nrow = naxes_vec
-    ncol = nfeedpol
-    if naxes_vec == 1 and nfeedpol == 4:
-        nrow = 2
-        ncol = 2
-    fig_size = (5 * ncol, 5 * nrow)
-    fig, ax = plt.subplots(
-        nrow, ncol, subplot_kw={"projection": "polar"}, figsize=fig_size, squeeze=False
+    plot_beam_arrays(
+        beam_vals,
+        az_array,
+        za_array,
+        complex_type=complex_type,
+        beam_name=name,
+        beam_type_label=beam_type_label,
+        freq_label=freq_title,
+        feedpol_label=feedpol_label,
+        logcolor=logcolor,
+        plt_kwargs=plt_kwargs,
+        norm_kwargs=norm_kwargs,
+        savefile=savefile,
     )
-
-    for row_i in range(nrow):
-        for col_i in range(ncol):
-            ax_use = ax[row_i, col_i]
-            if nrow == naxes_vec and ncol == nfeedpol:
-                vec_i = row_i
-                fp_i = col_i
-            else:
-                vec_i = 0
-                fp_i = row_i * 2 + col_i
-
-            ax_use.grid(True)
-            if reg_grid:
-                pl = ax_use.pcolormesh(
-                    az_array,
-                    az_za_radial_val,
-                    beam_vals[vec_i, fp_i],
-                    cmap=colormap,
-                    norm=norm_use,
-                    **plt_kwargs,
-                )
-            else:
-                pl = ax_use.scatter(
-                    az_array,
-                    az_za_radial_val,
-                    c=beam_vals[vec_i, fp_i],
-                    cmap=colormap,
-                    norm=norm_use,
-                    **plt_kwargs,
-                )
-            ax_use.set_rmax(np.max(az_za_radial_val))
-
-            _ = ax_use.set_title(
-                f"{feedpol_label[fp_i]} {name} {vec_label[vec_i]} "
-                f"response ({freq_str}){type_label}",
-                fontsize="medium",
-            )
-            _ = fig.colorbar(pl, ax=ax_use, shrink=0.5, pad=0.1)
-            _ = ax_use.set_yticks(np.sin(np.deg2rad(radial_ticks_deg)))
-            _ = ax_use.set_yticklabels(
-                [f"{rt}" + r"$\degree$" for rt in radial_ticks_deg]
-            )
-
-    fig.tight_layout()
-
-    if savefile is None:  # pragma: nocover
-        plt.show()
-    else:
-        plt.savefig(savefile, bbox_inches="tight")
-    plt.close()
