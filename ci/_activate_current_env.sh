dee709148d4a380a8eb4526cb3e8d5047ddbb0c7
Bryna Hazelton <bryna.hazelton@gmail.com>
2026-04-07 12:11:31 -0700

    rip out unneeded activate script

--- ci/_activate_current_env.sh
+++
@@ -1,6 +0,0 @@
-# This script should never be called directly, only sourced:
-
-#     source _activate_current_env.sh
-
-# For robustness, try all possible activate commands.
-conda activate "${ENV_NAME}" 2>/dev/null
