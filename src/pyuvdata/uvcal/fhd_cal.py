b137b19dede80d09fa0d0b1f61cfe8564b867920
Bryna Hazelton <bryna.hazelton@gmail.com>
2026-06-04 13:52:00 -0700

    Fix a bug in fhd_cal where pol_convention was not set due to a typo

--- src/pyuvdata/uvcal/fhd_cal.py
+++ src/pyuvdata/uvcal/fhd_cal.py
@@ -229,7 +229,7 @@ class FHDCal(UVCal):
         self._set_sky()
         self.gain_convention = "divide"
         self.gain_scale = "Jy"
-        self.pol_convetions = "sum"
+        self.pol_convention = "sum"
         self._set_gain()
 
         # currently don't have branch info. may change in future.
